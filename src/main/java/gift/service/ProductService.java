b6eca3ec1d453385ce0a86594731cfe7fa7106fa
Jinhwa-Park <parkjinhwa3914@gmail.com>
2024-07-18 16:32:16 +0900

    feat: update ProductRequest, ProductResponse, ProductService to include category handling

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,6 +1,8 @@
 package gift.service;
 
+import gift.entity.Category;
 import gift.entity.Product;
+import gift.repository.CategoryRepository;
 import gift.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,10 +14,12 @@ import java.util.Optional;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public Page<Product> findAll(Pageable pageable) {
@@ -33,6 +37,8 @@ public class ProductService {
     public void deleteById(Long id) {
         productRepository.deleteById(id);
     }
+
+    public Optional<Category> getCategoryById(Long id) {return categoryRepository.findById(id);}
 }
 
 
