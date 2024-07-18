3f683855ae3d5884dfb7dd6780ea1d225dd6b86c
Jinhwa-Park <parkjinhwa3914@gmail.com>
2024-07-18 16:47:03 +0900

    feat: Add code to WishController to verify ProductId

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -1,8 +1,10 @@
 package gift.controller;
 
 import gift.entity.Member;
+import gift.entity.Product;
 import gift.entity.Wish;
 import gift.repository.MemberRepository;
+import gift.repository.ProductRepository;
 import gift.service.WishService;
 import gift.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,27 +20,35 @@ import java.util.Optional;
 public class WishController {
     private final WishService wishService;
     private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
     private final JwtUtil jwtUtil;
 
     @Autowired
-    public WishController(WishService wishService, MemberRepository memberRepository, JwtUtil jwtUtil) {
+    public WishController(WishService wishService, MemberRepository memberRepository, ProductRepository productRepository, JwtUtil jwtUtil) {
         this.wishService = wishService;
         this.memberRepository = memberRepository;
+        this.productRepository = productRepository;
         this.jwtUtil = jwtUtil;
     }
 
     @PostMapping
     public ResponseEntity<Wish> addWish(@RequestBody Wish wish, HttpServletRequest request) {
         String email = jwtUtil.getEmailFromRequest(request);
-        if (email != null) {
-            Optional<Member> memberOpt = memberRepository.findByEmail(email);
-            if (memberOpt.isPresent()) {
-                wish.setMember(memberOpt.get());
-                Wish savedWish = wishService.save(wish);
-                return ResponseEntity.ok(savedWish);
-            }
+        if(email == null) {
+            return ResponseEntity.status(401).body(null);
         }
-        return ResponseEntity.status(401).body(null);
+        Optional<Member> memberOpt = memberRepository.findByEmail(email);
+        if(memberOpt.isEmpty()) {
+            return ResponseEntity.status(401).body(null);
+        }
+        Optional<Product> productOpt = productRepository.findById(wish.getProductId());
+        if(productOpt.isEmpty()) {
+            return ResponseEntity.status(401).body(null);
+        }
+        wish.setMember(memberOpt.get());
+        wish.setProduct(productOpt.get());
+        Wish savedWish = wishService.save(wish);
+        return ResponseEntity.status(201).body(savedWish);
     }
 
     @GetMapping
