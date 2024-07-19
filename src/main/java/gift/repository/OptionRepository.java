b74f7ad6399e7319146f39a3a94be2e2992cea06
Jinhwa-Park <parkjinhwa3914@gmail.com>
2024-07-19 15:32:13 +0900

    feat: add redundancy option check feature

--- src/main/java/gift/repository/OptionRepository.java
+++ src/main/java/gift/repository/OptionRepository.java
@@ -4,7 +4,10 @@ import gift.entity.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OptionRepository extends JpaRepository<Option, Long> {
     List<Option> findByProductId(long productId);
+
+    Optional<Option> findByProductIdAndName(Long productId, String name);
 }
