44ae309dd426611af325602a0e4f9329c05cf009
Jinhwa-Park <parkjinhwa3914@gmail.com>
2024-07-22 02:16:39 +0900

    refactor: enhance validation

--- src/main/java/gift/util/ProductValidator.java
+++ src/main/java/gift/util/ProductValidator.java
@@ -1,12 +1,18 @@
 package gift.util;
 
-import gift.dto.OptionDTO;
+import gift.dto.OptionRequest;
 import gift.dto.ProductRequest;
 
 public class ProductValidator {
     public static void validateProductRequest(ProductRequest productRequest) {
         if(productRequest.getOptions() == null || productRequest.getOptions().isEmpty()) {
             throw new IllegalArgumentException("Product must have at least one option.");
+        }
+
+        for(OptionRequest optionRequest : productRequest.getOptions()) {
+            if(optionRequest.getQuantity() < 1 || optionRequest.getQuantity() >= 100_000_000) {
+                throw new IllegalArgumentException("Quantity must be between 1 and 99,999,999");
+            }
         }
     }
 }
