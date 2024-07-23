364a25cf650b1cdeb20099e42f897bab96a28353
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-24 00:36:32 +0900

    feat: 이름으로 유저 찾기 기능 추가

--- src/main/java/gift/user/model/UserRepository.java
+++ src/main/java/gift/user/model/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
+
+    Optional<AppUser> findByName(String name);
 }
