42cd12d2a5777cbd88f0abbc41dfc9cee40dd4ac
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-23 19:21:44 +0900

    feat: congfiguration 어노테이션 추가

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,11 +1,14 @@
 package gift;
 
+import gift.user.oauth.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(KakaoProperties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
