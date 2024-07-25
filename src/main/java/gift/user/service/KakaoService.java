e299ed65c1d69b969c1899483234cd049548bf71
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-26 03:00:01 +0900

    feat: 카카오톡 나에게 메세지 보내기 기능 구현

---
+++ src/main/java/gift/user/service/KakaoService.java
@@ -0,0 +1,39 @@
+package gift.user.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gift.user.exception.OauthCustomException.FailedToSendMessageException;
+import gift.user.model.dto.AppUser;
+import gift.user.model.dto.KakaoMessageTemplate;
+import java.net.URI;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class KakaoService {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final RestClient client = RestClient.builder().build();
+
+    public void sendMessageToMe(AppUser user, String message) {
+        try {
+            String url = "https://kapi.kakao.com/v2/api/talk/memo/default/send";
+            String header = "Bearer " + user.getAccessToken();
+            MultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
+
+            KakaoMessageTemplate template = new KakaoMessageTemplate(message);
+            String jsonTemplate = objectMapper.writeValueAsString(template);
+            multiValueMap.add("template_object", jsonTemplate);
+
+            client.post()
+                    .uri(URI.create(url))
+                    .body(multiValueMap)
+                    .header("Authorization", header)
+                    .retrieve()
+                    .body(String.class);
+        } catch (Exception e) {
+            throw new FailedToSendMessageException();
+        }
+    }
+
+}
