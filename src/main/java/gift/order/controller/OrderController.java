04e9af0eefbc22c640861a1cccfdf62c9875dd56
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-26 03:01:02 +0900

    feat: 주문 완료 시 나에게 메세지 보내기 기능 구현

--- src/main/java/gift/order/controller/OrderController.java
+++ src/main/java/gift/order/controller/OrderController.java
@@ -5,6 +5,7 @@ import gift.order.model.dto.OrderResponse;
 import gift.order.service.OrderService;
 import gift.resolver.LoginUser;
 import gift.user.model.dto.AppUser;
+import gift.user.service.KakaoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,16 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/orders")
 public class OrderController {
     private final OrderService orderService;
+    private final KakaoService kakaoService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, KakaoService kakaoService) {
         this.orderService = orderService;
+        this.kakaoService = kakaoService;
     }
 
     @PostMapping
     public ResponseEntity<OrderResponse> createOrder(@LoginUser AppUser loginAppUser,
                                                      @RequestBody OrderRequest orderRequest) {
         OrderResponse orderResponse = orderService.createOrder(loginAppUser, orderRequest);
-
+        kakaoService.sendMessageToMe(loginAppUser, orderRequest.message());
         return ResponseEntity.ok().body(orderResponse);
     }
 }
