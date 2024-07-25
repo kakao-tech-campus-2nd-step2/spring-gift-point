75513b9eacaaa88274aef0dfdd0f50b6f5f6fc25
Jinhwa-Park <parkjinhwa3914@gmail.com>
2024-07-25 17:45:24 +0900

    feat: add code about kakao

--- src/main/java/gift/entity/Member.java
+++ src/main/java/gift/entity/Member.java
@@ -1,6 +1,10 @@
 package gift.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
 @Entity
 public class Member {
@@ -14,10 +18,18 @@ public class Member {
     @Column(nullable = false, length = 255)
     private String password;
 
+    @Column(unique = true)
+    private String kakaoId;
+
+    @Column(length = 255)
+    private String nickname;
+
     private Member(Builder builder) {
         this.id = builder.id;
         this.email = builder.email;
         this.password = builder.password;
+        this.kakaoId = builder.kakaoId;
+        this.nickname = builder.nickname;
     }
 
     public Member() {}
@@ -34,31 +46,58 @@ public class Member {
         return password;
     }
 
+    public String getKakaoId() {
+        return kakaoId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
     public void update(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    public void updateKakaoInfo(String kakaoId, String nickname) {
+        this.kakaoId = kakaoId;
+        this.nickname = nickname;
     }
 
     public static class Builder {
         private Long id;
         private String email;
         private String password;
+        private String kakaoId;
+        private String nickname;
 
         public Builder id(Long id) {
             this.id = id;
             return this;
         }
+
         public Builder email(String email) {
             this.email = email;
             return this;
         }
+
         public Builder password(String password) {
             this.password = password;
             return this;
         }
+
+        public Builder kakaoId(String kakaoId) {
+            this.kakaoId = kakaoId;
+            return this;
+        }
+
+        public Builder nickname(String nickname) {
+            this.nickname = nickname;
+            return this;
+        }
+
         public Member build() {
             return new Member(this);
         }
     }
 }
-
