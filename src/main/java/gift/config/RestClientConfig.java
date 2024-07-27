47744a9475d584fd65168bfe178dc685826db485
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-27 16:41:55 +0900

    feat: restClient 타임아웃 설정

--- src/main/java/gift/config/RestClientConfig.java
+++ src/main/java/gift/config/RestClientConfig.java
@@ -1,5 +1,7 @@
 package gift.config;
 
+import java.util.concurrent.TimeUnit;
+import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.DefaultHttpRequestRetryStrategy;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -25,8 +27,18 @@ public class RestClientConfig {
         // 직접 requestFactory를 생성하여 retry 설정을 4회로 변경
         final int maxRetryAttempts = 4;
         final long retryInterval = 300L;
+        final int connectTimeout = 5000; // 연결 시간 초과(서버에 연결을 시도할 때까지 기다리는 시간)
+        final int socketTimeout = 5000; // 응답 시간 초과
+        final int connectionRequestTimeout = 1000; // 연결 요청 시간 초과 (연결 풀에서 연결을 얻기 위해 기다리는 최대 시간)
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(connectTimeout, TimeUnit.MILLISECONDS)
+                .setResponseTimeout(socketTimeout, TimeUnit.MILLISECONDS)
+                .setConnectionRequestTimeout(connectionRequestTimeout, TimeUnit.MILLISECONDS)
+                .build();
 
         CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
                 .setRetryStrategy(
                         new DefaultHttpRequestRetryStrategy(maxRetryAttempts, TimeValue.ofMilliseconds(retryInterval)))
                 .build();
