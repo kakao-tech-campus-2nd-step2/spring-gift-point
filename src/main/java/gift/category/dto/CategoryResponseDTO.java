43bd198d2f7ffa36d91380e4823d02bad2599693
Lee Hyungi <geist9110@naver.com>
2024-07-29 17:19:43 +0900

    feat: Add CategoryResponseDTO

--- src/main/java/gift/category/dto/CategoryResponseDTO.java
+++ src/main/java/gift/category/dto/CategoryResponseDTO.java
@@ -3,8 +3,8 @@ package gift.category.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Objects;
 
-@Schema(description = "카테고리 요청 DTO")
-public class CategoryRequestDTO {
+@Schema(description = "카테고리 응답 DTO")
+public class CategoryResponseDTO {
 
     @Schema(description = "카테고리명")
     private String name;
@@ -13,17 +13,17 @@ public class CategoryRequestDTO {
         return name;
     }
 
-    public CategoryRequestDTO() {
+    public CategoryResponseDTO() {
     }
 
-    public CategoryRequestDTO(String name) {
+    public CategoryResponseDTO(String name) {
         this.name = name;
     }
 
     @Override
     public boolean equals(Object object) {
-        if (object instanceof CategoryRequestDTO categoryRequestDTO) {
-            return Objects.equals(name, categoryRequestDTO.name);
+        if (object instanceof CategoryResponseDTO categoryResponseDTO) {
+            return Objects.equals(name, categoryResponseDTO.name);
         }
         return false;
     }
@@ -32,4 +32,5 @@ public class CategoryRequestDTO {
     public int hashCode() {
         return Objects.hashCode(name);
     }
+
 }
