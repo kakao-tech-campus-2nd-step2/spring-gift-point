303d083c07d55ff78e9712d546023e73d90666e4
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-30 01:01:31 +0900

    test: MemberService 변화에 따른 테스트 코드 수정

--- src/test/java/gift/service/MemberServiceTest.java
+++ src/test/java/gift/service/MemberServiceTest.java
@@ -4,10 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import gift.common.util.JwtUtil;
 import gift.dto.MemberRequest;
-import gift.entity.Role;
 import gift.repository.MemberRepository;
-import gift.repository.MemberRoleRepository;
-import gift.repository.RoleRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,37 +17,27 @@ class MemberServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
-    @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired
-    private MemberRoleRepository memberRoleRepository;
-
     private MemberService memberService;
     private JwtUtil jwtUtil;
 
     @BeforeEach
     void setUp() {
         jwtUtil = new JwtUtil();
-        memberService = new MemberService(memberRepository, jwtUtil, roleRepository,
-            memberRoleRepository);
-
-        Role userRole = new Role("USER");
-        roleRepository.save(userRole);
+        memberService = new MemberService(memberRepository, jwtUtil);
     }
 
     @Test
     @DisplayName("회원 가입")
     void testRegisterMember() {
         // given
-        MemberRequest memberRequest = new MemberRequest("user@example.com", "password");
+        MemberRequest memberRequest = new MemberRequest("user2@example.com", "password");
 
         // when
         String token = memberService.register(memberRequest);
 
         // then
         assertThat(token).isNotNull();
-        assertThat(memberRepository.findByEmail("user@example.com").get().getEmail()).isEqualTo(
+        assertThat(memberRepository.findByEmail("user2@example.com").get().getEmail()).isEqualTo(
             memberRequest.getEmail());
 
     }
@@ -59,7 +46,7 @@ class MemberServiceTest {
     @DisplayName("회원 로그인")
     void testLoginMember() {
         // given
-        MemberRequest memberRequest = new MemberRequest("user@example.com", "password");
+        MemberRequest memberRequest = new MemberRequest("user2@example.com", "password");
         memberService.register(memberRequest);
 
         // when
