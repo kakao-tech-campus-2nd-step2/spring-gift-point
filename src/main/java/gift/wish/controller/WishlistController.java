8af6773c950ffd9f0ff7f5a24174ab06c502bfcb
dong_yxxn <g0326kdy@gmail.com>
2024-07-29 23:36:34 +0900

    refactor: CRUD 기능 보완

--- src/main/java/gift/wish/controller/WishlistController.java
+++ src/main/java/gift/wish/controller/WishlistController.java
@@ -1,15 +1,17 @@
 package gift.wish.controller;
 
-import gift.wish.domain.WishlistItem;
+import gift.wish.domain.WishlistRequest;
+import gift.wish.domain.WishlistResponse;
 import gift.wish.service.WishlistService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,18 +32,45 @@ public class WishlistController {
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "wishlist 가져오기")
-    public ResponseEntity<Page<WishlistItem>> getWishlist(@PathVariable("id") Long userId,
-                                                        @RequestParam(defaultValue = "0") int page,
-                                                        @RequestParam(defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<WishlistItem> wishlist = wishlistService.getWishlistByUserId(userId, pageable);
+    @Operation(summary = "사용자 위시리스트 상품조회")
+    public ResponseEntity<Page<WishlistResponse>> getUserWishlist(@PathVariable("id") Long userId,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "createdDate,desc") String sort) {
+        String[] sortParams = sort.split(",");
+        Sort.Direction direction = Sort.Direction.fromString(sortParams[1]);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortParams[0]));
+
+        Page<WishlistResponse> wishlist = wishlistService.getWishlistResponseByUserId(userId, pageable);
         return new ResponseEntity<>(wishlist, HttpStatus.OK);
     }
-    @PostMapping("/{id}")
-    @Operation(summary = "wishlist 만들기")
-    public ResponseEntity<List<WishlistItem>> createWishlist(@PathVariable("id") Long userId, @RequestBody List<WishlistItem> wishlistItems) {
-        List<WishlistItem> savedItems = wishlistService.saveWishlistItemsWithUserId(userId, wishlistItems);
-        return new ResponseEntity<>(savedItems, HttpStatus.CREATED);
+    @GetMapping
+    @Operation(summary = "위시리스트 상품조회")
+    public ResponseEntity<Page<WishlistResponse>> getWishlist(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "createdDate,desc") String sort) {
+        String[] sortParams = sort.split(",");
+        Sort.Direction direction = Sort.Direction.fromString(sortParams[1]);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortParams[0]));
+
+        Page<WishlistResponse> wishlist = wishlistService.getWishlistResponse(pageable);
+        return new ResponseEntity<>(wishlist, HttpStatus.OK);
+    }
+    @PostMapping
+    @Operation(summary = "위시리스트 상품 추가")
+    public ResponseEntity<WishlistResponse> addWish(@RequestBody WishlistRequest wishlistRequest){
+        try{
+            return new ResponseEntity<>(wishlistService.addWish(wishlistRequest), HttpStatus.OK);
+        }catch (IllegalArgumentException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{wishId}")
+    @Operation(summary = "위시리스트 상품 삭제")
+    public ResponseEntity<Void> deleteWish(@PathVariable("wishId") Long wishId){
+        wishlistService.deleteById(wishId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
