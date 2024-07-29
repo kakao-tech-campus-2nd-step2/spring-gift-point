f7ece929fa14e8b43c2605541d213bd5265cb3f6
dong_yxxn <g0326kdy@gmail.com>
2024-07-29 16:07:57 +0900

    feat(ProductRepository): categoryId로 조회기능 추가

--- src/main/java/gift/product/repository/ProductRepository.java
+++ src/main/java/gift/product/repository/ProductRepository.java
@@ -2,15 +2,22 @@ package gift.product.repository;
 
 import gift.category.domain.Category;
 import gift.product.domain.Product;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> getProductsByCategoryId(Long categoryId, Pageable pageable);
+
     @Modifying
     @Transactional
     @Query("UPDATE Product p SET p.name = :name, p.price = :price, p.imageUrl = :imageUrl, p.category = :category WHERE p.id = :id")
