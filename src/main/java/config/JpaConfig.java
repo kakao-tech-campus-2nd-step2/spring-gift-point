2b716f5669302c69af886e2274b1b885a767f705
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-29 15:28:22 +0900

    merge

--- src/main/java/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
--- src/main/java/config/OauthConfig.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(KakaoProperties.class)
-public class OauthConfig {
-}
