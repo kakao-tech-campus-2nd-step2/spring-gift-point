6a9c6c804b7ac0478ea33d42408597baf6ef1295
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-30 00:58:00 +0900

    feat: Slice 관련 ProductRepository 추가

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,12 +1,14 @@
 package gift.repository;
 
 import gift.entity.Product;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Product findByName(String name);
+    Slice<Product> findByCategoryId(Long categoryId, Pageable pageable);
 
 }
