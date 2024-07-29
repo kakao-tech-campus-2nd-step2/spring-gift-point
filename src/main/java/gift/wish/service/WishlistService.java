4b26d536e2c9957a5c7c68b41972e38df84664b2
dong_yxxn <g0326kdy@gmail.com>
2024-07-29 23:36:00 +0900

    feat: CRUD 기능 강화 - wishlistRequst 이용해서 addwish() - List<wishlistRequest> -> addWishes() - 기존에 wishlistItem 파라미터로 받는 함수 삭제 :

--- src/main/java/gift/wish/service/WishlistService.java
+++ src/main/java/gift/wish/service/WishlistService.java
@@ -1,16 +1,16 @@
 package gift.wish.service;
 
+import gift.product.domain.Product;
+import gift.product.repository.ProductRepository;
 import gift.product.service.ProductService;
-import gift.wish.domain.WishlistDTO;
+import gift.user.domain.User;
+import gift.user.repository.UserRepository;
+import gift.wish.domain.WishlistRequest;
 import gift.wish.domain.WishlistItem;
 import gift.wish.domain.WishlistResponse;
 import gift.wish.repository.WishlistRepository;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -22,61 +22,78 @@ public class WishlistService {
     private final WishlistRepository wishlistRepository;
     private final ProductService productService;
 
-    public WishlistService(WishlistRepository wishlistRepository, ProductService productService) {
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+
+    public WishlistService(WishlistRepository wishlistRepository, ProductService productService,
+        UserRepository userRepository, ProductRepository productRepository) {
         this.wishlistRepository = wishlistRepository;
         this.productService = productService;
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
     }
-
 
     public Page<WishlistItem> getWishlistByUserId(Long userId, Pageable pageable) {
         return wishlistRepository.findListByUserId(userId, pageable);
+    }
+    public Page<WishlistResponse> getWishlistResponse(Pageable pageable) {
+        Page<WishlistItem> wishlistItems = wishlistRepository.findAll(pageable);
+        List<WishlistResponse> wishlistResponses = wishlistItems.stream()
+            .map(this::convertEntityToResponse)
+            .toList();
+
+        return new PageImpl<>(wishlistResponses, pageable, wishlistItems.getTotalElements());
     }
     public Page<WishlistResponse> getWishlistResponseByUserId(Long userId, Pageable pageable){
         Page<WishlistItem> wishlistItems = wishlistRepository.findListByUserId(userId, pageable);
 
         List<WishlistResponse> wishlistResponses = wishlistItems.stream()
-            .map(item -> new WishlistResponse(
-                item.getId(),
-                item.getUser().getId(),
-                productService.convertToDTO(item.getProduct()),
-                item.getAmount()))
+            .map(this::convertEntityToResponse)
             .toList();
 
         return new PageImpl<>(wishlistResponses, pageable, wishlistItems.getTotalElements());
     }
-    public List<WishlistItem> saveWishlistItemsWithUserId(Long userId, List<WishlistItem> wishlistItems) {
-        List<WishlistItem> userWishlistItems = wishlistRepository.findListByUserId(userId);
-        if(!userWishlistItems.isEmpty()){
-            Map<Long, WishlistItem> userWishlistMap = convertListToMap(userWishlistItems);
-            userWishlistItems = mergeWishlist(wishlistItems, userWishlistMap);
-        }else{
-            userWishlistItems = wishlistItems;
-        }
-        return wishlistRepository.saveAll(userWishlistItems);
+    public void deleteById(Long wishId) {
+        wishlistRepository.deleteById(wishId);
     }
     public void deleteByUserIdProductId(Long userId, Long productId){
         wishlistRepository.deleteByUserIdProductId(userId, productId);
     }
-    private static Map<Long, WishlistItem> convertListToMap(List<WishlistItem> wishlistItems) {
-        Map<Long, WishlistItem> userWishlistMap = new HashMap<>();
-        for(WishlistItem wishlistItem : wishlistItems){
-            userWishlistMap.put(wishlistItem.getProduct().getId(), wishlistItem);
-        }
-        return userWishlistMap;
+    public WishlistResponse addWish(WishlistRequest wishlistRequest) {
+        User user = userRepository.findById(wishlistRequest.getUserId())
+            .orElseThrow(() -> new IllegalArgumentException(
+                "userId " + wishlistRequest.getUserId() + "가 없습니다."));
+        Product product = productRepository.findById(wishlistRequest.getProductId())
+            .orElseThrow(() -> new IllegalArgumentException(
+                "productId " + wishlistRequest.getProductId() + "가 없습니다."));
+        WishlistItem wishlistItem = wishlistRepository.save(new WishlistItem(user, product, wishlistRequest.getAmount()));
+        return convertEntityToResponse(wishlistItem);
+    }
+    public void addWishes(List<WishlistRequest> wishlistRequests){
+        List<WishlistItem> wishlistItems = convertRequestsToEntities(wishlistRequests);
+        wishlistRepository.saveAll(wishlistItems);
     }
 
-    private static List<WishlistItem> mergeWishlist(List<WishlistItem> wishlistItems,
-        Map<Long, WishlistItem> userWishlistMap) {
-        for (WishlistItem wishlistItem : wishlistItems) {
-            Long productId = wishlistItem.getProduct().getId();
-            if (userWishlistMap.containsKey(productId)) {
-                WishlistItem existingItem = userWishlistMap.get(productId);
-                existingItem.setAmount(existingItem.getAmount() + wishlistItem.getAmount());
-                userWishlistMap.put(productId, existingItem);
-            } else {
-                userWishlistMap.put(productId, wishlistItem);
-            }
+    private List<WishlistItem> convertRequestsToEntities(List<WishlistRequest> wishlistRequests) {
+        List<WishlistItem> wishlistItemList = new ArrayList<>();
+        for(WishlistRequest wishlistRequest : wishlistRequests){
+            WishlistItem wishlistItem = new WishlistItem();
+            wishlistItem.setUser(userRepository.findById(wishlistRequest.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("userId " + wishlistRequest.getUserId() + "가 없습니다.")));
+
+            Long productId = wishlistRequest.getProductId();
+            wishlistItem.setProduct(productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("productId " + productId + "가 없습니다.")));
+
+            wishlistItem.setAmount(wishlistRequest.getAmount());
+            wishlistItemList.add(wishlistItem);
         }
-        return new ArrayList<>(userWishlistMap.values());
+        return wishlistItemList;
     }
+
+    private WishlistResponse convertEntityToResponse(WishlistItem wishlistItem) {
+        return new WishlistResponse(wishlistItem.getId(), wishlistItem.getUser().getId(),
+            productService.convertToDTO(wishlistItem.getProduct()), wishlistItem.getAmount(), wishlistItem.getCreatedDate());
+    }
+
 }
