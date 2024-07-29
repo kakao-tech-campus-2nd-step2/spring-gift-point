7809c82bf4a98d0d5b0e90d335080cc912fa7415
dong_yxxn <g0326kdy@gmail.com>
2024-07-29 23:34:23 +0900

    feat: createdDate 필드 추가

--- src/main/java/gift/wish/domain/WishlistResponse.java
+++ src/main/java/gift/wish/domain/WishlistResponse.java
@@ -1,7 +1,8 @@
 package gift.wish.domain;
 
 import gift.product.domain.ProductDTO;
+import java.time.LocalDateTime;
 
-public record WishlistResponse(Long wishlistId, Long userId, ProductDTO productDTO, Long amount) {
+public record WishlistResponse(Long wishlistId, Long userId, ProductDTO productDTO, Long amount, LocalDateTime createdDate) {
 
 }
