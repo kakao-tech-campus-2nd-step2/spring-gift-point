26ccf9d97b08ff956b79878b4b402fcab6acc879
wvssm <neoramiregi@gmail.com>
2024-07-31 05:11:05 +0900

    feat(ProductResponse): toEntity 메서드 삭제

--- src/main/java/gift/dto/response/ProductResponse.java
+++ src/main/java/gift/dto/response/ProductResponse.java
@@ -2,12 +2,8 @@ package gift.dto.response;
 
 import gift.domain.Product;
 
-public record ProductResponse(Long id, String name, int price, String imageUrl, CategoryResponse categoryResponse) {
-    public static ProductResponse from(final Product product){
-        return new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), CategoryResponse.from(product.getCategory()));
-    }
-
-    public Product toEntity(){
-        return new Product(this.id, this.name, this.price, this.imageUrl, this.categoryResponse.toEntity());
+public record ProductResponse(Long id, String name, int price, String imageUrl, Long categoryId) {
+    public static ProductResponse from(final Product product) {
+        return new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), product.getCategory().getId());
     }
 }
