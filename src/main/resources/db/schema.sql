110cfbc431f75da9bdba1d84d68732102e34c4c1
stopmin <geemin0@naver.com>
2024-07-30 17:35:07 +0900

    feat: ddl validate - schema sql 수정

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE users
     password          VARCHAR(255) NOT NULL,
     profile_image_url VARCHAR(255),
     kakao_id          BIGINT,
+    login_type        VARCHAR(255),
     created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -20,11 +21,11 @@ CREATE TABLE wishlist
 
 CREATE TABLE category
 (
-    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    category_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
-    color       VARCHAR(7)   NOT NULL,
     description VARCHAR(255),
-    image_url   VARCHAR(255) NOT NULL
+    image_url   VARCHAR(255) NOT NULL,
+    color       VARCHAR(7)   NOT NULL
 );
 
 CREATE TABLE product
@@ -34,15 +35,17 @@ CREATE TABLE product
     price DOUBLE NOT NULL,
     image_url   VARCHAR(255) NOT NULL,
     category_id BIGINT       NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category (id)
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES category (category_id)
 );
 
 CREATE TABLE product_option
 (
-    product_option_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name              VARCHAR(255) NOT NULL,
-    product_id        BIGINT       NOT NULL,
-    quentity          BIGINT       NOT NULL,
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    product_id BIGINT       NOT NULL,
+    quantity   BIGINT       NOT NULL,
+
     FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
 
@@ -51,7 +54,21 @@ CREATE TABLE wishlist_product
     wishlist_product_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     wishlist_id         BIGINT NOT NULL,
     product_id          BIGINT NOT NULL,
-    quentity            BIGINT NOT NULL,
+    product_option_id   BIGINT,
+    quantity            BIGINT NOT NULL,
     FOREIGN KEY (wishlist_id) REFERENCES wishlist (wishlist_id),
-    FOREIGN KEY (product_id) REFERENCES product (product_id)
+    FOREIGN KEY (product_id) REFERENCES product (product_id),
+    FOREIGN KEY (product_option_id) REFERENCES product_option (id)
+);
+
+CREATE TABLE orders
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id         BIGINT NOT NULL,
+    option_id       BIGINT NOT NULL,
+    quantity        BIGINT NOT NULL,
+    message         VARCHAR(255),
+    order_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (option_id) REFERENCES product_option (id)
 );
