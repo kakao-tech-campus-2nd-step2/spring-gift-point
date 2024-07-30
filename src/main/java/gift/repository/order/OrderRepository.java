7853ead2f71254de8a37933fc170bc35e8e26439
JJY <ddglackrp@gmail.com>
2024-07-30 19:44:07 +0900

    feat "OrderRepository Page method 추가"

--- src/main/java/gift/repository/order/OrderRepository.java
+++ src/main/java/gift/repository/order/OrderRepository.java
@@ -1,7 +1,11 @@
 package gift.repository.order;
 
 import gift.domain.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Page<Order> findAllByMemberId(Pageable pageable, Long memberId);
 }
