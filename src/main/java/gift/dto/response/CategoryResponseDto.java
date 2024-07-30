aba6d9c61c5043126e540dabd977ef0297347f2e
JJY <ddglackrp@gmail.com>
2024-07-30 19:33:31 +0900

    feat "Category Dto 수정"

--- src/main/java/gift/dto/response/CategoryResponseDto.java
+++ src/main/java/gift/dto/response/CategoryResponseDto.java
@@ -2,14 +2,18 @@ package gift.dto.response;
 
 import gift.domain.Category;
 
-public record CategoryResponseDto(Long id, String name, String color) {
-
-    public static CategoryResponseDto of(Long id, String name, String color) {
-        return new CategoryResponseDto(id, name, color);
+public record CategoryResponseDto(Long id,
+                                  String name,
+                                  String color,
+                                  String imageUrl,
+                                  String description)
+{
+    public static CategoryResponseDto of(Long id, String name, String color, String imageUrl, String description) {
+        return new CategoryResponseDto(id, name, color, imageUrl, description);
     }
 
     public static CategoryResponseDto from(Category category) {
-        return new CategoryResponseDto(category.getId(), category.getName(), category.getColor());
+        return new CategoryResponseDto(category.getId(), category.getName(), category.getColor(), category.getImageUrl(), category.getDescription());
     }
 
 }
