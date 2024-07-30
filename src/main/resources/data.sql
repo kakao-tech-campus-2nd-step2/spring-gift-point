bf8f421df379c70b4e68bfc6a65364f5e2a88993
JinSeoHyun <15roa1121@gmail.com>
2024-07-31 00:47:22 +0900

    fix: 데이터 베이스 칼럼명 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO Category (name, color, imageUrl, description)
+INSERT INTO Category (name, color, image_url, description)
 VALUES ('패션', 'RED', 'http://example.com/fashion.jpg', '패션 관련 카테고리');
-INSERT INTO Category (name, color, imageUrl, description)
+INSERT INTO Category (name, color, image_url, description)
 VALUES ('뷰티', 'PINK', 'http://example.com/beauty.jpg', '뷰티 관련 카테고리');
-INSERT INTO Category (name, color, imageUrl, description)
+INSERT INTO Category (name, color, image_url, description)
 VALUES ('식품', 'GREEN', 'http://example.com/food.jpg', '식품 관련 카테고리');
-INSERT INTO Category (name, color, imageUrl, description)
+INSERT INTO Category (name, color, image_url, description)
 VALUES ('디지털/가전', 'BLUE', 'http://example.com/digital.jpg', '디지털 및 가전제품 관련 카테고리');
-INSERT INTO Category (name, color, imageUrl, description)
+INSERT INTO Category (name, color, image_url, description)
 VALUES ('인테리어/그릇', 'BROWN', 'http://example.com/interior.jpg', '인테리어 및 그릇 관련 카테고리');
-INSERT INTO Category (name, color, imageUrl, description)
+INSERT INTO Category (name, color, image_url, description)
 VALUES ('출산/유아동', 'YELLOW', 'http://example.com/baby.jpg', '출산 및 유아동 관련 카테고리');
 
 
