d90373f4060a30686e886424362dd9befa51b3ee
wvssm <neoramiregi@gmail.com>
2024-07-31 04:56:05 +0900

    feat(CategoryRestController): CommonResponse으로 응답값 감싸기

--- src/main/java/gift/controller/CategoryRestController.java
+++ src/main/java/gift/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package gift.controller;
 
 import gift.dto.request.CategoryRequest;
 import gift.dto.response.CategoryResponse;
+import gift.dto.response.CommonResponse;
 import gift.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -27,29 +28,29 @@ public class CategoryRestController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "ID로 카테고리를 수정합니다")
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateCategoryById(@PathVariable Long id, @RequestBody CategoryRequest categoryRequest){
+        categoryService.updateById(id,categoryRequest);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Operation(summary = "모든 카테고리를 조회합니다")
+    @GetMapping
+    public ResponseEntity<CommonResponse> getAllCategories(){
+        return ResponseEntity.status(HttpStatus.OK).body(new CommonResponse(categoryService.findAll(), "모든 카테고리 조회 성공", true));
+    }
+
     @Operation(summary = "ID로 카테고리를 조회합니다")
     @GetMapping("/{id}")
     public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(categoryService.findById(id));
     }
 
-    @Operation(summary = "모든 카테고리를 조회합니다")
-    @GetMapping
-    public ResponseEntity<List<CategoryResponse>> getAllCategories(){
-        return ResponseEntity.status(HttpStatus.OK).body(categoryService.findAll());
-    }
-
     @Operation(summary = "ID로 카테고리를 삭제합니다")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCategoryById(@PathVariable Long id){
         categoryService.deleteById(id);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-    @Operation(summary = "ID로 카테고리 정보를 업데이트합니다")
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateCategoryById(@PathVariable Long id, @RequestBody CategoryRequest categoryRequest){
-        categoryService.updateById(id,categoryRequest);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
