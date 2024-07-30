27a99f0fe063b24b3384ea52c6e6f82084ae83db
momnpa333 <momnpa333@gmail.com>
2024-07-30 19:53:55 +0900

    feat: page response 수정

--- src/main/java/gift/global/dto/PageResponse.java
+++ src/main/java/gift/global/dto/PageResponse.java
@@ -8,8 +8,8 @@ public record PageResponse<T>(
     List<T> content,
     Integer page,
     Integer size,
-    Integer totalPage,
-    Integer totalSize
+    Integer totalPages,
+    Integer totalElements
 ) {
 
     public static <T, U> PageResponse<T> from(Page<U> page, Function<U, T> mapper) {
