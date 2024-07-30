d4895ea73c4b108c4558014ac00a11eadc466cf0
dong_yxxn <g0326kdy@gmail.com>
2024-07-30 15:57:35 +0900

    refactor: 이름 변경 및 kakao 로그인 환경 제공

--- src/main/java/gift/kakao/login/controller/KakaoLoginController.java
+++ src/main/java/gift/kakao/login/controller/KakaoLoginController.java
@@ -1,24 +1,35 @@
 package gift.kakao.login.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/login")
-public class KakaoLoginViewController {
+@RestController
+@RequestMapping("/api/members")
+@Tag(name = "회원 API")
+public class KakaoLoginController {
     @Value("${kakao.client-id}")
     private String clientId;
 
     @Value("${kakao.redirect-uri}")
     private String redirectUri;
 
-    @GetMapping
-    public String showLoginPageTest(Model model){
-        model.addAttribute("restApiKey", clientId);
-        model.addAttribute("redirectUri", redirectUri);
-        return "kakao_login";
+    @GetMapping("/kakao")
+    public void showKakaoLogin(HttpServletResponse response) throws IOException {
+        String uri = "https://kauth.kakao.com/oauth/authorize?scope=talk_message&response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri;
+        response.sendRedirect(uri);
+    }
+
+    @PostMapping("/kakao")
+    @Operation(summary = "카카오 로그인")
+    public void handleKakaoLoginPost(HttpServletResponse response) throws IOException {
+        String redirectUri = "/api/members/kakao";
+        response.sendRedirect(redirectUri);
     }
 }
