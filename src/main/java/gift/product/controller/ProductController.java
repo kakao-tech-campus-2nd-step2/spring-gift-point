84e57f9a7f9f3dcbb8e60b4226d2f684118a9b7f
dong_yxxn <g0326kdy@gmail.com>
2024-07-31 00:01:41 +0900

    refactor: uri 매칭 수정

--- src/main/java/gift/product/controller/ProductController.java
+++ src/main/java/gift/product/controller/ProductController.java
@@ -1,32 +1,22 @@
 package gift.product.controller;
 
-import gift.product.option.domain.Option;
 import gift.product.option.domain.OptionDTO;
 import gift.product.option.service.OptionService;
 import gift.product.domain.ProductDTO;
 import gift.product.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -44,27 +34,25 @@ public class ProductController {
     @Operation(summary = "상품 생성", tags = {"상품 API"})
     public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDTO) {
         ProductDTO response = productService.createProduct(productDTO);
-        optionService.saveAllwithProductId(productDTO.getOptionDTOList(), response.getId());
-        return new ResponseEntity<>(response,HttpStatus.CREATED);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("")
-    @Operation(summary = "상품목록조회", tags = {"상품 API"})
+    @Operation(summary = "상품 목록 조회", tags = {"상품 API"})
     public ResponseEntity<Page<ProductDTO>> getAllProductsWishCategoryId(
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "10") int size,
         @RequestParam(defaultValue = "name,asc") String sort,
-        @RequestParam(required = false) Long categoryId){
+        @RequestParam(required = false) Long categoryId) {
 
         String[] sortParams = sort.split(",");
         Sort.Direction direction = Sort.Direction.fromString(sortParams[1]);
         Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortParams[0]));
 
         Page<ProductDTO> products;
-
-        if(categoryId != null){
+        if (categoryId != null) {
             products = productService.getAllProductsByCategoryId(categoryId, pageable);
-        }else{
+        } else {
             products = productService.getAllProducts(pageable);
         }
         return new ResponseEntity<>(products, HttpStatus.OK);
@@ -78,7 +66,6 @@ public class ProductController {
             .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-
     @PutMapping("/{productId}")
     @Operation(summary = "상품 수정", tags = {"상품 API"})
     public ResponseEntity<ProductDTO> updateProduct(@PathVariable("productId") Long id, @Valid @RequestBody ProductDTO productDTO) {
@@ -86,9 +73,9 @@ public class ProductController {
         if (existingProductDTO.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        try{
+        try {
             return new ResponseEntity<>(productService.updateProduct(id, productDTO), HttpStatus.OK);
-        }catch(EntityNotFoundException e){
+        } catch (EntityNotFoundException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -102,42 +89,39 @@ public class ProductController {
         productService.deleteProduct(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-    // option
-    @GetMapping("{productId}/options")
-    @Operation(summary = "상품옵션 목록 조회", tags = {"상품 옵션 API"})
-    public ResponseEntity<List<OptionDTO>> getOptionList(@PathVariable Long productId){
-        return new ResponseEntity<>(optionService.findAllByProductId(productId),HttpStatus.OK);
+
+    // Option 관련 메서드
+    @GetMapping("/{productId}/options")
+    @Operation(summary = "상품 옵션 목록 조회", tags = {"상품 옵션 API"})
+    public ResponseEntity<List<OptionDTO>> getOptionList(@PathVariable Long productId) {
+        return new ResponseEntity<>(optionService.findAllByProductId(productId), HttpStatus.OK);
     }
 
-    @PostMapping("{productId}/options")
+    @PostMapping("/{productId}/options")
     @Operation(summary = "상품 옵션 추가", tags = {"상품 옵션 API"})
-    public ResponseEntity<OptionDTO> addOption(@PathVariable Long productId,
-        @RequestBody OptionDTO optionDTO){
-        try{
+    public ResponseEntity<OptionDTO> addOption(@PathVariable Long productId, @RequestBody OptionDTO optionDTO) {
+        try {
             optionDTO = optionService.addOption(productId, optionDTO);
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(optionDTO);
     }
 
-    @PutMapping("{productId}/options/{optionId}")
+    @PutMapping("/{productId}/options/{optionId}")
     @Operation(summary = "상품 옵션 수정", tags = {"상품 옵션 API"})
-    public ResponseEntity<OptionDTO> updateOption(@PathVariable Long productId,
-        @PathVariable Long optionId,
-        @RequestBody OptionDTO optionDTO){
-        try{
+    public ResponseEntity<OptionDTO> updateOption(@PathVariable Long productId, @PathVariable Long optionId, @RequestBody OptionDTO optionDTO) {
+        try {
             optionDTO = optionService.updateOption(productId, optionId, optionDTO);
             return new ResponseEntity<>(optionDTO, HttpStatus.OK);
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
-    @DeleteMapping("{productId}/options/{optionId}")
+    @DeleteMapping("/{productId}/options/{optionId}")
     @Operation(summary = "상품 옵션 삭제", tags = {"상품 옵션 API"})
-    public ResponseEntity<Void> deleteOption(@PathVariable Long productId,
-        @PathVariable Long optionId){
+    public ResponseEntity<Void> deleteOption(@PathVariable Long productId, @PathVariable Long optionId) {
         optionService.deleteOption(productId, optionId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
