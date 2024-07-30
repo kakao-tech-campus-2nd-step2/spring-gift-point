1b255c06763c26263d8634b8879737ec87b7a85a
dong_yxxn <g0326kdy@gmail.com>
2024-07-30 15:58:37 +0900

    delete: 필요없어져서 주석 처리

--- src/main/java/gift/user/controller/UserController.java
+++ src/main/java/gift/user/controller/UserController.java
@@ -21,9 +21,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/users")
+//@RestController
+//@RequestMapping("/api/users")
 public class UserController {
+    /*
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -94,4 +95,6 @@ public class UserController {
         userService.deleteUser(id);
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
+
+     */
 }
