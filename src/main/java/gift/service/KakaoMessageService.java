2c506ba1586a03abc1a0c867f3d03e5f1a627280
ez23re <ez23re@gmail.com>
2024-07-31 02:14:33 +0900

    메시지서비스 : ErrorHandler로 수정

--- src/main/java/gift/service/KakaoMessageService.java
+++ src/main/java/gift/service/KakaoMessageService.java
@@ -7,12 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.ResponseErrorHandler;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,53 +32,92 @@ public class KakaoMessageService {
     public KakaoMessageService(RestTemplate kakaoRestTemplate) {
         this.kakaoRestTemplate = kakaoRestTemplate;
         this.objectMapper = new ObjectMapper();
+        this.kakaoRestTemplate.setErrorHandler(new CustomResponseErrorHandler());
     }
 
     public void sendMessage(OrderResponse order, String accessToken) {
-        String message = String.format("OptionId: %d\nQuantity: %d\nOrder Date: %s\nMessage: %s",
+        try {
+            String message = createMessage(order);  // 주문 정보로부터 메시지 생성
+            String templateObjectJson = createTemplateObjectJson(message);  // 메시지 JSON 생성
+
+            HttpEntity<MultiValueMap<String, String>> request = createRequestEntity(templateObjectJson, accessToken);  // 요청 엔티티 생성
+            ResponseEntity<String> response = sendKakaoRequest(request);  // 카카오 요청 전송
+
+            handleResponse(response);  // 응답 처리
+        } catch (JsonProcessingException e) {
+            logger.error("JSON 처리 중 오류 발생: {}", e.getMessage());
+            throw new RuntimeException("template_object를 JSON으로 변환하는 중 오류가 발생했습니다.", e);
+        }
+    }
+
+    // 주문 정보로부터 메시지 생성
+    private String createMessage(OrderResponse order) {
+        return String.format("OptionId: %d\nQuantity: %d\nOrder Date: %s\nMessage: %s",
                 order.getOptionId(), order.getQuantity(), order.getOrderDateTime().toString(), order.getMessage());
+    }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessToken);
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
+    // 메시지 JSON 생성
+    private String createTemplateObjectJson(String message) throws JsonProcessingException {
         Map<String, String> link = new HashMap<>();
         link.put("web_url", "http://www.example.com");
         link.put("mobile_web_url", "http://www.example.com");
 
         KakaoMessageTemplate templateObject = new KakaoMessageTemplate("text", message, link);
+        String templateObjectJson = objectMapper.writeValueAsString(templateObject);
+        logger.info("Template Object JSON: {}", templateObjectJson);
 
-        try {
-            String templateObjectJson = objectMapper.writeValueAsString(templateObject);
-            logger.info("Template Object JSON: {}", templateObjectJson);
+        return templateObjectJson;
+    }
 
-            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-            body.add("template_object", templateObjectJson);
+    // 요청 엔티티 생성
+    private HttpEntity<MultiValueMap<String, String>> createRequestEntity(String templateObjectJson, String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("template_object", templateObjectJson);
 
-            ResponseEntity<String> response;
-            try {
-                logger.info("Sending Kakao message with access token: {}", accessToken);
-                response = kakaoRestTemplate.postForEntity(messageUrl, request, String.class);
-                logger.info("Kakao message response: {}", response);
-            } catch (HttpClientErrorException e) {
-                logger.error("Failed to send Kakao message", e);
-                logger.error("HTTP Status: {}", e.getStatusCode());
-                logger.error("Response Body: {}", e.getResponseBodyAsString());
-                throw new RuntimeException("카카오 메시지 전송 중 예외가 발생했습니다.: " + e.getMessage(), e);
-            }
+        return new HttpEntity<>(body, headers);
+    }
 
-            if (response.getStatusCode() != HttpStatus.OK) {
-                logger.error("Kakao message response body: {}", response.getBody());
-                throw new RuntimeException("카카오 메시지 전송에 실패했습니다.: " + response.getBody());
-            }
-        } catch (JsonProcessingException e) {
-            logger.error("JSON 처리 중 오류 발생: {}", e.getMessage());
-            throw new RuntimeException("template_object를 JSON으로 변환하는 중 오류가 발생했습니다.", e);
-        } catch (Exception e) {
-            logger.error("예상치 못한 오류 발생: {}", e.getMessage());
-            throw new RuntimeException("예상치 못한 오류가 발생했습니다.", e);
+    // 카카오 요청 전송
+    private ResponseEntity<String> sendKakaoRequest(HttpEntity<MultiValueMap<String, String>> request) {
+        logger.info("Sending Kakao message with access token");
+        return kakaoRestTemplate.postForEntity(messageUrl, request, String.class);
+    }
+
+    // 응답 처리
+    private void handleResponse(ResponseEntity<String> response) {
+        if (response.getStatusCode() != HttpStatus.OK) {
+            logger.error("Kakao message response body: {}", response.getBody());
+            throw new RuntimeException("카카오 메시지 전송에 실패했습니다.: " + response.getBody());
+        }
+        logger.info("Kakao message response: {}", response);
+    }
+
+    private static class CustomResponseErrorHandler implements ResponseErrorHandler {
+        private final Logger logger = LoggerFactory.getLogger(CustomResponseErrorHandler.class);
+
+        @Override
+        public boolean hasError(ClientHttpResponse response) throws IOException {
+            return response.getStatusCode().isError();
+        }
+
+        @Override
+        public void handleError(ClientHttpResponse response) throws IOException {
+            HttpStatus statusCode = HttpStatus.resolve(response.getStatusCode().value());
+
+            Map<HttpStatus, String> errorMessages = Map.of(
+                    HttpStatus.BAD_REQUEST, "Bad Request",
+                    HttpStatus.NOT_FOUND, "Not Found",
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"
+            );
+
+            String errorMessage = errorMessages.getOrDefault(statusCode, "Unexpected error occurred");
+            String responseBody = new String(response.getBody().readAllBytes());
+            logger.error("Failed to send Kakao message, HTTP Status: {}, Response Body: {}", statusCode, responseBody);
+            throw new RuntimeException("카카오 메시지 전송 중 예외가 발생했습니다.: " + errorMessage + ": " + responseBody);
         }
     }
 }
