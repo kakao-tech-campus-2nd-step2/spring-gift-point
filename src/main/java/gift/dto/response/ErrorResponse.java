a0870ef48e8a068a74a4a240ce6990f783deab20
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-07-30 23:29:09 +0900

    refactor : 회원 API 명세서에 따른 리팩토링

--- src/main/java/gift/dto/response/ErrorResponse.java
+++ src/main/java/gift/dto/response/ErrorResponse.java
@@ -1,7 +1,10 @@
 package gift.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+<<<<<<< HEAD
 import com.fasterxml.jackson.annotation.JsonCreator;
+=======
+>>>>>>> 08d440e (refactor : 회원 API 명세서에 따른 리팩토링)
 
 public class ErrorResponse {
     
@@ -11,6 +14,7 @@ public class ErrorResponse {
     @JsonProperty("error_code")
     private int code;
 
+<<<<<<< HEAD
     @JsonCreator
     public ErrorResponse(
         @JsonProperty("message")
@@ -18,6 +22,9 @@ public class ErrorResponse {
 
         @JsonProperty("error_code")
         int code){
+=======
+    public ErrorResponse(String message, int code){
+>>>>>>> 08d440e (refactor : 회원 API 명세서에 따른 리팩토링)
         this.message = message;
         this.code = code;
     }
