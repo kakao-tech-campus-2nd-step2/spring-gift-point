b1ad75f90221fe4585adf92f413e19a467501cca
pykido <animoto1@naver.com>
2024-07-30 13:09:53 +0900

    feat : 회원 API 구현 완료

--- src/main/java/gift/member/dto/SignUpReqeust.java
+++ src/main/java/gift/member/dto/SignUpReqeust.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class LoginRequest {
-    @NotBlank(message = "이메일은 필수 항목입니다.")
+public class SignUpReqeust {
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식이 아닙니다.")
     String email;
 
