b72002688a6e07da5ce77a223097bf4291bcbc2d
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-30 13:39:40 +0900

    refactor: UpdateProductRequest remove id beacuase Path Id var

--- src/main/java/gift/dto/request/UpdateProductRequest.java
+++ src/main/java/gift/dto/request/UpdateProductRequest.java
@@ -7,8 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateProductRequest(
-        @NotNull
-        Long id,
         @NotBlank
         @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
         @NotContainsKakao(message = "상품 이름에 `카카오`가 포함된 문구는 담당 MD와 협의한 경우에만 사용 가능합니다.")
