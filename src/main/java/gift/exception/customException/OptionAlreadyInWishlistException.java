d1bc64e90a5c073662a2b0f6810e22a1cfc55ce2
wvssm <neoramiregi@gmail.com>
2024-07-31 05:02:55 +0900

    delete(OptionAlreadyInWishlistException): 사용하지 않는 exception 삭제

--- src/main/java/gift/exception/customException/OptionAlreadyInWishlistException.java
+++
@@ -1,7 +0,0 @@
-package gift.exception.customException;
-
-public class OptionAlreadyInWishlistException extends RuntimeException {
-    public OptionAlreadyInWishlistException(String message) {
-        super(message);
-    }
-}
