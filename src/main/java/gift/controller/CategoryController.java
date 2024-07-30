29220ed489a59ca1d6e80ac70a2c5ad18353971d
hyunseo <wndlthsk@naver.com>
2024-07-31 00:55:10 +0900

    refactor : 카테고리 API 수정

--- src/main/java/gift/controller/CategoryController.java
+++ src/main/java/gift/controller/CategoryController.java
@@ -8,9 +8,11 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,27 +28,27 @@ public class CategoryController {
     }
 
     @GetMapping
-    @Operation(summary = "전체 카테고리 조회", description = "전체 카테고리를 조회합니다.")
+    @Operation(summary = "카테고리 목록 조회", description = "모든 카테고리의 목록을 조회한다.")
     public ResponseEntity<List<CategoryDto>> getAllCategories(){
         return ResponseEntity.ok(categoryService.getAllCategories());
     }
 
-    @PostMapping("/add")
-    @Operation(summary = "카테고리 저장", description = "카테고리를 저장합니다.")
+    @PostMapping
+    @Operation(summary = "카테고리 생성", description = "새 카테고리를 등록한다.")
     public ResponseEntity<String> addCategory(@Valid @RequestBody CategoryDto categoryDto) {
         String category = categoryService.addCategory(categoryDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("추가된 카테고리: " + category);
     }
 
-    @PostMapping("/update/{categoryId}")
-    @Operation(summary = "카테고리 수정", description = "카테고리를 수정합니다.")
+    @PutMapping("/{categoryId}")
+    @Operation(summary = "카테고리 수정", description = "기존 카테고리를 수정한다.")
     public ResponseEntity<CategoryDto> updateProduct(@PathVariable("categoryId") Long id, @Valid @RequestBody CategoryDto categoryDto) {
         CategoryDto category = categoryService.updateCategory(id, categoryDto);
         return ResponseEntity.status(HttpStatus.OK).body(category);
     }
 
-    @PostMapping("/delete/{categoryId}")
-    @Operation(summary = "카테고리 삭제", description = "카테고리를 삭제합니다.")
+    @DeleteMapping("/{categoryId}")
+    @Operation(summary = "카테고리 삭제", description = "카테고리를 삭제한다.")
     public ResponseEntity<String> deleteWish(@PathVariable("categoryId") Long categoryId) {
         String category = categoryService.deleteCategory(categoryId);
         return ResponseEntity.status(HttpStatus.OK).body("삭제된 카테고리: " + category);
