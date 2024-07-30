2a93b12ed357dd0d22758f108d1be563e4d5bfd3
JinSeoHyun <15roa1121@gmail.com>
2024-07-30 23:38:33 +0900

    feat(WishProductRepository) : 프로덕트 아이디와 유저 아이디로 위시리스트 반환 메서드 추가

--- src/main/java/gift/main/repository/WishProductRepository.java
+++ src/main/java/gift/main/repository/WishProductRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WishProductRepository extends JpaRepository<WishProduct, Long> {
@@ -15,6 +16,8 @@ public interface WishProductRepository extends JpaRepository<WishProduct, Long> 
     boolean existsAllByUserId(Long userId);
 
     boolean existsByProductIdAndUserId(Long productId, Long userId);
+
+    Optional<WishProduct> findByProductIdAndUserId(Long productId, Long userId);
 
     Page<WishProduct> findAllByUserId(Long userId, Pageable pageable);
 
