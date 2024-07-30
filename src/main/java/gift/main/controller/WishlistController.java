a36ecc9131d74e781a9fde6aeaf7887a93bc042d
JinSeoHyun <15roa1121@gmail.com>
2024-07-30 22:39:02 +0900

    refactor(WishlistController): API 문서에 맞춰서 URL과 요청 객체, 응답 객체 수정

--- src/main/java/gift/main/controller/WishlistController.java
+++ src/main/java/gift/main/controller/WishlistController.java
@@ -1,16 +1,16 @@
 package gift.main.controller;
 
 import gift.main.annotation.SessionUser;
+import gift.main.dto.ProductId;
 import gift.main.dto.UserVo;
 import gift.main.dto.WishProductResponse;
 import gift.main.service.WishProductService;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/wishes")
 public class WishlistController {
 
     private final WishProductService wishProductService;
@@ -19,24 +19,25 @@ public class WishlistController {
         this.wishProductService = wishProductService;
     }
 
-    @DeleteMapping("/wishlist/{productId}")
-    @Transactional
-    public ResponseEntity<?> deleteWishProduct(@PathVariable(name = "productId") Long productId, @SessionUser UserVo sessionUserVo) {
-        wishProductService.deleteProducts(productId, sessionUserVo);
-        return ResponseEntity.ok("successfully deleted the item to your wishlist");
-    }
-
-    @GetMapping("/wishlists")
-    public ResponseEntity<?> getWishProductPage(@SessionUser UserVo sessionUserVo, Pageable pageable) {
-        Page<WishProductResponse> wishProductPage = wishProductService.getWishProductPage(sessionUserVo, pageable);
+    //특정 페이지의 위시 목록 조회
+    @GetMapping()
+    public ResponseEntity<?> getWishProductPage(@RequestParam(value = "page") int pageNum, @SessionUser UserVo sessionUserVo) {
+        Page<WishProductResponse> wishProductPage = wishProductService.getWishProductPage(sessionUserVo, pageNum);
         return ResponseEntity.ok(wishProductPage);
     }
 
-    @Transactional
-    @PostMapping("/wishlist/{productId}")
-    public ResponseEntity<?> addWishlistProduct(@PathVariable(name = "productId") Long productId, @SessionUser UserVo sessionUser) {
-        wishProductService.addWishlistProduct(productId, sessionUser);
+    //새로운 위시 추가
+    @PostMapping()
+    public ResponseEntity<?> addWishlistProduct(@RequestBody ProductId productId, @SessionUser UserVo sessionUser) {
+        wishProductService.addWishlistProduct(productId.productId(), sessionUser);
         return ResponseEntity.ok("successfully added the item to your wishlist");
+    }
+
+    //위시 삭제
+    @DeleteMapping("/{wish_id}")
+    public ResponseEntity<?> deleteWishProduct(@PathVariable(name = "wish_id") Long wishId, @SessionUser UserVo sessionUserVo) {
+        wishProductService.deleteWishProduct(wishId);
+        return ResponseEntity.ok("successfully deleted the item to your wishlist");
     }
 
 }
