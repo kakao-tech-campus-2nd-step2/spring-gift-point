457b8d355a91e14026dfade3dc727ebb472abe94
Kangboom <jkl6147@naver.com>
2024-07-30 13:52:15 +0900

    feat: findAllByMember 추가

--- src/main/java/gift/domain/order/repository/OrderRepository.java
+++ src/main/java/gift/domain/order/repository/OrderRepository.java
@@ -1,11 +1,14 @@
 package gift.domain.order.repository;
 
 
+import gift.domain.member.entity.Member;
 import gift.domain.order.entity.Orders;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, Long> {
-
+    Page<Orders> findAllByMember(Pageable pageable, Member member);
 }
