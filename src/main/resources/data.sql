708deebbdf08f4921bd50952b38555d773db688c
dong_yxxn <g0326kdy@gmail.com>
2024-07-30 17:56:54 +0900

    feat: 옵션 자동 insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (name) VALUES ('교환권');
+INSERT INTO categories (name) VALUES ('상품권');
+INSERT INTO categories (name) VALUES ('뷰티');
+INSERT INTO categories (name) VALUES ('식품');
