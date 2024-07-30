1bd5286226fa1776e3b1ab3f406cf7479a2a87a1
ez23re <ez23re@gmail.com>
2024-07-31 01:47:10 +0900

    KakaoClient : 에러메시지 매핑

--- src/main/java/gift/client/KakaoClient.java
+++ src/main/java/gift/client/KakaoClient.java
@@ -9,27 +9,35 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class KakaoClient {
 
     private final RestTemplate restTemplate;
+    private final Map<HttpStatus, String> errorMessages;
 
     public KakaoClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
         this.restTemplate.setErrorHandler(errorHandler());
+
+        errorMessages = new HashMap<>();
+        errorMessages.put(HttpStatus.BAD_REQUEST, "Bad Request");
+        errorMessages.put(HttpStatus.NOT_FOUND, "Not Found");
+        errorMessages.put(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
     }
 
     private ResponseErrorHandler errorHandler() {
         return new DefaultResponseErrorHandler() {
             @Override
             public void handleError(ClientHttpResponse response) throws IOException {
-                if (response.getStatusCode() == HttpStatus.BAD_REQUEST) {
-                    throw new ApiRequestException("Bad Request");
-                } else if (response.getStatusCode() == HttpStatus.NOT_FOUND) {
-                    throw new ApiRequestException("Not Found");
-                } else if (response.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
-                    throw new ApiRequestException("Internal Server Error");
+                HttpStatus statusCode = HttpStatus.valueOf(response.getStatusCode().value());
+                String statusText = response.getStatusText();
+
+                String errorMessage = errorMessages.get(statusCode);
+                if (errorMessage != null) {
+                    throw new ApiRequestException(errorMessage + ": " + statusText);
                 } else {
                     super.handleError(response);
                 }
