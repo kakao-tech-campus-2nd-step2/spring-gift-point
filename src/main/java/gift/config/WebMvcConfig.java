992f47d9305b53fa9f7faf65fad09d5c5ef374f4
HyeJiJUN <jhj910421@gmail.com>
2024-07-30 18:47:37 +0900

    refactor: change end point in WebMvcConfig

--- src/main/java/gift/config/WebMvcConfig.java
+++ src/main/java/gift/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor)
-                .addPathPatterns("/wishlist/**", "/KakaoOrder/**")
+                .addPathPatterns("/api/wishes/**", "/api/orders/**")
                 .excludePathPatterns("/member/**", "/products/**");
     }
 }
