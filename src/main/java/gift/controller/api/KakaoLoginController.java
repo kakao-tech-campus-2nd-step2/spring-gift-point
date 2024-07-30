2a65cb594c7f9160a4641f88e425bc423f417531
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-30 11:45:18 +0900

    feat: kakaologin `redirect` add

--- src/main/java/gift/controller/api/KakaoLoginController.java
+++ src/main/java/gift/controller/api/KakaoLoginController.java
@@ -5,10 +5,15 @@ import gift.dto.response.KakaoTokenResponse;
 import gift.service.KakaoApiService;
 import gift.service.MemberService;
 import gift.service.TokenService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class KakaoLoginController {
@@ -16,6 +21,8 @@ public class KakaoLoginController {
     private final MemberService memberService;
     private final TokenService tokenService;
     private final KakaoApiService kakaoApiService;
+    @Value("authorizationCode.request.uri")
+    private String authorizationCodeRequestUri;
 
     public KakaoLoginController(MemberService memberService, TokenService tokenService, KakaoApiService kakaoApiService) {
         this.memberService = memberService;
@@ -34,5 +41,13 @@ public class KakaoLoginController {
         tokenService.saveKakaoAccessToken(memberId, kakaoToken);
 
         return ResponseEntity.ok().body(JwtResponse);
+    }
+
+    @GetMapping("/kakaologin")
+    public ResponseEntity<Void> redirect() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(authorizationCodeRequestUri));
+
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
