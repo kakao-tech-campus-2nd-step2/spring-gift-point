a9d352518800013335a00ddab634d70512ecac0b
hyunseo <wndlthsk@naver.com>
2024-07-30 17:43:52 +0900

    refactor : @EnableAsync 추가

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -3,9 +3,11 @@ package gift;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
