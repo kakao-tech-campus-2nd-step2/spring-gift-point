c684c77eae8e4b1179316bbcfd1eac3b30beeb9a
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-07-30 14:54:00 +0900

    update: 서버에서도 요청은 localhost이다

--- src/main/java/gift/util/url/ServerUrl.java
+++ src/main/java/gift/util/url/ServerUrl.java
@@ -2,5 +2,5 @@ package gift.util.url;
 
 public class ServerUrl {
 
-    public static String commonUrl = "http://13.125.209.244:8080";
+    public static String commonUrl = "http://localhost:8080";
 }
