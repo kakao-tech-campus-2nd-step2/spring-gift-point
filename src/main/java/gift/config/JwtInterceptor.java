011aa6bf3208d933b81bb4e05c96ce23bf2f1657
Profile-exe <kang67572346@gmail.com>
2024-08-01 00:13:00 +0900

    feat(JwtInterceptor): preflight 요청 허용

--- src/main/java/gift/config/JwtInterceptor.java
+++ src/main/java/gift/config/JwtInterceptor.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -21,6 +22,15 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        if (CorsUtils.isCorsRequest(request)) {
+            return true;
+        }
+
+        if (CorsUtils.isPreFlightRequest(request)) {
+            return true;
+        }
+
         if (handler instanceof HandlerMethod handlerMethod) {   // @AllowAnonymous 어노테이션을 붙이면 세션 검증을 하지 않음
             if (handlerMethod.getMethodAnnotation(AllowAnonymous.class) != null) {
                 return true;
