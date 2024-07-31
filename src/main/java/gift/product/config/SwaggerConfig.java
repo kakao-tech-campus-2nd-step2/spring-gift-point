8f629c79347ac180100e0d6ff6e553820b118f08
배민수 <duecost@gmail.com>
2024-07-31 20:19:43 +0900

    feat: swagger ui에서 헤더 엑세스 토큰을 전역적으로 셋팅할 수 있도록 기능 추가

--- src/main/java/gift/product/config/SwaggerConfig.java
+++ src/main/java/gift/product/config/SwaggerConfig.java
@@ -2,11 +2,15 @@ package gift.product.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import java.util.Set;
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +23,21 @@ import org.springframework.context.annotation.Configuration;
     version = "1.0.0"
 ))
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api() {
+        SecurityScheme apiKey = new SecurityScheme()
+            .type(SecurityScheme.Type.APIKEY)
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+            .addList("Bearer Token");
+
+        return new OpenAPI()
+            .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+            .addSecurityItem(securityRequirement);
+    }
 
     @Bean
     public OpenApiCustomizer customAuthParameter() {
