4409befa7c86afa543962bfcbd45e1bcfb4284d6
dong_yxxn <g0326kdy@gmail.com>
2024-07-31 19:27:01 +0900

    refactor: joinColumn 추가

--- src/main/java/gift/order/domain/Order.java
+++ src/main/java/gift/order/domain/Order.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
@@ -24,6 +25,7 @@ public class Order {
     @NotNull
     String message;
     @ManyToOne
+    @JoinColumn(name = "option_id", nullable = false)
     Option option;
 
     public Order() {
