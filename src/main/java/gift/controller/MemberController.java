e3ea753c7241a02fa913b8f9e60510bf6aae690e
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-31 14:36:38 +0900

    refactor: 멤버 api url 변경

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api//members")
+@RequestMapping("/api")
 public class MemberController {
 
     private final MemberService memberService;
