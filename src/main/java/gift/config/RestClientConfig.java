62b4d3440a6512fcbeb0e5ac9dc5564b11386eda
hjinshin <gudwls818@gmail.com>
2024-07-31 21:07:33 +0900

    feat: COnfiguration에서 RestClient 설정 Bean으로 등록

---
+++ src/main/java/gift/config/RestClientConfig.java
@@ -0,0 +1,24 @@
+package gift.config;
+
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestClient restClient() {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withReadTimeout(Duration.ofSeconds(2))
+                .withConnectTimeout(Duration.ofSeconds(5));
+
+        return RestClient.builder()
+                .requestFactory(ClientHttpRequestFactories.get(settings))
+                .build();
+    }
+}
