0ce47fb5026041cb480520fc6b904fe19e6417f0
Kangboom <jkl6147@naver.com>
2024-07-31 21:34:22 +0900

    feat: swagger 기능 추가

--- src/main/java/gift/util/dto/JwtResponse.java
+++ src/main/java/gift/util/dto/JwtResponse.java
@@ -1,5 +1,10 @@
 package gift.util.dto;
 
-public record JwtResponse(String token) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "JWT 응답 Dto")
+public record JwtResponse(
+    @Schema(description = "JWT 토큰", example = "header.payload.signature")
+    String token) {
 
 }
