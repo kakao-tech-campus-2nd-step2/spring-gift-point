95f683d3019b195ab1d136f87c6d66fa79b3ef7c
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-31 21:03:13 +0900

    feat: Response 형식에 맞게 MockMvc andExpect 변경
    - 각 도메인마다 ApiResponse 상속하도록 변경
    - ApiResponse는 result, message, httpStatus, 제네릭 data로 구성
    - mockMvc andExpect를 각 data에 맞게 변경

--- src/test/java/gift/controller/CategoryControllerTest.java
+++ src/test/java/gift/controller/CategoryControllerTest.java
@@ -9,6 +9,7 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,11 +20,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import gift.dto.CategoryDto;
-import gift.model.Category;
-import gift.model.Product;
+import gift.category.controller.CategoryController;
+import gift.category.dto.CategoryRequest;
+import gift.category.model.Category;
+import gift.product.model.Product;
 import gift.security.LoginMemberArgumentResolver;
-import gift.service.CategoryService;
+import gift.category.service.CategoryService;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -40,7 +42,6 @@ import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -66,9 +67,9 @@ class CategoryControllerTest {
     private WebApplicationContext context;
 
     private Category category;
-    private CategoryDto categoryDto;
+    private CategoryRequest categoryRequest;
     private Product product;
-    private List<CategoryDto> categoryDtoArrayList = new ArrayList<CategoryDto>();
+    private List<CategoryRequest> categoryRequestArrayList = new ArrayList<CategoryRequest>();
 
     @BeforeEach
     void setUp(RestDocumentationContextProvider restDocumentation) {
@@ -84,29 +85,44 @@ class CategoryControllerTest {
         product = new Product(1L, "상품", 10000, "image.jpg", category);
         var productNameList = new ArrayList<String>();
         productNameList.add(product.getName());
-        categoryDto = new CategoryDto(category.getId(), category.getName(), productNameList);
-        categoryDtoArrayList.add(categoryDto);
+        categoryRequest = new CategoryRequest(category.getId(), category.getName(),
+            productNameList);
+        categoryRequestArrayList.add(categoryRequest);
     }
 
     @Test
     void getAllCategoriesTest() throws Exception {
         // given
-        given(categoryService.getAllCategories()).willReturn(categoryDtoArrayList);
+        given(categoryService.getAllCategories()).willReturn(categoryRequestArrayList);
 
         // when & then
-        mockMvc.perform(get("/categories")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(categoryDto)))
+        mockMvc.perform(get("/api/categories")
+                .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$").isMap())
             .andExpect(jsonPath("$.result").value("OK"))
+            .andExpect(jsonPath("$.message").value("카테고리 전체 조회 성공"))
             .andExpect(jsonPath("$.httpStatus").value("OK"))
-            .andDo(document("categories/get-categories", responseFields(
-                fieldWithPath("result").description("API 호출 결과"),
-                fieldWithPath("message").description("호출 내용"),
-                fieldWithPath("httpStatus").description("HTTP 응답 상태")
-            )));
+            .andExpect(jsonPath("$.data[0].id").value(1))
+            .andExpect(jsonPath("$.data[0].name").value("상품권"))
+            .andExpect(jsonPath("$.data[0].productName[0]").value("상품"))
+            .andExpect(jsonPath("$.data[0].color").isEmpty())
+            .andExpect(jsonPath("$.data[0].imageUrl").isEmpty())
+            .andExpect(jsonPath("$.data[0].description").isEmpty())
+            .andDo(document("categories/get-categories",
+                responseFields(
+                    fieldWithPath("result").description("API 호출 결과"),
+                    fieldWithPath("message").description("호출 내용"),
+                    fieldWithPath("httpStatus").description("HTTP 응답 상태"),
+                    fieldWithPath("data").description("카테고리 목록"),
+                    fieldWithPath("data[].id").description("카테고리 ID"),
+                    fieldWithPath("data[].name").description("카테고리 이름"),
+                    fieldWithPath("data[].productName").description("카테고리에 속한 상품 이름 목록"),
+                    fieldWithPath("data[].color").description("카테고리 색상").optional(),
+                    fieldWithPath("data[].imageUrl").description("카테고리 이미지 URL").optional(),
+                    fieldWithPath("data[].description").description("카테고리 설명").optional()
+                )
+            ));
     }
 
     @Test
@@ -115,7 +131,7 @@ class CategoryControllerTest {
         given(categoryService.getCategoryById(1L)).willReturn(category);
 
         // when & then
-        mockMvc.perform(get("/categories/1")
+        mockMvc.perform(get("/api/categories/1")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(category)))
             .andDo(print())
@@ -123,57 +139,90 @@ class CategoryControllerTest {
             .andExpect(jsonPath("$").isMap())
             .andDo(print())
             .andExpect(jsonPath("$.result").value("OK"))
-            .andExpect(jsonPath("$.message").value(containsString("name='상품권'")))
+            .andExpect(jsonPath("$.message").value(containsString("카테고리 검색 성공")))
             .andExpect(jsonPath("$.httpStatus").value("OK"))
             .andDo(document("categories/get-category", responseFields(
                 fieldWithPath("result").description("API 호출 결과"),
                 fieldWithPath("message").description("호출 내용"),
-                fieldWithPath("httpStatus").description("HTTP 응답 상태")
+                fieldWithPath("httpStatus").description("HTTP 응답 상태"),
+                fieldWithPath("data.id").description("카테고리 ID"),
+                fieldWithPath("data.name").description("카테고리명"),
+                fieldWithPath("data.color").description("카테고리 색상").optional(),
+                fieldWithPath("data.imageUrl").description("카테고리 이미지 URL").optional(),
+                fieldWithPath("data.description").description("카테고리 설명").optional()
             )));
     }
 
     @Test
     void addCategoryTest() throws Exception {
         // given
-        Category savedCategory = new Category(1L, "상품권", new ArrayList<>());
-        given(categoryService.addCategory(any(Category.class))).willReturn(savedCategory);
+        Category savedCategory = new Category(1L, "상품권", null);
+        given(categoryService.addCategory(any(CategoryRequest.class))).willReturn(savedCategory);
 
         // when, then
-        mockMvc.perform(post("/add/category")
+        mockMvc.perform(post("/api/categories")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(category))).andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.result").value("OK"))
-            .andExpect(jsonPath("$.message").value(savedCategory.toString()))
+            .andExpect(jsonPath("$.message").value("카테고리 추가 성공"))
             .andExpect(jsonPath("$.httpStatus").value("OK"))
-            .andDo(document("categories/add-category", responseFields(
-                fieldWithPath("result").description("API 호출 결과"),
-                fieldWithPath("message").description("호출 내용"),
-                fieldWithPath("httpStatus").description("HTTP 응답 상태")
-            )));
+            .andExpect(jsonPath("$.data").value(savedCategory))
+            .andDo(document("categories/add-category",
+                requestFields(
+                    fieldWithPath("id").description("카테고리 ID"),
+                    fieldWithPath("name").description("카테고리명"),
+                    fieldWithPath("color").description("색상"),
+                    fieldWithPath("imageUrl").description("이미지주소"),
+                    fieldWithPath("description").description("상세정보")
+                ),
+                responseFields(
+                    fieldWithPath("result").description("API 호출 결과"),
+                    fieldWithPath("message").description("호출 내용"),
+                    fieldWithPath("httpStatus").description("HTTP 응답 상태"),
+                    fieldWithPath("data.id").description("카테고리 ID"),
+                    fieldWithPath("data.name").description("카테고리 이름"),
+                    fieldWithPath("data.color").description("카테고리 색상").optional(),
+                    fieldWithPath("data.imageUrl").description("카테고리 이미지 URL").optional(),
+                    fieldWithPath("data.description").description("카테고리 설명").optional()
+                )));
     }
 
     @Test
     void updateCategoryTest() throws Exception {
         // given
-        var categoryDto = new CategoryDto(1L, "새상품권", Collections.singletonList("productName"));
-        given(categoryService.updateCategory(any(Long.class), any(Category.class))).willReturn(
-            categoryDto);
+        var categoryDto = new CategoryRequest(1L, "새상품권", Collections.singletonList("productName"));
+        given(categoryService.updateCategory(any(CategoryRequest.class))).willReturn(
+            category);
 
         // when & then
-        mockMvc.perform(put("/update/category").param("id", String.valueOf(categoryDto.getId()))
+        mockMvc.perform(put("/api/categories").param("id", String.valueOf(categoryDto.getId()))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(categoryDto)))
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.result").value("OK"))
-            .andExpect(jsonPath("$.message").value(categoryDto.toString()))
+            .andExpect(jsonPath("$.message").value("카테고리 수정 성공"))
             .andExpect(jsonPath("$.httpStatus").value("OK"))
-            .andDo(document("categories/update-category", responseFields(
-                fieldWithPath("result").description("API 호출 결과"),
-                fieldWithPath("message").description("호출 내용"),
-                fieldWithPath("httpStatus").description("HTTP 응답 상태")
-            )));
+            .andDo(document("categories/update-category",
+                requestFields(
+                    fieldWithPath("id").description("카테고리 ID"),
+                    fieldWithPath("name").description("카테고리명"),
+                    fieldWithPath("productName").description("상품명 리스트"),
+                    fieldWithPath("color").description("색상"),
+                    fieldWithPath("imageUrl").description("이미지주소"),
+                    fieldWithPath("description").description("상세정보")
+                ),
+                responseFields(
+                    fieldWithPath("result").description("API 호출 결과"),
+                    fieldWithPath("message").description("호출 내용"),
+                    fieldWithPath("httpStatus").description("HTTP 응답 상태"),
+                    fieldWithPath("data.id").description("카테고리 ID"),
+                    fieldWithPath("data.name").description("카테고리명"),
+                    fieldWithPath("data.color").description("카테고리 색상").optional(),
+                    fieldWithPath("data.imageUrl").description("카테고리 이미지 URL").optional(),
+                    fieldWithPath("data.description").description("카테고리 설명").optional()
+                )));
     }
 
     @Test
@@ -182,19 +231,21 @@ class CategoryControllerTest {
         willDoNothing().given(categoryService).deleteCategoryById(any(Long.class));
 
         // when & then
-        mockMvc.perform(delete("/delete/category")
+        mockMvc.perform(delete("/api/categories")
                 .param("id", String.valueOf(1L))
                 .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.result").value("OK"))
-            .andExpect(jsonPath("$.message").value("삭제 성공"))
+            .andExpect(jsonPath("$.message").value("카테고리 삭제 성공"))
             .andExpect(jsonPath("$.httpStatus").value("OK"))
-            .andDo(document("categories/delete-category", responseFields(
-                fieldWithPath("result").description("API 호출 결과"),
-                fieldWithPath("message").description("호출 내용"),
-                fieldWithPath("httpStatus").description("HTTP 응답 상태")
-            )));
+            .andDo(document("categories/delete-category",
+                responseFields(
+                    fieldWithPath("result").description("API 호출 결과"),
+                    fieldWithPath("message").description("호출 내용"),
+                    fieldWithPath("httpStatus").description("HTTP 응답 상태"),
+                    fieldWithPath("data").description("데이터")
+                )));
     }
 
 
