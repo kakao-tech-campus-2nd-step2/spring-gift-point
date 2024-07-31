02544dc8e9721c6b2cf2ec2fc637760a146586f2
안원모 <tommy45231@naver.com>
2024-07-31 22:48:32 +0900

    Refactor : 위시 리스트 상품 삭제 StatusCode 구체화 + 에러 코드 구체화

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -11,12 +11,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +28,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Tag(name = "위시리스트 API", description = "위시리스트 관련된 API")
 @RestController
-@RequestMapping("/wishes")
+@RequestMapping("api/wishes")
 public class WishController {
     private final WishService wishService;
     private final ProductService productService;
@@ -69,9 +72,20 @@ public class WishController {
     }
 
     @Operation(summary = "위시리스트에서 아이템 제거", description = "위시리스트에서 특정 아이템을 제거합니다.")
-    @DeleteMapping
-    public ResponseEntity<String> removeWish(@RequestBody @Valid WishDTO wishDTO, @LoginMember Member member) {
-        wishService.removeWish(member.getId(), wishDTO.getProductId());
-        return ResponseEntity.ok("Wish removed successfully");
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> removeWish(@PathVariable Long productId, @LoginMember Member member) {
+        if (member == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+        Optional<ProductDTO> product = Optional.ofNullable(productService.getProductById(productId));
+        if (!product.isPresent()) {
+            return ResponseEntity.badRequest().body("상품이 존재하지 않습니다.");
+        }
+        try {
+            wishService.removeWish(member.getId(), productId);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("찜 목록에서 제거 실패: " + e.getMessage());
+        }
+        return ResponseEntity.noContent().build();
     }
 }
