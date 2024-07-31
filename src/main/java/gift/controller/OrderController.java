cee4c0f00154d5a3383803f7ffeb700a8001506e
cussle <cussle@kakao.com>
2024-08-01 00:23:24 +0900

    feat: 주문 API 통일

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -1,9 +1,17 @@
 package gift.controller;
 
+import static gift.util.constants.MemberConstants.EMAIL_ALREADY_USED;
+import static gift.util.constants.OptionConstants.INSUFFICIENT_QUANTITY;
+import static gift.util.constants.OptionConstants.OPTION_NOT_FOUND;
+
 import gift.dto.order.OrderRequest;
 import gift.dto.order.OrderResponse;
 import gift.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +33,35 @@ public class OrderController {
     }
 
     @Operation(summary = "주문 생성", description = "새로운 주문을 생성합니다.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "201", description = "주문 성공"),
+            @ApiResponse(
+                responseCode = "400",
+                description = "옵션 수량 부족",
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\"error\": \"" + INSUFFICIENT_QUANTITY + "(옵션 Id)\"}")
+                )
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "존재하지 않는 옵션 Id",
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\"error\": \"" + OPTION_NOT_FOUND + "(옵션 Id)\"}")
+                )
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "서버 오류",
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\"error\": \"(서버 오류 메시지)\"}")
+                )
+            )
+        }
+    )
     @PostMapping
     public ResponseEntity<OrderResponse> createOrder(
         @RequestBody OrderRequest orderRequest,
