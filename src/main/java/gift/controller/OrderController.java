95982156c4806c6669ba3cde9486e61593dc8b39
박민규 <pmq0101@naver.com>
2024-07-31 14:59:07 +0900

    feat(OrderController): 주문 페이지 조회 api 추가

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -6,6 +6,7 @@ import gift.domain.User;
 import gift.dto.common.apiResponse.ApiResponseBody.SuccessBody;
 import gift.dto.common.apiResponse.ApiResponseGenerator;
 import gift.dto.requestdto.OrderRequestDTO;
+import gift.dto.responsedto.OrderPageResponseDTO;
 import gift.dto.responsedto.OrderResponseDTO;
 import gift.service.OptionService;
 import gift.service.OrderService;
@@ -15,9 +16,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Parameter;
 
@@ -46,4 +49,16 @@ public class OrderController {
         return ApiResponseGenerator.success(HttpStatus.CREATED, "주문이 생성되었습니다.", orderResponseDTO);
     }
 
+    @GetMapping()
+    @Operation(summary = "주문 조회 api", description = "주문 조회 api입니다")
+    @ApiResponse(responseCode = "200", description = "주문 조회 성공")
+    public ResponseEntity<SuccessBody<OrderPageResponseDTO>> getAllOrderPages(
+        @RequestParam(value = "page", defaultValue = "0") int page,
+        @RequestParam(value = "size", defaultValue = "8") int size,
+        @RequestParam(value = "criteria", defaultValue = "id") String criteria,
+        @LoginUser User user
+    ){
+        OrderPageResponseDTO orderPageResponseDTO = orderService.getAllOrders(user.getId(), page, size, criteria);
+        return ApiResponseGenerator.success(HttpStatus.OK, "주문이 조회되었습니다.", orderPageResponseDTO);
+    }
 }
--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -3,13 +3,23 @@ package gift.service;
 import gift.auth.KakaoClient;
 import gift.domain.Option;
 import gift.domain.Order;
+import gift.domain.Product;
 import gift.domain.User;
 import gift.domain.Wish.WishList;
+import gift.dto.common.PageInfo;
 import gift.dto.requestdto.OrderRequestDTO;
+import gift.dto.responsedto.OrderPageResponseDTO;
 import gift.dto.responsedto.OrderResponseDTO;
+import gift.dto.responsedto.ProductPageResponseDTO;
+import gift.dto.responsedto.ProductResponseDTO;
 import gift.repository.JpaOrderRepository;
 import gift.repository.JpaWishRepository;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -38,5 +48,21 @@ public class OrderService {
 
         kakaoClient.sendMessage(user.getAccessToken(), orderRequestDTO.message());
         return OrderResponseDTO.from(order);
+    }
+
+    public OrderPageResponseDTO getAllOrders(Long userId, int page, int size, String criteria) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(criteria));
+
+        Page<Order> orderPage = jpaOrderRepository.findAllByUserId(userId, pageable);
+
+        List<OrderResponseDTO> orderResponseDTOList = orderPage
+            .stream()
+            .map(OrderResponseDTO::from)
+            .toList();
+
+        PageInfo pageInfo = new PageInfo(page, orderPage.getTotalElements(), orderPage.getTotalPages());
+
+        return new OrderPageResponseDTO(pageInfo, orderResponseDTOList);
+
     }
 }
