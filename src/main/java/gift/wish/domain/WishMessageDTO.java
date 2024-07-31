47f0339dcbb117e9950514ea19441987a55b07b9
dong_yxxn <g0326kdy@gmail.com>
2024-08-01 02:03:20 +0900

    refactor: message 클래스 생성

--- src/main/java/gift/wish/domain/WishMessageDTO.java
+++ src/main/java/gift/wish/domain/WishMessageDTO.java
@@ -1,2 +1,17 @@
-package gift.wish.domain;public class WishMessageDTO {
+package gift.wish.domain;
+
+public class WishMessageDTO {
+    private String message;
+
+    public WishMessageDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
