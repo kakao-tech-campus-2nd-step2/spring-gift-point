7bd7a414b088c5e863694d0428707e4922a78a8b
Gyumin Bae <bgmsound0528@gmail.com>
2024-07-31 17:49:53 +0900

    refactor: api spec 변경

--- src/main/java/gift/product/restapi/dto/response/ProductCategoryResponse.java
+++ src/main/java/gift/product/restapi/dto/response/ProductCategoryResponse.java
@@ -1,15 +1,24 @@
 package gift.product.restapi.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import gift.core.domain.product.ProductCategory;
 
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record ProductCategoryResponse(
         Long id,
-        String name
+        String name,
+        String color,
+        String imageUrl,
+        String description
 ) {
     public static ProductCategoryResponse of(ProductCategory category) {
         return new ProductCategoryResponse(
                 category.id(),
-                category.name()
+                category.name(),
+                category.color(),
+                category.imageUrl(),
+                category.description()
         );
     }
 }
