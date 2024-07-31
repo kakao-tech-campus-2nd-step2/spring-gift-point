d210e7cbf11cd33dcf5a21600096142f0e0e3264
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-31 14:47:26 +0900

    refactor: 필터 경로 변경

--- src/main/java/gift/filter/AuthFilter.java
+++ src/main/java/gift/filter/AuthFilter.java
@@ -59,7 +59,7 @@ public class AuthFilter implements Filter {
     private boolean isUnauthenticatedPath(String path) {
         return path.equals("/home") || path.startsWith("api/login")
                 || path.startsWith("api/register") || path.startsWith("/h2-console")
-                || path.startsWith("/api/products") || path.startsWith("/kakao/login")
+                || path.startsWith("/api/products") || path.startsWith("api/kakao")
                 || path.startsWith("/api/categories") || path.startsWith("/swagger-ui")
                 || path.startsWith("/api-docs");
     }
