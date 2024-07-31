d5137ade367829b81386d92d08aa54752070bec3
dong_yxxn <g0326kdy@gmail.com>
2024-07-31 19:28:10 +0900

    refactor: header로 user 식별 기능

--- src/main/java/gift/wish/controller/WishlistController.java
+++ src/main/java/gift/wish/controller/WishlistController.java
@@ -1,5 +1,9 @@
 package gift.wish.controller;
 
+import gift.user.domain.User;
+import gift.user.repository.UserRepository;
+import gift.user.service.UserService;
+import gift.utility.JwtUtil;
 import gift.wish.domain.WishlistRequest;
 import gift.wish.domain.WishlistResponse;
 import gift.wish.service.WishlistService;
@@ -9,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,35 +32,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class WishlistController {
 
     private final WishlistService wishlistService;
+    private final UserService userService;
 
-    public WishlistController(WishlistService wishlistService) {
+    public WishlistController(WishlistService wishlistService, UserService userService) {
         this.wishlistService = wishlistService;
+        this.userService = userService;
     }
 
-    @GetMapping("/{id}")
-    @Operation(summary = "사용자 위시리스트 상품조회")
-    public ResponseEntity<Page<WishlistResponse>> getUserWishlist(@PathVariable("id") Long userId,
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int size,
-        @RequestParam(defaultValue = "createdDate,desc") String sort) {
-        String[] sortParams = sort.split(",");
-        Sort.Direction direction = Sort.Direction.fromString(sortParams[1]);
-        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortParams[0]));
-
-        Page<WishlistResponse> wishlist = wishlistService.getWishlistResponseByUserId(userId, pageable);
-        return new ResponseEntity<>(wishlist, HttpStatus.OK);
-    }
     @GetMapping
     @Operation(summary = "위시리스트 상품조회")
     public ResponseEntity<Page<WishlistResponse>> getWishlist(
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "10") int size,
-        @RequestParam(defaultValue = "createdDate,desc") String sort) {
+        @RequestParam(defaultValue = "createdDate,desc") String sort,
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader){
+        String email = JwtUtil.extractEmailAsBearer(authorizationHeader);
+        User user = userService.findByEmail(email)
+            .orElseThrow(()->new IllegalArgumentException("email " + email + "인 user가 없습니다."));
+        Long userId = user.getId();
+
         String[] sortParams = sort.split(",");
         Sort.Direction direction = Sort.Direction.fromString(sortParams[1]);
         Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortParams[0]));
 
-        Page<WishlistResponse> wishlist = wishlistService.getWishlistResponse(pageable);
+        Page<WishlistResponse> wishlist = wishlistService.getWishlistResponseByUserId(userId, pageable);
         return new ResponseEntity<>(wishlist, HttpStatus.OK);
     }
     @PostMapping
