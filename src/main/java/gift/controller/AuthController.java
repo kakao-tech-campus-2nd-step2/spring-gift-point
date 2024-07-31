f4ac3086073387094c37f7b43d0406bb0655da54
minju26 <mjalswn26@gmail.com>
2024-08-01 01:51:46 +0900

    refactor(AuthController): 카카오 로그인 요청 시 리디렉션하도록 수정

--- src/main/java/gift/controller/AuthController.java
+++ src/main/java/gift/controller/AuthController.java
@@ -3,18 +3,23 @@ package gift.controller;
 import gift.dto.request.AuthRequest;
 import gift.dto.response.AuthResponse;
 import gift.service.AuthService;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/api/members")
 @Tag(name = "Account", description = "회원가입, 로그인 등 사용자 계정과 관련된 API Controller")
 public class AuthController {
 
+    @Value("${kakao.get-code.url}")
+    private String getCodeUrl;
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -38,6 +43,14 @@ public class AuthController {
     @GetMapping("/kakao")
     @Operation(summary = "카카오 회원가입 및 로그인 api")
     @ApiResponse(responseCode = "200", description = "카카오 로그인 성공")
+    public RedirectView kakaoLoginRedirect() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(getCodeUrl);
+        return redirectView;
+    }
+
+    @Hidden
+    @GetMapping("/kakao/redirect")
     public ResponseEntity<AuthResponse> kakaoLogin(@RequestParam("code") String code) {
         return new ResponseEntity<>(authService.kakaoLogin(code), HttpStatus.OK);
     }
