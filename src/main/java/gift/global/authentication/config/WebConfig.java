f41c79ca815e29c0979c6ed535d84dfd6b7e96e0
sim-mer <gyu2781@gmail.com>
2024-07-31 20:04:10 +0900

    chore(WebConfig): CORS 설정 및 카테고리 API 인터셉트 예외 설정

--- src/main/java/gift/global/authentication/config/WebConfig.java
+++ src/main/java/gift/global/authentication/config/WebConfig.java
@@ -5,6 +5,7 @@ import gift.global.authentication.interceptor.TokenCheckInterceptor;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,12 +28,21 @@ public class WebConfig implements WebMvcConfigurer {
             .excludePathPatterns(
                 "/api/members/register",
                 "/api/members/login",
-                "/api/members/reissue"
+                "/api/members/reissue",
+                "/api/categories/**"
             );
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(memberIdResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*");
     }
 }
