ee2c47f57f7d7ece0494a07d9213c9d89a3762f6
rdme0 <we6610@gmail.com>
2024-08-01 03:21:08 +0900

    :recycle: refactor: @Controller -> @RestController

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -18,13 +18,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/orders")
 public class OrderController {
     private final OrderService orderService;
