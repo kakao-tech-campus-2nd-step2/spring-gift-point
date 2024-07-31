266b31faa8d6d2602e1b957d64797b795e6775bf
Kangboom <jkl6147@naver.com>
2024-07-31 21:38:38 +0900

    feat: 옵션 리스트 응답 dto 추가

---
+++ src/main/java/gift/domain/option/dto/OptionsResponse.java
@@ -0,0 +1,12 @@
+package gift.domain.option.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+
+@Schema(description = "옵션 리스트 응답 Dto")
+public record OptionsResponse (
+    @Schema(description = "옵션 리스트")
+    List<OptionResponse> options
+){
+
+}
