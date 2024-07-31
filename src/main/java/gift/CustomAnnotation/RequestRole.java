4d67daf9b60a04704fa86d0a4c132eba6029c1c7
dkswoals <a4445656@gmail.com>
2024-07-31 16:12:10 +0900

    feat: 권한 열거형 추가

---
+++ src/main/java/gift/CustomAnnotation/RequestRole.java
@@ -0,0 +1,13 @@
+package gift.CustomAnnotation;
+
+import gift.model.user.Role;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface RequestRole {
+    public Role value() default Role.ROLE_USER;
+}
---
+++ src/main/java/gift/model/user/Role.java
@@ -0,0 +1,6 @@
+package gift.model.user;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER
+}
