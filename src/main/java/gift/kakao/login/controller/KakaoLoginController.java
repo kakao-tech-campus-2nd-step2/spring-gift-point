f5a8a2b689ea2ff3fa04dccef1888b5d725c6b31
dong_yxxn <g0326kdy@gmail.com>
2024-07-31 19:23:39 +0900

    jwt 변환안한 accessToken 저장 - 프론트에는 email을 jwt 변환하여 토큰 반환 - 일반 로그인 기능 추가

--- src/main/java/gift/kakao/login/controller/KakaoLoginController.java
+++ src/main/java/gift/kakao/login/controller/KakaoLoginController.java
@@ -1,14 +1,24 @@
 package gift.kakao.login.controller;
 
+import gift.kakao.login.service.KakaoLoginService;
+import gift.user.domain.User;
+import gift.user.domain.dto.LoginRequest;
+import gift.user.domain.dto.LoginResponse;
+import gift.user.service.UserService;
+import gift.utility.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/api/members")
@@ -20,16 +30,47 @@ public class KakaoLoginController {
     @Value("${kakao.redirect-uri}")
     private String redirectUri;
 
-    @GetMapping("/kakao")
-    public void showKakaoLogin(HttpServletResponse response) throws IOException {
-        String uri = "https://kauth.kakao.com/oauth/authorize?scope=talk_message&response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri;
-        response.sendRedirect(uri);
+    private final UserService userService;
+    private final KakaoLoginService kakaoLoginService;
+
+    public KakaoLoginController(UserService userService, KakaoLoginService kakaoLoginService) {
+        this.userService = userService;
+        this.kakaoLoginService = kakaoLoginService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponse> register(@RequestBody LoginRequest loginRequest) {
+        LoginResponse loginResponse = userService.save(loginRequest);
+
+        return new ResponseEntity<>(loginResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        Optional<User> userOptional = userService.findByEmail(loginRequest.email());
+        if (userOptional.isPresent() && loginRequest.password().equals(userOptional.get().getPassword())) {
+            String token = JwtUtil.generateToken(loginRequest.email());
+            return ResponseEntity.ok(new LoginResponse(token));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
     }
 
     @PostMapping("/kakao")
-    @Operation(summary = "카카오 로그인")
-    public void handleKakaoLoginPost(HttpServletResponse response) throws IOException {
-        String redirectUri = "/api/members/kakao";
-        response.sendRedirect(redirectUri);
+    @Operation(summary = "카카오 회원가입 로그인 api")
+    public RedirectView showKakaoLogin() {
+        RedirectView redirectView = new RedirectView();
+        String uri = "https://kauth.kakao.com/oauth/authorize?scope=talk_message&response_type=code&client_id=" + clientId + "&redirect_uri=" + redirectUri;
+        redirectView.setUrl(uri);
+        return redirectView;
+    }
+
+    @GetMapping("/kakao/redirect")
+    @Operation(summary = "카카오로부터 access code 받아서 로그인 진행")
+    public ResponseEntity<LoginResponse> handleKakaoCallback(
+        @RequestParam(value = "code") String code) {
+        String accessToken = kakaoLoginService.getAccessToken(code);
+        Long userId = kakaoLoginService.getUserInfo(accessToken);
+        String jwtToken = userService.getJwtTokenByUserId(userId);
+        return new ResponseEntity<>(new LoginResponse(jwtToken), HttpStatus.OK);
     }
 }
