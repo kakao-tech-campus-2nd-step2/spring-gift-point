264c599807dfe88d374a05cdcdb08563e7300635
Kangboom <jkl6147@naver.com>
2024-07-31 21:36:22 +0900

    feat: swagger 기능 추가

--- src/main/java/gift/domain/order/dto/OrderResponse.java
+++ src/main/java/gift/domain/order/dto/OrderResponse.java
@@ -1,10 +1,19 @@
 package gift.domain.order.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "주문 응답 Dto")
 public record OrderResponse(
+
+    @Schema(description = "주문 Id")
     Long id,
+    @Schema(description = "옵션 Id")
     Long optionId,
+    @Schema(description = "옵션 수량")
     int quantity,
+    @Schema(description = "주문 생성 시간")
     String orderDateTime,
+    @Schema(description = "주문 메세지")
     String message
 ) {
 
