93a61ca25ffe8e755c288409d6bcc504dcd9fc94
Woojae Jeong <woo6388@naver.com>
2024-08-01 01:16:40 +0900

    refactor : 로그인 회원가입 관련 Swagger content 내용 수정

--- src/main/java/gift/Controller/MemberController.java
+++ src/main/java/gift/Controller/MemberController.java
@@ -1,14 +1,14 @@
 package gift.Controller;
 
 import gift.DTO.RequestMemberDTO;
-import gift.Model.Entity.Member;
 import gift.Service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,8 +26,10 @@ public class MemberController {
     }
 
     @Operation(summary = "회원가입", description = "회원가입을 진행합니다")
-    @ApiResponse(responseCode = "201", description = "회원가입 완료")
-    @ApiResponse(responseCode = "400", description = "잘못된 요청입니다. 입력값을 확인해주세요")
+    @ApiResponse(responseCode = "201", description = "회원가입 완료",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+    })
+    @ApiResponse(responseCode = "400", description = "잘못된 요청입니다. 입력값을 확인하거나 중복된 이메일입니다")
     @ApiResponse(responseCode = "500", description = "서버 내부 에러 발생")
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@Valid @RequestBody RequestMemberDTO requestMemberDTO){
@@ -36,8 +38,11 @@ public class MemberController {
     }
 
     @Operation(summary = "로그인", description = "존재하는 사용자인지 확인 후 토큰을 발급합니다" )
-    @ApiResponse(responseCode = "200", description = "로그인 완료")
+    @ApiResponse(responseCode = "200", description = "로그인 완료",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = String.class))
+    })
     @ApiResponse(responseCode = "400", description = "잘못된 요청입니다. 입력값을 확인해주세요")
+    @ApiResponse(responseCode = "403", description = "아이디 또는 비밀번호가 틀렸습니다")
     @ApiResponse(responseCode = "500", description = "서버 내부 에러 발생")
     @PostMapping("/login")
     public ResponseEntity<String> loginUser(@Valid @RequestBody RequestMemberDTO member) {
