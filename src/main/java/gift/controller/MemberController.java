5539c3f273bb8a28d73945c70042967534f3a8d3
seojiu <seojiu5598@gmail.com>
2024-08-01 00:36:54 +0900

    chore: 클래스명 변경

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -1,6 +1,6 @@
 package gift.controller;
 
-import gift.dto.MemberDto;
+import gift.dto.MemberRequestDto;
 import gift.dto.MemberResponseDto;
 import gift.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -25,8 +25,8 @@ public class MemberController {
 
     @PostMapping("/register")
     @Operation(summary = "회원 가입", description = "회원 가입을합니다.")
-    public ResponseEntity<MemberResponseDto> register(@RequestBody @Valid MemberDto memberDto) {
-        MemberResponseDto response = memberService.register(memberDto);
+    public ResponseEntity<MemberResponseDto> register(@RequestBody @Valid MemberRequestDto memberRequestDto) {
+        MemberResponseDto response = memberService.register(memberRequestDto);
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + response.getToken());
         return ResponseEntity.ok().headers(headers).body(response);
@@ -34,8 +34,8 @@ public class MemberController {
 
     @PostMapping("/login")
     @Operation(summary = "로그인", description = "기존 회원을 로그인 합니다.")
-    public ResponseEntity<MemberResponseDto> login(@RequestBody @Valid MemberDto memberDto) {
-        MemberResponseDto response = memberService.login(memberDto);
+    public ResponseEntity<MemberResponseDto> login(@RequestBody @Valid MemberRequestDto memberRequestDto) {
+        MemberResponseDto response = memberService.login(memberRequestDto);
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + response.getToken());
         return ResponseEntity.ok().headers(headers).body(response);
--- src/main/java/gift/dto/MemberRequestDto.java
+++ src/main/java/gift/dto/MemberRequestDto.java
@@ -3,7 +3,7 @@ package gift.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public class MemberDto {
+public class MemberRequestDto {
     @NotBlank(message = "이메일은 필수 입력값입니다.")
     @Email(message = "유효하지 않은 이메일 형식입니다.")
     private String email;
