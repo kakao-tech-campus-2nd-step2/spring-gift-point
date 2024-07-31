974ee7aadb5b1888f0d5a9e3dae1c538b3971fd4
young-in <hardworkingini@gmail.com>
2024-08-01 03:11:40 +0900

    feat(WebConfig) : Swagger 사용 시 발생하는 HttpMediaTypeNotAcceptableException 오류 해결

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -4,15 +4,18 @@ import gift.resolver.LoginMemberArgumentResolver;
 import gift.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport {
+public class WebConfig implements WebMvcConfigurer {
     private final MemberService memberService;
+
     public WebConfig(MemberService memberService) {
         this.memberService = memberService;
     }
@@ -20,6 +23,11 @@ public class WebConfig extends WebMvcConfigurationSupport {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(memberService));
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
     }
 
     @Bean
