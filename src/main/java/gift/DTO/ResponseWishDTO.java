8f18bfab9477a6b089ca64a8289285c4642c9df1
Woojae Jeong <woo6388@naver.com>
2024-08-01 00:40:21 +0900

    refactor : API 명세서에 맞게 리팩토링 및 레코드 클래스로 전환

--- src/main/java/gift/DTO/ResponseWishDTO.java
+++ src/main/java/gift/DTO/ResponseWishDTO.java
@@ -2,24 +2,23 @@ package gift.DTO;
 
 import gift.Model.Entity.Wish;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
 
 @Schema(description = "찜 응답 DTO")
-public class ResponseWishDTO {
-    @Schema(description = "상품 이름")
-    private String name;
-
-    public ResponseWishDTO() {
-    }
-
-    public ResponseWishDTO(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
+public record ResponseWishDTO(
+        Long id,
+        String name,
+        int price,
+        String imageUrl,
+        LocalDateTime createdDate
+) {
     public static ResponseWishDTO of(Wish wish){
-        return new ResponseWishDTO(wish.getProduct().getName().getValue());
+        return new ResponseWishDTO(wish.getProduct().getId(),
+                wish.getProduct().getName().getValue(),
+                wish.getProduct().getPrice().getValue(),
+                wish.getProduct().getImageUrl().getValue(),
+                wish.getCreatedDate());
     }
 }
