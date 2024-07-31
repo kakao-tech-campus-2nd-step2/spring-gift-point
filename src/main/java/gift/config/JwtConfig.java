bff484aabd51ae5c4567c29f326231e694f1a3bd
young-in <hardworkingini@gmail.com>
2024-08-01 04:14:48 +0900

    refactor(JwtConfig) : token 생성 오류 해결

--- src/main/java/gift/config/JwtConfig.java
+++ src/main/java/gift/config/JwtConfig.java
@@ -1,20 +1,26 @@
 package gift.config;
 
-import gift.constant.Constants;
-import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import gift.constant.Constants;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtConfig {
     public String generateToken(String email) {
-        Claims claims = (Claims) Jwts.claims();
+        // Create a claims map
+        Map<String, Object> claims = new HashMap<>();
         claims.put("email", email);
+
+        // Generate the JWT token
         return Jwts.builder()
                 .setClaims(claims)
+                .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + Constants.ONE_DAY_MILLIS))
                 .signWith(Keys.hmacShaKeyFor(Constants.SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
