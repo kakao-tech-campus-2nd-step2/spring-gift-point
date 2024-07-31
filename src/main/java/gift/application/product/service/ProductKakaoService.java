d6ec0d9fe5a90223e15675865d93d06d5049c7fd
momnpa333 <momnpa333@gmail.com>
2024-07-31 20:19:47 +0900

    refact: 메세지 전송 로직 수정

--- src/main/java/gift/application/product/service/ProductKakaoService.java
+++ src/main/java/gift/application/product/service/ProductKakaoService.java
@@ -2,7 +2,10 @@ package gift.application.product.service;
 
 import gift.application.product.service.apiCaller.ProductKakaoApiCaller;
 import gift.application.token.TokenManager;
+import gift.model.member.Member;
+import gift.model.member.Provider;
 import gift.model.token.KakaoToken;
+import gift.repository.member.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,14 +13,24 @@ public class ProductKakaoService {
 
     private final ProductKakaoApiCaller productKakaoApiCaller;
     private final TokenManager tokenManager;
+    private final MemberRepository memberRepository;
 
-    public ProductKakaoService(ProductKakaoApiCaller productKakaoApiCaller,
-        TokenManager tokenManager) {
+    public ProductKakaoService(
+        ProductKakaoApiCaller productKakaoApiCaller,
+        TokenManager tokenManager,
+        MemberRepository memberRepository
+    ) {
         this.productKakaoApiCaller = productKakaoApiCaller;
         this.tokenManager = tokenManager;
+        this.memberRepository = memberRepository;
     }
 
     public void sendPurchaseMessage(Long memberId, String message) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new IllegalArgumentException("Member not found"));
+        if (member.getProvider() != Provider.KAKAO) {
+            return;
+        }
         KakaoToken token = tokenManager.getToken(memberId);
         productKakaoApiCaller.sendMessage(token.getAccessToken(), message);
     }
