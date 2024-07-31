c2d7fdf7839c3997da40e9d3b32e6ae123a2ee6a
Woojae Jeong <woo6388@naver.com>
2024-07-31 21:17:43 +0900

    refactor : 누락된 @Valid 어노테이션 추가

--- src/main/java/gift/Controller/MemberController.java
+++ src/main/java/gift/Controller/MemberController.java
@@ -6,6 +6,7 @@ import gift.Service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,9 +28,9 @@ public class MemberController {
     @ApiResponse(responseCode = "400", description = "잘못된 요청입니다. 입력값을 확인해주세요")
     @ApiResponse(responseCode = "500", description = "서버 내부 에러 발생")
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody RequestMemberDTO requestMemberDTO){
+    public ResponseEntity<String> registerUser(@Valid @RequestBody RequestMemberDTO requestMemberDTO){
         Member member = memberService.signUpUser(requestMemberDTO);
-        return ResponseEntity.created(URI.create("/members" + member.getId())).body("회원가입이 정상적으로 되었습니다");
+        return ResponseEntity.created(URI.create("/members/register/" + member.getId())).body("회원가입이 정상적으로 되었습니다");
     }
 
     @Operation(summary = "로그인", description = "존재하는 사용자인지 확인 후 토큰을 발급합니다" )
@@ -37,7 +38,7 @@ public class MemberController {
     @ApiResponse(responseCode = "400", description = "잘못된 요청입니다. 입력값을 확인해주세요")
     @ApiResponse(responseCode = "500", description = "서버 내부 에러 발생")
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody RequestMemberDTO member) {
+    public ResponseEntity<String> loginUser(@Valid @RequestBody RequestMemberDTO member) {
         return ResponseEntity.ok(memberService.loginUser(member));
     }
 }
