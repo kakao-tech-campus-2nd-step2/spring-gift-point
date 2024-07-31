b2eea7420e1636c4e9caee640728b1198e63faef
JinSeoHyun <15roa1121@gmail.com>
2024-08-01 03:24:06 +0900

    fix : 옵견 리스트 반환 시, API 문서 맞춰서 키값 추가

--- src/main/java/gift/main/controller/OptionController.java
+++ src/main/java/gift/main/controller/OptionController.java
@@ -7,7 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
  * *옵션리스트는 처음 프로덕트 등록시에만 사용한다.
@@ -31,8 +33,10 @@ public class OptionController {
     //특정 제품의 옵션 목록 조회
     @GetMapping("/{product_id}")
     public ResponseEntity<?> findAllOption(@PathVariable(value = "product_id") long productId) {
+        Map<String, Object> response = new HashMap<>();
         List<OptionResponse> options = optionService.findAllOption(productId);
-        return ResponseEntity.ok(options);
+        response.put("options", options);
+        return ResponseEntity.ok(response);
     }
 
     //새로운 옵션 추가
