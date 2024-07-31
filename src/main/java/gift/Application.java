7dd825e1c46b342c6abb241e7bf414e1dc2a336d
us4c0d3 <jwseok312@gmail.com>
2024-08-01 02:37:49 +0900

    fix(Application): 누락된 어노테이션 추가

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -2,7 +2,11 @@ package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
