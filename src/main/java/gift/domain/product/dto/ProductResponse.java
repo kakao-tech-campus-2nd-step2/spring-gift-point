d1f513a963415d27b8df0236d3f5a151bfae3d19
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-31 16:00:43 +0900

    refactor: 상품 API 명세에 맞게 수정

--- src/main/java/gift/domain/product/dto/ProductResponse.java
+++ src/main/java/gift/domain/product/dto/ProductResponse.java
@@ -1,24 +1,23 @@
 package gift.domain.product.dto;
 
 import gift.domain.product.entity.Product;
-import java.util.List;
 
 public record ProductResponse(
     Long id,
-    CategoryResponse category,
     String name,
     int price,
     String imageUrl,
-    List<OptionResponse> options
+    Long categoryId,
+    String categoryName
 ) {
     public static ProductResponse from(Product product) {
         return new ProductResponse(
             product.getId(),
-            CategoryResponse.from(product.getCategory()),
             product.getName(),
             product.getPrice(),
             product.getImageUrl(),
-            product.getOptions().stream().map(OptionResponse::from).toList()
+            product.getCategory().getId(),
+            product.getCategory().getName()
         );
     }
 }
