8d1faa65b7241f52687e966dd2bb4671d5d02157
Gyumin Bae <bgmsound0528@gmail.com>
2024-07-31 17:47:39 +0900

    refactor: api spec 변경

--- src/main/java/gift/product/restapi/dto/request/CategoryUpdateRequest.java
+++ src/main/java/gift/product/restapi/dto/request/CategoryUpdateRequest.java
@@ -1,7 +1,10 @@
 package gift.product.restapi.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import gift.core.domain.product.ProductCategory;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record CategoryUpdateRequest(
         String name,
         String color,
