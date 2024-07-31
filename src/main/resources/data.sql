ca12e3b9f84712b455ddd85499a9d7f38bf118b2
JunHyeongChoi <ajfeh22@gmail.com>
2024-07-31 16:05:35 +0900

    test: 기본 데이터 삽입으로 인한 테스트 실패 해결

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,22 +9,22 @@ VALUES ('외식 통합권 10만원권', 100000, 'image_url_1', 1),
        ('[선물포장/미니퍼퓸증정] 디켄터 리드 디퓨저 300ml + 메시지카드', 108000, 'image_url_2', 2),
        ('[단독각인] 피렌체 1221 에디션 오드코롱 50ml (13종 택1)', 145000, 'image_url_3', 2);
 
-INSERT INTO option (product_id, name, quantity)
-VALUES (1, 'Option A', 100),
-       (1, 'Option B', 200),
-       (1, 'Option C', 300);
+INSERT INTO option (product_id, name, version, quantity)
+VALUES (1, 'Option A', 0, 100),
+       (1, 'Option B', 0, 200),
+       (1, 'Option C', 0, 300);
 
-INSERT INTO option (product_id, name, quantity)
-VALUES (2, 'Option A', 150),
-       (2, 'Option B', 250),
-       (2, 'Option C', 350);
+INSERT INTO option (product_id, name, version, quantity)
+VALUES (2, 'Option A', 0, 150),
+       (2, 'Option B', 0, 250),
+       (2, 'Option C', 0, 350);
 
-INSERT INTO option (product_id, name, quantity)
-VALUES (3, 'Option A', 100),
-       (3, 'Option B', 200),
-       (3, 'Option C', 300);
+INSERT INTO option (product_id, name, version, quantity)
+VALUES (3, 'Option A', 0, 100),
+       (3, 'Option B', 0, 200),
+       (3, 'Option C', 0, 300);
 
-INSERT INTO option (product_id, name, quantity)
-VALUES (4, 'Option A', 100),
-       (4, 'Option B', 200),
-       (4, 'Option C', 300);
+INSERT INTO option (product_id, name, version, quantity)
+VALUES (4, 'Option A', 0, 100),
+       (4, 'Option B', 0, 200),
+       (4, 'Option C', 0, 300);
