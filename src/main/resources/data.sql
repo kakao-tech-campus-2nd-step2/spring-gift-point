eccf2e625204893c80864f83908364d8b5c21d57
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-31 13:51:37 +0900

    refactor : 테이블 및 추가할 데이터 양식 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 insert into member(email, password, role) values ('admin', '1234', 'ADMIN');
 
-insert into category(name, color, image_url, description) values('상품권','#6c95d1','https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png','');
-insert into category(name, color, image_url, description) values('인형','#a31ad2','https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png','');
+insert into category(name, image_url, description) values('상품권','https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png','');
+insert into category(name, image_url, description) values('인형','https://gift-s.kakaocdn.net/dn/gift/images/m640/dimm_theme.png','');
 
 
-insert into product(category_id, name, price, image_url) values(1, '라이언', 1000, 'https://i.namu.wiki/i/OgK0X2DAdO0K6vIBGUDvE8fR2jP0dmTu3z6mAM0JVGw310C7H3c9DmsQ_SyBc-s835u5kwHxVpe0HutSHIqo7Q.webp');
-insert into product(category_id, name, price, image_url) values(1, '어피치', 2000, 'https://i.namu.wiki/i/OgK0X2DAdO0K6vIBGUDvE8fR2jP0dmTu3z6mAM0JVGw310C7H3c9DmsQ_SyBc-s835u5kwHxVpe0HutSHIqo7Q.webp');
-insert into product(category_id, name, price, image_url) values(1, '춘식', 3000, 'https://i.namu.wiki/i/OgK0X2DAdO0K6vIBGUDvE8fR2jP0dmTu3z6mAM0JVGw310C7H3c9DmsQ_SyBc-s835u5kwHxVpe0HutSHIqo7Q.webp');
-insert into product(category_id, name, price, image_url) values(1, '제이지', 1, 'https://i.namu.wiki/i/OgK0X2DAdO0K6vIBGUDvE8fR2jP0dmTu3z6mAM0JVGw310C7H3c9DmsQ_SyBc-s835u5kwHxVpe0HutSHIqo7Q.webp');
+insert into product(name, price, image_url, category_id) values('라이언', 1000, 'https://i.namu.wiki/i/OgK0X2DAdO0K6vIBGUDvE8fR2jP0dmTu3z6mAM0JVGw310C7H3c9DmsQ_SyBc-s835u5kwHxVpe0HutSHIqo7Q.webp', 1);
+insert into product(name, price, image_url, category_id) values('어피치', 2000, 'https://i.namu.wiki/i/OgK0X2DAdO0K6vIBGUDvE8fR2jP0dmTu3z6mAM0JVGw310C7H3c9DmsQ_SyBc-s835u5kwHxVpe0HutSHIqo7Q.webp',1 );
+insert into product(name, price, image_url, category_id) values('춘식', 3000, 'https://i.namu.wiki/i/OgK0X2DAdO0K6vIBGUDvE8fR2jP0dmTu3z6mAM0JVGw310C7H3c9DmsQ_SyBc-s835u5kwHxVpe0HutSHIqo7Q.webp', 1);
+insert into product(name, price, image_url, category_id) values('제이지', 1, 'https://i.namu.wiki/i/OgK0X2DAdO0K6vIBGUDvE8fR2jP0dmTu3z6mAM0JVGw310C7H3c9DmsQ_SyBc-s835u5kwHxVpe0HutSHIqo7Q.webp', 1);
 
 insert into wish(member_id, product_id) values(1,1);
 insert into wish(member_id, product_id) values(1,2);
