364b41098a015d6f75803e550c1187c65d781808
Lee Hyungi <geist9110@naver.com>
2024-07-31 20:26:55 +0900

    fix: AuthExceptionHandler not handle exception OrderController

--- src/main/java/gift/exceptionhandler/AuthExceptionHandler.java
+++ src/main/java/gift/exceptionhandler/AuthExceptionHandler.java
@@ -3,13 +3,18 @@ package gift.exceptionhandler;
 import gift.exception.FailedLoginException;
 import gift.exception.InvalidAccessTokenException;
 import gift.member.MemberController;
+import gift.order.OrderController;
 import gift.wishlist.WishlistController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = {WishlistController.class, MemberController.class})
+@RestControllerAdvice(assignableTypes = {
+    WishlistController.class,
+    MemberController.class,
+    OrderController.class,
+})
 public class AuthExceptionHandler {
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
