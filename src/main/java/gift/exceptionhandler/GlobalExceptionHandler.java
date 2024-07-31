3e633cd13d6106c19642c1980bca42e4fc60e77d
Lee Hyungi <geist9110@naver.com>
2024-07-31 20:12:02 +0900

    feat: Add Internal Server Error exception handler

--- src/main/java/gift/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/gift/exceptionhandler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException.InternalServerError;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,6 +12,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     public String handleIllegalArgumentException(IllegalArgumentException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(InternalServerError.class)
+    public String handleInternalServerError(InternalServerError exception) {
         return exception.getMessage();
     }
 }
