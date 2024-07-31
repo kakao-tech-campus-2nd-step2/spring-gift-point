6b9df581aa89f39d670df3d1a0c45fca2547cade
dong_yxxn <g0326kdy@gmail.com>
2024-07-31 21:27:49 +0900

    refactor: api문서에 맞게 수정

--- src/main/java/gift/wish/domain/WishlistResponse.java
+++ src/main/java/gift/wish/domain/WishlistResponse.java
@@ -1,8 +1,5 @@
 package gift.wish.domain;
 
-import gift.product.domain.ProductDTO;
-import java.time.LocalDateTime;
-
-public record WishlistResponse(Long wishlistId, Long userId, ProductDTO productDTO, Long amount, LocalDateTime createdDate) {
+public record WishlistResponse(Long wishlistId, String name, Long price, String imageUrl) {
 
 }
