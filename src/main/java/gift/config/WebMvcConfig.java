3e27f6802f18b876dd48fb4a18cb195c769f9f1f
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-07-31 22:10:54 +0900

    feat: CORS 설정

--- src/main/java/gift/config/WebMvcConfig.java
+++ src/main/java/gift/config/WebMvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,6 +27,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberResolver(tokenService));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://43.203.234.144:8080") // 허용할 출처 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 설정
+                .allowedHeaders("*") // 허용할 헤더 설정
+                .allowCredentials(true); // 자격 증명 허용 설정
     }
 
     @Bean
