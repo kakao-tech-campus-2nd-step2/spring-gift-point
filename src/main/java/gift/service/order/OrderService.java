6a57cda8d39e60229bdf24cd43457caeef498124
pjhcsols <pjhcsols@naver.com>
2024-07-31 14:42:02 +0900

    Feat: 주문 기능 API 생성[박한솔]

---
+++ src/main/java/gift/service/order/OrderService.java
@@ -0,0 +1,33 @@
+package gift.service.order;
+
+import gift.domain.order.Order;
+import gift.repository.order.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public Page<Order> getAllOrders(int page, int size, String sortBy, String sortOrder) {
+        Sort.Direction sortDirection = sortOrder.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, sortBy));
+        return orderRepository.findAll(pageable);
+    }
+}
