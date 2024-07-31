8afe03ccedec3340cf525ff04cd963b5e8a06d32
daolove0323 <daolove0323@gmail.com>
2024-07-31 18:53:09 +0900

    feat: CORS 적용

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -2,8 +2,10 @@ package gift.config;
 
 import gift.service.MemberService;
 import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,5 +21,19 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserArgumentResolver(memberService));
         resolvers.add(new LoginAdminArgumentResolver(memberService));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("Content-Type", "Authorization")
+                    .maxAge(3600);
+            }
+        };
     }
 }
