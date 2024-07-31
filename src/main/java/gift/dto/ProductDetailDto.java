3056aba41e384507cd58d505de647d6536976353
seojiu <seojiu5598@gmail.com>
2024-07-31 17:35:35 +0900

    fix: 위시리스트 추가시 발생하는 오류 수정, 관리자화면 오류 수정

--- src/main/java/gift/dto/ProductDetailDto.java
+++ src/main/java/gift/dto/ProductDetailDto.java
@@ -13,31 +13,15 @@ public class ProductDetailDto {
         this.imageUrl = imageUrl;
     }
 
-    public ProductDetailDto(Product product) {
-        this(product.getName(), product.getPrice(), product.getImageUrl());
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
     public String getImageUrl() {
         return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
     }
 }
