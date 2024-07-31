648506634800a08f016abb5f6285cd762b44fdf3
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-31 21:29:08 +0900

    feat: cors 설정 추가

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -5,6 +5,7 @@ import gift.util.resolver.LoginUserArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,5 +20,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserArgumentResolver(jwtService));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // 모든 출처 허용
+                .allowedMethods("*")  // 모든 HTTP 메서드 허용
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .maxAge(3600);  // pre-flight 요청 캐시 시간
     }
 }
