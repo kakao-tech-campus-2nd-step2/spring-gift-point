92b96ef2974425456e9a8fed39593fc42eda09cb
doyooon <ian661087@gmail.com>
2024-07-31 18:55:06 +0900

    feat Request : Request 일관 되게 변경

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 - [x] api 명세에 맞춰 controller path 수정
 - [ ] api 명세에 맞춰 변경
   - [ ] 상품 목록 조회 (페이지네이션 적용)
-  - [ ] Request 일관 되게 변경
-    - [ ] json format으로 나타내기
-    - [ ] Request 요청에서 id 삭제
+  - [x] Request 일관 되게 변경
+    - [x] json format으로 나타내기
+    - [x] Request 요청에서 id 삭제
   - [ ] Response 일관되게 변경
     - [ ] 목록 반환은 Contents로 이름 통일
     - [ ] status 값 수정
--- src/main/java/gift/product/ProductRequest.java
+++ src/main/java/gift/product/ProductRequest.java
@@ -1,13 +1,14 @@
 package gift.product;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import gift.option.OptionRequest;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProductRequest {
-    private Long id;
-
     @NotNull(message = "상품 명은 필수 값 입니다.")
     @Size(min = 1, max = 15, message = "상품 명의 길이는 1~15자 입니다.")
     @Pattern(regexp = "^[A-Za-z가-힣0-9()\\[\\]\\-&/_+\\s]*$", message = "( ), [ ], +, -, &, /, _ 를 제외한 특수문자는 사용할 수 없습니다.")
@@ -23,14 +24,6 @@ public class ProductRequest {
 
     @NotNull(message = "반드시 옵션 하나는 있어야 합니다.")
     private OptionRequest optionRequest;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -64,12 +57,12 @@ public class ProductRequest {
         this.categoryId = categoryId;
     }
 
-    public OptionRequest getOptionRequest(){
+    public OptionRequest getOptionRequest() {
         return optionRequest;
     }
 
-    public Product toEntity(){
-        return new Product(this.id, this.name, this.price, this.imageUrl, this.categoryId);
+    public Product toEntity() {
+        return new Product(this.name, this.price, this.imageUrl, this.categoryId);
     }
 
 }
