aed7f91424e425d7339b7517c4fb8d04bab0157b
BOMIN LYU <rbm0524@naver.com>
2024-08-01 10:25:38 +0900

    refactor : 오버라이딩한 메서드 삭제, 수정

--- src/main/java/gift/Repository/ProductJpaRepository.java
+++ src/main/java/gift/Repository/ProductJpaRepository.java
@@ -8,7 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
-    List<Product> findByisDeletedFalse();
 
-    Page<Product> findByisDeletedFalse(Pageable pageable);
 }
