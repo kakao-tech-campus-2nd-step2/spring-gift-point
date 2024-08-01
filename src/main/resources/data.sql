9ec0d60307d4fd1d65a45d6520d53ac3fe0d1747
JinSeoHyun <15roa1121@gmail.com>
2024-08-01 20:16:03 +0900

    sql: 데이터 삽입문 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,45 @@
 INSERT INTO Category (name, color, image_url, description)
-VALUES ('패션', 'RED', 'http://example.com/fashion.jpg', '패션 관련 카테고리');
+VALUES ('교환권', 'RED', 'http://example.com/coupon.jpg', '교환권 관련 카테고리');
+
 INSERT INTO Category (name, color, image_url, description)
-VALUES ('뷰티', 'PINK', 'http://example.com/beauty.jpg', '뷰티 관련 카테고리');
+VALUES ('상품권', 'INDIGO', 'src/main/resources/image.jpeg', '상품권 관련 카테고리');
+
 INSERT INTO Category (name, color, image_url, description)
-VALUES ('식품', 'GREEN', 'http://example.com/food.jpg', '식품 관련 카테고리');
+VALUES ('뷰티', 'YELLOW', 'src/main/resources/image.jpeg', '뷰티 관련 카테고리');
+
 INSERT INTO Category (name, color, image_url, description)
-VALUES ('디지털/가전', 'BLUE', 'http://example.com/digital.jpg', '디지털 및 가전제품 관련 카테고리');
+VALUES ('패션', 'GREEN', 'src/main/resources/image.jpeg', '패션 관련 카테고리');
+
 INSERT INTO Category (name, color, image_url, description)
-VALUES ('인테리어/그릇', 'BROWN', 'http://example.com/interior.jpg', '인테리어 및 그릇 관련 카테고리');
+VALUES ('식품', 'BLUE', 'src/main/resources/image.jpeg', '식품 관련 카테고리');
+
 INSERT INTO Category (name, color, image_url, description)
-VALUES ('출산/유아동', 'YELLOW', 'http://example.com/baby.jpg', '출산 및 유아동 관련 카테고리');
+VALUES ('와인/양주/전통주', 'NAVY', 'src/main/resources/image.jpeg', '와인/양주/전통주 관련 카테고리');
+
+INSERT INTO Category (name, color, image_url, description)
+VALUES ('리빙/도서', 'PURPLE', 'src/main/resources/image.jpeg', '리빙/도서 관련 카테고리');
+
+INSERT INTO Category (name, color, image_url, description)
+VALUES ('레저/스포츠', 'BROWN', 'src/main/resources/image.jpeg', '레저/스포츠 관련 카테고리');
+
+INSERT INTO Category (name, color, image_url, description)
+VALUES ('아티스트/캐릭터', 'PINK', 'src/main/resources/image.jpeg', '아티스트/캐릭터 관련 카테고리');
+
+INSERT INTO Category (name, color, image_url, description)
+VALUES ('유아동/반려', 'WHITE', 'src/main/resources/image.jpeg', '유아동/반려 관련 카테고리');
+
+INSERT INTO Category (name, color, image_url, description)
+VALUES ('디지털/가전', 'BLACK', 'src/main/resources/image.jpeg', '디지털/가전 관련 카테고리');
+
+INSERT INTO Category (name, color, image_url, description)
+VALUES ('카카오프렌즈', 'ORANGE', 'src/main/resources/image.jpeg', '카카오프렌즈 관련 카테고리');
+
+INSERT INTO Category (name, color, image_url, description)
+VALUES ('트렌드 선물', 'CYAN', 'src/main/resources/image.jpeg', '트렌드 선물 관련 카테고리');
+
+INSERT INTO Category (name, color, image_url, description)
+VALUES ('백화점', 'GRAY', 'src/main/resources/image.jpeg', '백화점 관련 카테고리');
+
 
 
 -- INSERT INTO Users (name, email, password, role)
