2a49105e988dbd83165df0f1ce70f040c5ea316d
suhyeon7497 <nucc1186@gmail.com>
2024-08-01 22:10:42 +0900

    feat: cors 설정

--- src/main/java/gift/common/config/WebConfig.java
+++ src/main/java/gift/common/config/WebConfig.java
@@ -5,6 +5,7 @@ import gift.common.auth.LoginMemberArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,5 +31,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedHeaders("*")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("*");
     }
 }
