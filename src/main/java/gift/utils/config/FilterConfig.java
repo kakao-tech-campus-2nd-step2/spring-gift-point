b2c45aed181015bc0c30658409dd2fb6aeb2a523
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-08-01 15:59:19 +0900

    fix : cors filter 다시 삭제

--- src/main/java/gift/utils/config/FilterConfig.java
+++ src/main/java/gift/utils/config/FilterConfig.java
@@ -5,7 +5,6 @@ import gift.utils.ExternalApiService;
 import gift.utils.JwtTokenProvider;
 import gift.utils.filter.AuthFilter;
 import gift.utils.filter.OAuthFilter;
-import gift.utils.filter.CorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,20 +22,12 @@ public class FilterConfig {
         this.externalApiService = externalApiService;
     }
 
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new CorsFilter());
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setOrder(0); // CORS 필터를 가장 먼저 적용
-        return registrationBean;
-    }
 
     @Bean
     public FilterRegistrationBean<AuthFilter> authFilter() {
         FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new AuthFilter(jwtTokenProvider));
-        registrationBean.addUrlPatterns("/*");
+        registrationBean.addUrlPatterns("/*"); // 모든 경로에 필터 적용
         registrationBean.setOrder(1);
         return registrationBean;
     }
@@ -44,9 +35,11 @@ public class FilterConfig {
     @Bean
     public FilterRegistrationBean<OAuthFilter> oauthFilter() {
         FilterRegistrationBean<OAuthFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new OAuthFilter(jwtTokenProvider, tokenRepository, externalApiService));
-        registrationBean.addUrlPatterns("/*");
+        registrationBean.setFilter(new OAuthFilter(jwtTokenProvider,tokenRepository,externalApiService));
+        registrationBean.addUrlPatterns("/*"); // 모든 경로에 필터 적용
         registrationBean.setOrder(2);
         return registrationBean;
     }
+
+
 }
