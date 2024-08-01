e715d11d59f015a7038cb7cc863ac6854d5bb634
5win <94297900+5win@users.noreply.github.com>
2024-08-01 22:52:30 +0900

    feat: Change product URL and return type

--- src/main/java/gift/controller/ProductViewController.java
+++ src/main/java/gift/controller/ProductViewController.java
@@ -1,12 +1,7 @@
 package gift.controller;
 
-import gift.dto.ProductResponse;
 import gift.service.CategoryService;
 import gift.service.ProductService;
-import gift.utils.PageNumberListGenerator;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,25 +21,11 @@ public class ProductViewController {
     }
 
     /**
-     * 상품 목록을 보여주는 products.html 을 렌더링하여 반환
-     *
-     * @return products.html
-     */
-    @GetMapping()
-    public String getAllProducts(Model model, @PageableDefault(size = 5) Pageable pageable) {
-        Page<ProductResponse> products = productService.getAllProducts(pageable);
-
-        model.addAttribute("pageNumbers", PageNumberListGenerator.generatePageNumberList(products));
-        model.addAttribute("products", products);
-        return "products";
-    }
-
-    /**
      * 상품을 추가하는 페이지인 addForm.html 반환
      *
      * @return addForm.html
      */
-    @GetMapping("/product")
+    @GetMapping("/addForm")
     public String addProductForm(Model model) {
         model.addAttribute("categories", categoryService.getCategoryList());
         return "addForm";
@@ -56,7 +37,7 @@ public class ProductViewController {
      * @param id 수정할 상품의 id
      * @return editForm.html
      */
-    @GetMapping("/product/{id}")
+    @GetMapping("/editForm/{id}")
     public String editProductForm(@PathVariable("id") Long id, Model model) {
         model.addAttribute("product", productService.getProduct(id));
         model.addAttribute("categories", categoryService.getCategoryList());
--- src/main/java/gift/repository/ProductJpaDao.java
+++ src/main/java/gift/repository/ProductJpaDao.java
@@ -2,10 +2,13 @@ package gift.repository;
 
 import gift.entity.Product;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductJpaDao extends JpaRepository<Product, Long> {
 
     Optional<Product> findByName(String name);
 
+    Page<Product> findByCategory_Id(Long category, Pageable pageable);
 }
