3629e764e7ccd9b02511c9628353ce7d5598a225
hyunseo <wndlthsk@naver.com>
2024-08-01 15:20:35 +0900

    refactor : 위시 response 필드 변경

--- src/main/java/gift/dto/WishResponseDto.java
+++ src/main/java/gift/dto/WishResponseDto.java
@@ -4,20 +4,18 @@ import gift.domain.Wish;
 import java.time.LocalDateTime;
 
 public record WishResponseDto(
-    Long id,
-    String member_email,
-    String product_name,
-    int product_price,
-    LocalDateTime createdDate
+    Long wishId,
+    String name,
+    int price,
+    String imageUrl
     ) {
 
     public static WishResponseDto convertToDto(Wish wish) {
         return new WishResponseDto(
             wish.getId(),
-            wish.getMember().getEmail(),
             wish.getProduct().getName(),
             wish.getProduct().getPrice(),
-            wish.getCreatedDate()
+            wish.getProduct().getImageUrl()
         );
     }
 }
