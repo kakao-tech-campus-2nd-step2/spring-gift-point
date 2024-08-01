4efe0317bef48e20cd0b5bb49351e065b260e5cf
dong_yxxn <g0326kdy@gmail.com>
2024-08-02 00:55:25 +0900

    feat: root로 진입하면 관리자페이지 나오게함

--- src/main/java/gift/global/view/rootView.java
+++ src/main/java/gift/global/view/rootView.java
@@ -1,2 +1,20 @@
-package gift.global.view;public class rootView {
+package gift.global.view;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class rootView {
+    @GetMapping
+    public String showAdminPageGet(){
+        return "redirect:/admin/products";
+    }
+    @PostMapping
+    public String showAdminPagePost(){
+        return "redirect:/admin/products";
+    }
 }
