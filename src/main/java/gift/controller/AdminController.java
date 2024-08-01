f08d3f577999baf9b629c4247e1f3d9e9b65ae8e
ez23re <ez23re@gmail.com>
2024-08-01 14:18:22 +0900

    프론트 요청에 따라 api 수정

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -3,6 +3,8 @@ package gift.controller;
 import gift.dto.ProductDto;
 import gift.exception.ProductNotFoundException;
 import gift.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@Tag(name="관리자 화면 API")
 @RequestMapping("/api/admin")
 public class AdminController {
     private final ProductService productService;
@@ -18,6 +21,7 @@ public class AdminController {
         this.productService = productService;
     }
 
+    @Operation(summary = "상품 목록 화면", description = "list.html 을 보여줍니다.")
     @GetMapping("/products/list")
     public String listProducts(Model model) {
         List<ProductDto> products = productService.findAll();
@@ -25,6 +29,7 @@ public class AdminController {
         return "list"; // list.html 파일 보여주기
     }
 
+    @Operation(summary = "상품 상세 정보 화면", description = "view.html 을 보여줍니다.")
     @GetMapping("/products/view/{productId}")
     public String viewProduct(@PathVariable("id") Long id, Model model) {
         ProductDto product = productService.findById(id)
@@ -33,12 +38,14 @@ public class AdminController {
         return "view"; // view.html 파일 보여주기
     }
 
+    @Operation(summary = "상품 추가 화면", description = "add.html 을 보여줍니다.")
     @GetMapping("/products/add")
     public String showAddProductForm(Model model) {
         model.addAttribute("product", new ProductDto());
         return "add"; // add.html 파일 보여주기
     }
 
+    @Operation(summary = "상품 수정 화면", description = "edit.html 을 보여줍니다.")
     @GetMapping("/products/edit/{productId}")
     public String showEditProductForm(@PathVariable("id") Long id, Model model) {
         ProductDto product = productService.findById(id)
@@ -47,6 +54,7 @@ public class AdminController {
         return "edit"; // edit.html 파일 보여주기
     }
 
+    @Operation(summary = "상품 삭제", description = "해당 상품 삭제 후, 관리자 API로 리디렉션 ")
     @DeleteMapping("/products/{productId}")
     public String deleteProduct(@PathVariable("id") Long id) {
         productService.delete(id);
--- src/main/java/gift/controller/CategoryController.java
+++ src/main/java/gift/controller/CategoryController.java
@@ -2,11 +2,14 @@ package gift.controller;
 
 import gift.entity.Category;
 import gift.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Tag(name="카테고리 API")
 @RequestMapping("/api/categories")
 public class CategoryController {
 
@@ -16,6 +19,8 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    // 프론트 요청 : response(카테고리 정보 리스트 리턴해주세요)
+    @Operation(summary = "카테고리 목록 조회")
     @GetMapping
     public List<Category> getCategories() {
         return categoryService.findAll();
