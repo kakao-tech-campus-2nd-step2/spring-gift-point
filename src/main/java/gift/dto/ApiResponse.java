98a46ea28aa3ba85888d11133475aeff200b23ee
Kim Sanghae <pelikan@pusan.ac.kr>
2024-08-01 10:38:05 +0900

    feat: DTO 데이터 제네릭 이용

--- src/main/java/gift/dto/ApiResponse.java
+++ src/main/java/gift/dto/ApiResponse.java
@@ -7,12 +7,24 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Data
-@AllArgsConstructor
 @Getter
-public class ApiResponse {
+@AllArgsConstructor
+public class ApiResponse<T> {
 
     private HttpResult result;
     private String message;
     private HttpStatus httpStatus;
+    private T data;
 
+    public ApiResponse(HttpResult result, String message, HttpStatus httpStatus) {
+        this.result = result;
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    public ApiResponse(HttpResult result, T data, HttpStatus httpStatus) {
+        this.result = result;
+        this.data = data;
+        this.httpStatus = httpStatus;
+    }
 }
