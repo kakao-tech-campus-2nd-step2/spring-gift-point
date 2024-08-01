5b57be43c80318e26b25a3173700a54fb1c392cc
SeoYeongU <westzeroright@gmail.com>
2024-08-02 01:06:14 +0900

    fix: 동일한 이름 config 파일 에러 해결

--- src/main/java/gift/config/CorsConfig.java
+++ src/main/java/gift/config/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
