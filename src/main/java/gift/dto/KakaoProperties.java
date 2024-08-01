28462af4016f5b7e6e2b70e69f14937cb75faca7
Kim Sanghae <pelikan@pusan.ac.kr>
2024-08-01 10:42:42 +0900

    chore: 파일 패키지 이동

--- src/main/java/gift/dto/KakaoProperties.java
+++
@@ -1,9 +0,0 @@
-package gift.dto;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "kakao")
-public record KakaoProperties(
-    String clientId,
-    String redirectUrl
-) { }
