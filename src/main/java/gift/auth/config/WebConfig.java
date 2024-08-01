0b269a234d3cd0ce3ac9c805aade591925d233c6
dong_yxxn <g0326kdy@gmail.com>
2024-08-01 18:04:15 +0900

    refactor: cors 추가

--- src/main/java/gift/auth/config/WebConfig.java
+++ src/main/java/gift/auth/config/WebConfig.java
@@ -4,8 +4,10 @@ import gift.global.auth.resolver.LoginMemberArgumentResolver;
 import gift.user.service.UserService;
 import gift.utility.JwtUtil;
 import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,5 +21,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(userService));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // 모든 경로에 대해
+                    .allowedOrigins("*")  // 모든 오리진에 대해
+                    .allowedMethods("*")  // 모든 HTTP 메소드에 대해
+                    .allowedHeaders("*");
+            }
+        };
     }
 }
