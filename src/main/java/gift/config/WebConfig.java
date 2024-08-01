220f67c10475d73818744070fae604e4d921217e
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-08-02 00:16:18 +0900

    add: CORS 설정, react포트넘버 3000에 대해서 localhost에서 접속시 확인할 수 있도록 열어줌
    addCorsMapping 적용

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -2,6 +2,7 @@ package gift.config;
 
 import gift.controller.auth.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +22,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**")
                 .excludePathPatterns("/api/members/login/**")
                 .excludePathPatterns("/api/members/register");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
