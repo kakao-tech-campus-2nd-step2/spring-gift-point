bb5bb8b90270b53c3c5e258c6079b5fdfa78ab47
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-08-01 15:04:57 +0900

    fix : cors 문제 해결

--- src/main/java/gift/utils/config/WebConfig.java
+++ src/main/java/gift/utils/config/WebConfig.java
@@ -18,10 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
 //    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-            .allowedOrigins("")  // 모든 출처 허용
-            .allowedMethods("")  // 모든 HTTP 메서드 허용
-            .allowedHeaders("")  // 모든 헤더 허용
+        registry.addMapping("/**")
+            .allowedOrigins("*")  // 모든 출처 허용
+            .allowedMethods("*")  // 모든 HTTP 메서드 허용
+            .allowedHeaders("*")  // 모든 헤더 허용
             .maxAge(3600);  // pre-flight 요청 캐시 시간
     }
 
