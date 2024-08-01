66ddfce851a768b5b9d4e66a44174584bbfe2b1b
Dalsungmin <cnww123@naver.com>
2024-08-01 16:30:01 +0900

    Refactor: 레스트 컨트롤러로 변환
    기존 로그인기능을 사용하지 않고 카카오로그인 기능만을 사용하기로 정해서 특별한 수정 필요 없음

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -8,12 +8,12 @@ import java.util.Map;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
