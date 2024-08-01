e22b11744a575aaa6b0ff957e3d051806fa35219
peacefullyquietly <pjh73015@gmail.com>
2024-08-02 02:54:53 +0900

    refactor : 주문 목록에 대한 페이지네이션 추가

--- src/main/java/gift/controller/OrderController.java
+++ src/main/java/gift/controller/OrderController.java
@@ -1,21 +1,29 @@
 package gift.controller;
 
 import gift.dto.OrderDTO;
+import gift.dto.ProductDTO;
 import gift.service.JwtUtil;
 import gift.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @Tag(name = "상품 주문 API")
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/api/product/order")
 public class OrderController {
     private final OrderService orderService;
     private final JwtUtil jwtUtil;
@@ -26,7 +34,7 @@ public class OrderController {
         this.jwtUtil = jwtUtil;
     }
 
-    @PostMapping
+    @PostMapping("/{optionid}")
     @Operation(summary = "상품 주문", description = "상품 주문 시 카카오톡 메시지가 발송됩니다.")
     public ResponseEntity<?> addOrder(@RequestHeader("Authorization") String token, @RequestBody OrderDTO order) {
         String parsedToken = token.replace("Bearer ", "");
@@ -38,4 +46,20 @@ public class OrderController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @GetMapping
+    @Operation(summary = "주문 목록 조회", description = "해당 사용자의 주문 목록을 페이지네이션으로 조회합니다.")
+    public ResponseEntity<Page<OrderDTO>> getOrder(
+        @RequestHeader("Authorization") String token,
+        @Min(value = 1, message = "페이지 번호는 1이상이어야 합니다.")
+        @RequestParam(value = "page", defaultValue = "0") int page,
+        @Min(value = 1, message = "한 페이지당 1개 이상의 항목이 포함되어야 합니다.")
+        @RequestParam(value = "size", defaultValue = "10") int size) {
+
+        String parsedToken = token.replace("Bearer ", "");
+        Long userId = jwtUtil.extractUserId(parsedToken);
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<OrderDTO> orders = orderService.getUserOrders(userId, pageable);
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
 }
--- src/main/java/gift/repository/OrderRepository.java
+++ src/main/java/gift/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package gift.repository;
 
+import org.springframework.data.domain.Pageable;
 import gift.entity.OrderEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    Page<OrderEntity> findByUserId(Long userId, Pageable pageable);
 }
