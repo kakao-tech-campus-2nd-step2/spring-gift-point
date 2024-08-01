6bfb60fe6ca08310059ce0496d45eb7f90b0691d
Kim Sanghae <pelikan@pusan.ac.kr>
2024-08-01 10:18:29 +0900

    feat: 위시 리스트 조회 페이지네이션 추가

--- src/main/java/gift/wish/service/WishService.java
+++ src/main/java/gift/wish/service/WishService.java
@@ -1,12 +1,19 @@
-package gift.service;
+package gift.wish.service;
 
-import gift.model.Member;
-import gift.model.Product;
-import gift.model.Wish;
-import gift.repository.ProductRepository;
-import gift.repository.WishRepository;
+import gift.member.model.Member;
+import gift.product.model.Product;
+import gift.wish.model.Wish;
+import gift.product.repository.ProductRepository;
+import gift.wish.repository.WishRepository;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class WishService {
@@ -31,10 +38,21 @@ public class WishService {
         var product = productRepository.findById(productId)
             .orElseThrow(() -> new IllegalArgumentException("없는 상품입니다."));
         var wish = new Wish(product, member);
+        member.getWishList().add(wish);
         return wishRepository.save(wish);
     }
 
+    @Transactional
     public boolean removeWish(Long id, Member member) {
         return wishRepository.deleteByIdAndMember(id, member) > 0;
     }
+
+    public Page<Wish> getWishListPage(Member member,
+        int page, int pageSize, String sortProperty,
+        Direction direction
+    ) {
+        Pageable pageable = PageRequest.of(page, page, Sort.by(new Order(direction, sortProperty)));
+        return wishRepository.findAllByMember(pageable, member);
+    }
+
 }
