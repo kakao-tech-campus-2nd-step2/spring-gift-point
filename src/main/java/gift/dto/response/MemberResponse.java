ebf5c0474fce3250027d21658d44ca3d24eb2a62
wvssm <neoramiregi@gmail.com>
2024-08-01 21:45:03 +0900

    feat(MemberResponse): MemberResponse에 points 필드 추가

--- src/main/java/gift/dto/response/MemberResponse.java
+++ src/main/java/gift/dto/response/MemberResponse.java
@@ -2,8 +2,8 @@ package gift.dto.response;
 
 import gift.domain.Member;
 
-public record MemberResponse(Long id, String email, String password) {
+public record MemberResponse(Long id, String email, String password, int points) {
     public static MemberResponse from(final Member member){
-        return new MemberResponse(member.getId(), member.getEmail(), member.getPassword());
+        return new MemberResponse(member.getId(), member.getEmail(), member.getPassword(), member.getPoints());
     }
 }
