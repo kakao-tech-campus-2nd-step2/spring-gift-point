6048c7f8d07bb63ed2c23503fa956dc727d606eb
anacnoda77 <sm1108shin@naver.com>
2024-08-01 22:34:32 +0900

    feat: CORS 설정

--- src/main/java/gift/config/LoginWebConfig.java
+++ src/main/java/gift/config/LoginWebConfig.java
@@ -8,6 +8,7 @@ import gift.resolver.LoginMemberArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -42,4 +43,14 @@ public class LoginWebConfig implements WebMvcConfigurer {
         resolvers.add(new LoginMemberArgumentResolver());
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 서버에서 CORS를 적용할 uri 패턴
+            .allowedOriginPatterns("*") // CORS를 지원할 클라이언트 Origin
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*") // CORS에 허용되는 요청 헤더
+            .allowCredentials(true) // 클라이언트에 대한 응답에 credentials(인증 헤더, 쿠키)를 포함할 수 있는 여부
+            .exposedHeaders("Location") // 클라이언트 측에 전달되는 응답에 노출을 허용하게 하는 헤더
+            .maxAge(1800); // 예비 요청(options)의 결과값을 캐싱해놓을 수 있는 시간 (cross-origin 요청 시 캐싱에 의해 예비 요청 생략 가능)
+    }
 }
