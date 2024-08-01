fac7c10f31eb6d44b2cc2d828193e30254f36866
BOMIN LYU <rbm0524@naver.com>
2024-08-01 10:14:37 +0900

    refactor : 필드 수정

--- src/main/java/gift/Entity/Category.java
+++ src/main/java/gift/Entity/Category.java
@@ -10,25 +10,30 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long categoryId;
+    private long id;
     private String name;
+    private String description;
+    private String color;
+    private String imageUrl;
 
     protected Category() {
 
     }
 
-    public Category(long categoryId, String name) {
-        this.categoryId = categoryId;
+    public Category(long id, String name, String description, String color, String imageUrl) {
+        this.id = id;
         this.name = name;
+        this.description = description;
+        this.color = color;
+        this.imageUrl = imageUrl;
     }
 
-
-    public long getCategoryId() {
-        return categoryId;
+    public long getId() {
+        return id;
     }
 
-    public void setCategoryId(long categoryId) {
-        this.categoryId = categoryId;
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -37,5 +42,29 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
--- src/main/java/gift/Model/CategoryDto.java
+++ src/main/java/gift/Model/CategoryDto.java
@@ -4,14 +4,20 @@ public class CategoryDto {
 
     private long id;
     private String name;
+    private String description;
+    private String color;
+    private String imageUrl;
 
     public CategoryDto() {
 
     }
 
-    public CategoryDto(long id, String name) {
+    public CategoryDto(long id, String name, String description, String color, String imageUrl) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.color = color;
+        this.imageUrl = imageUrl;
     }
 
     public long getId() {
@@ -28,5 +34,29 @@ public class CategoryDto {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
