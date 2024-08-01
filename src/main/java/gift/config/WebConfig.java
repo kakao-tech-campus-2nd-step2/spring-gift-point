500c905f3e018e81d465a5b38b3c8cf296fe2806
나제법 <skwpqjq@gmail.com>
2024-08-01 22:32:56 +0900

    feat: WebConfig.java CORS 설정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -9,11 +9,14 @@ import java.util.List;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
     private final LoginMemberArgumentResolver loginUserArgumentResolver;
     private final JwtResolver jwtResolver;
 
@@ -47,5 +50,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public Client feignClient() {
         return new Client.Default(null, null);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+            .exposedHeaders(HttpHeaders.LOCATION);
     }
 }
