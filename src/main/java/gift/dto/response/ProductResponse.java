5da823fd9b3414b473dcda26e5e3bf3d99cb84f7
yooonwodyd <dbswodyd00@naver.com>
2024-08-01 22:17:45 +0900

    [feat] 요구사항 변경에 따라 Res 변경
    (cherry picked from commit f2db60b2ba7ec334e21209e1f18c12f25ff7e313)

--- src/main/java/gift/dto/response/ProductResponse.java
+++ src/main/java/gift/dto/response/ProductResponse.java
@@ -6,18 +6,20 @@ import java.util.stream.Collectors;
 import gift.product.domain.Product;
 
 public record ProductResponse(
+	Long id,
 	String name,
 	Long price,
-	String imageUrl,
-	String categoryName
+	String imageUrl
+	// String categoryName,
 	// List<OptionResponse> options
 ) {
 	static public ProductResponse from(Product product) {
 		return new ProductResponse(
+			product.getId(),
 			product.getName(),
 			product.getPrice(),
-			product.getImageUrl(),
-			product.getCategory().getName()
+			product.getImageUrl()
+			// product.getCategory().getName(),
 			// product.getOptions().stream()
 			// 	.map(OptionResponse::from)
 			// 	.collect(Collectors.toList())
