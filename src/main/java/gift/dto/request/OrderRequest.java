eb9cb16020529a339c0200e1081a5bb046df79e1
yooonwodyd <dbswodyd00@naver.com>
2024-08-01 22:17:08 +0900

    [feat] 요구사항 변경에 따른 Res,Req 수정
    (cherry picked from commit f9cba0b37427fb5ea4bd8b19d0111117086a076f)

--- src/main/java/gift/dto/request/OrderRequest.java
+++ src/main/java/gift/dto/request/OrderRequest.java
@@ -1,5 +1,9 @@
 package gift.dto.request;
 
+/*
+optionId, optionQuantity, senderId, receiverId, hasCashReceipt:boolean,cashReceiptType?: 'PERSONAL' | 'BUSINESS';
+  cashReceiptNumber?: string;, messageCardTextMessage: string;
+ */
 public record OrderRequest(
 	Long optionId,
 	Long quantity,
--- src/main/java/gift/dto/response/OptionResponse.java
+++ src/main/java/gift/dto/response/OptionResponse.java
@@ -5,17 +5,19 @@ import gift.product.domain.Option;
 public record OptionResponse(
 	Long id,
 	String name,
-	Long quantity
+	Long quantity,
+	Long productId
 ) {
-	public static OptionResponse of(Long id, String name, Long quantity) {
-		return new OptionResponse(id, name, quantity);
+	public static OptionResponse of(Long id, String name, Long quantity, Long productId) {
+		return new OptionResponse(id, name, quantity,productId);
 	}
 
 	public static OptionResponse from(Option option) {
 		return new OptionResponse(
 			option.getId(),
 			option.getName(),
-			option.getQuantity()
+			option.getQuantity(),
+			option.getProduct().getId()
 		);
 	}
 }
