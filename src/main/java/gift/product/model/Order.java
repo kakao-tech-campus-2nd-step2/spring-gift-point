7e59917fc09504b5a4579f045864f7e9d7dd9a48
Seunghyun Ko <chris00825@naver.com>
2024-08-01 20:07:36 +0900

    fix: resolve confilct
    기존 주문을 저장하는 테이블로 사용되었던 테이블의 삭제 시
    order예약어와 중복되어 충돌이 발생
    따라서 주문을 저장하는 테이블의 이름을 order에서 orders로 변경하여
    예약어와 중복을 피하면서 충돌을 해결

--- src/main/java/gift/product/model/Order.java
+++ src/main/java/gift/product/model/Order.java
@@ -7,10 +7,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Table(name = "orders")
 @Entity
 public class Order {
 
