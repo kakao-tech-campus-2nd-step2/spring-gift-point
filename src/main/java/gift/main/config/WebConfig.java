7ddcba5d5010b4bd92c48792d385120367f2f360
JinSeoHyun <15roa1121@gmail.com>
2024-08-01 20:16:22 +0900

    config : cors 설정

--- src/main/java/gift/main/config/WebConfig.java
+++ src/main/java/gift/main/config/WebConfig.java
@@ -4,6 +4,7 @@ import gift.main.interceptor.AuthInterceptor;
 import gift.main.resolver.SessionUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -32,6 +33,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(authLoginInterceptor)
                 .addPathPatterns("/api/orders/**", "/api/orders", "/api/wishes/**", "/api/wishes");
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 3000 포트에서 오는 요청 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowCredentials(true) // 쿠키를 사용할 경우
+                .allowedHeaders("*"); // 허용할 헤더
     }
 
 }
