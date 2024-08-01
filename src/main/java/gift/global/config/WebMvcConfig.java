a58374ce72c5faf1580bf850faa92d93f38ff707
JaeBin <201924576@pusan.ac.kr>
2024-08-01 15:12:30 +0900

    feat(global): add cors setting

--- src/main/java/gift/global/config/WebMvcConfig.java
+++ src/main/java/gift/global/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import gift.global.security.TokenManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -25,5 +26,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public HandlerMethodArgumentResolver authenticationPrincipalArgumentResolver() {
         return new AuthenticationPrincipalArgumentResolver(tokenManager);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 경로에 대해
+                .allowedOrigins("*")  // 모든 오리진에 대해
+                .allowedMethods("*")  // 모든 HTTP 메소드에 대해
+                .allowedHeaders("*");  // 모든 헤더에 대해
     }
 }
