6a3fdb384a561bc8e0a974b6f5f62cd54e1e1fe7
나제법 <skwpqjq@gmail.com>
2024-08-02 01:38:26 +0900

    refactor: CORS 설정 환경변수화

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -6,19 +6,25 @@ import gift.authentication.filter.AuthenticationFilter;
 import gift.authentication.token.JwtResolver;
 import gift.web.resolver.LoginMemberArgumentResolver;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-    public static final String EXPOSED_HEADERS = "Set-Cookie, Authorization, Content-Type, X-Requested-With, Accept, Origin, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Credentials";
+
+    @Value("${cors.exposed-headers}")
+    public String EXPOSED_HEADERS;
+
+    @Value("${cors.allowed-method-names}")
+    public String ALLOWED_METHOD_NAMES;
+
     private final LoginMemberArgumentResolver loginUserArgumentResolver;
+
     private final JwtResolver jwtResolver;
 
     public WebConfig(LoginMemberArgumentResolver loginUserArgumentResolver, JwtResolver jwtResolver) {
