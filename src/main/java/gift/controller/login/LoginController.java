15b2f6d70c73198e8ba04a9b8b168f856b6cabf8
Kiwoong Kim <gw150311@naver.com>
2024-08-01 19:20:08 +0900

    chore: 엔드포인트 URL 수정
    - API 명세서에 맞춤

--- src/main/java/gift/controller/login/LoginController.java
+++ src/main/java/gift/controller/login/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/api/members/login")
 public class LoginController {
 
     private final MemberService memberService;
