2df076d478a15c4c2ea2dd9af01d77d5324a0d6a
amm0124 <amm012450@gmail.com>
2024-08-01 21:47:31 +0900

    feat : 더미 데이터 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,7 +1,4 @@
-INSERT INTO member (email, password) VALUES
-                                         ('zz', '1'),
-                                         ('zzz', '2');
-
-INSERT INTO product (name, price, image_url) VALUES
-                                                 ('Product 1', 100, 'http://example.com/product1.jpg'),
-                                                 ('Product 2', 150, 'http://example.com/product2.jpg');
+-- 데이터 삽입 예제
+INSERT INTO category (id, name, description, color, image_url) VALUES
+                                                                   (2920, '생일', '감동을 높여줄 생일 선물 리스트', '#5949A3', 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png'),
+                                                                   (2930, '교환권', '놓치면 후회할 교환권 특가', '#9290C3', 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240131153049_5a22b137a8d346e9beb020a7a7f4254a.jpg');
