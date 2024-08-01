23300aac688c2cb5945de3af8e033efb97991ed4
Seonghun Jeong <tjdgns5506@gmail.com>
2024-08-01 13:43:57 +0900

    test: 카테고리 테스트 유지보수

--- src/test/java/gift/controller/api/CategoryControllerTest.java
+++ src/test/java/gift/controller/api/CategoryControllerTest.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -29,6 +31,7 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.http.HttpDocumentation.httpRequest;
 import static org.springframework.restdocs.http.HttpDocumentation.httpResponse;
@@ -105,27 +108,46 @@ class CategoryControllerTest {
         categoryResponses.add(new CategoryResponse(1L, "아이패드", "화이트", "pineappleipad.com", "아이패드 화이트"));
         categoryResponses.add(new CategoryResponse(2L, "티셔츠", "회색", "image.com", "회색 티셔츠"));
 
+        Page<CategoryResponse> page = new PageImpl<>(categoryResponses);
         //When
-        when(categoryService.getAllCategoryResponses()).thenReturn(categoryResponses);
+        when(categoryService.getAllCategoryResponsesByPageable(any())).thenReturn(page);
 
         //Then
         mockMvc.perform(MockMvcRequestBuilders.get(URL)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpectAll(
-                        status().isOk(),
-                        jsonPath("[0].id").value(1L),
-                        jsonPath("[1].id").value(2L)
+                        status().isOk()
                 )
                 .andDo(document("category-get",
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
 
+//                        responseFields(
+////                                fieldWithPath("[]").description("The array of categories"),
+////                                fieldWithPath("[].id").description("카테고리 ID").type(NUMBER),
+////                                fieldWithPath("[].name").description("카테고리 이름").type(STRING),
+////                                fieldWithPath("[].color").description("카테고리 색").type(STRING),
+////                                fieldWithPath("[].imageUrl").description("카테고리 이미지 주소").type(STRING),
+////                                fieldWithPath("[].description").description("카테고리 설명").type(STRING)
+//                        )
                         responseFields(
-                                fieldWithPath("[]").description("The array of categories"),
-                                fieldWithPath("[].id").description("카테고리 ID").type(NUMBER),
-                                fieldWithPath("[].name").description("카테고리 이름").type(STRING),
-                                fieldWithPath("[].color").description("카테고리 색").type(STRING),
-                                fieldWithPath("[].imageUrl").description("카테고리 이미지 주소").type(STRING),
-                                fieldWithPath("[].description").description("카테고리 설명").type(STRING)
+                                fieldWithPath("totalElements").description("사용 가능한 전체 요소 수"),
+                                fieldWithPath("totalPages").description("사용 가능한 전체 페이지 수"),
+                                fieldWithPath("first").description("이 페이지가 첫 페이지인지 여부를 나타내는 boolean 값"),
+                                fieldWithPath("last").description("이 페이지가 마지막 페이지인지 여부를 나타내는 boolean 값"),
+                                fieldWithPath("size").description("페이지당 요소 수"),
+                                fieldWithPath("content[]").description("카테고리 목록"),
+                                fieldWithPath("content[].id").description("카테고리의 ID"),
+                                fieldWithPath("content[].name").description("카테고리의 이름"),
+                                fieldWithPath("content[].color").description("카테고리의 색"),
+                                fieldWithPath("content[].imageUrl").description("카테고리 이미지의 URL"),
+                                fieldWithPath("content[].description").description("카테고리 설명"),
+                                fieldWithPath("number").description("현재 페이지 번호"),
+                                fieldWithPath("sort.empty").description("정렬이 비어 있는지 여부를 나타내는 boolean 값"),
+                                fieldWithPath("sort.sorted").description("결과가 정렬되어 있는지 여부를 나타내는 boolean 값"),
+                                fieldWithPath("sort.unsorted").description("결과가 정렬되지 않은 상태인지 여부를 나타내는 boolean 값"),
+                                fieldWithPath("numberOfElements").description("현재 페이지의 요소 수"),
+                                fieldWithPath("pageable").description("페이지네이션 정보"),
+                                fieldWithPath("empty").description("페이지가 비어 있는지 여부를 나타내는 boolean 값")
                         )
                 ));
     }
