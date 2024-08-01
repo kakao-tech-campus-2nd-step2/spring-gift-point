61133c20e13ede57dee08d091e4982a7c01f2f54
Kim Sanghae <pelikan@pusan.ac.kr>
2024-08-01 10:32:14 +0900

    feat: OrderRepository 생성

---
+++ src/main/java/gift/order/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package gift.order.repository;
+
+import gift.member.model.Member;
+import gift.order.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Page<Order> findAllByMember(Member member, Pageable pageable);
+}
