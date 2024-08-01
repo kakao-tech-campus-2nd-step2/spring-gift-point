00a4627ff3eeb5ce1a2f380f0f3e19a641423e37
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-08-01 15:23:24 +0900

    fix : cors 수정

--- src/main/java/gift/utils/config/WebConfig.java
+++ src/main/java/gift/utils/config/WebConfig.java
@@ -7,21 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//            .allowedOrigins("*") // 나중에 지정 예정
-//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//            .allowedHeaders("*");
-////            .allowCredentials(true)
-////            .exposedHeaders(HttpHeaders.LOCATION);
-//    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")  // 모든 출처 허용
             .allowedMethods("*")  // 모든 HTTP 메서드 허용
             .allowedHeaders("*")  // 모든 헤더 허용
+            .exposedHeaders("Location")
             .maxAge(3600);  // pre-flight 요청 캐시 시간
     }
 
