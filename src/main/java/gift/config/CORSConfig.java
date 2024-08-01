b7dc59d863a073822c6845d5ac258f17da930ecf
sanghee0820 <lsh277604@gmail.com>
2024-08-01 19:49:28 +0900

    feat: Cors 처리 Config추가

---
+++ src/main/java/gift/config/CORSConfig.java
@@ -0,0 +1,18 @@
+package gift.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CORSConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
