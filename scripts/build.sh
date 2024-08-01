6c239a71169156ddfacbb5230f41b6660555288a
GyuminBae <168344532+GyuminBae@users.noreply.github.com>
2024-08-01 20:53:32 +0900

    경북대 BE_배규민 6주차 과제(2단계) (#177)
    * fix: cors 문제 해결
    
    * chore: 빌드스크립트 추가
    
    * fix: gradle 빌드 명령어 수정
    
    ---------
    
    Co-authored-by: Gyumin Bae <bgmsound0528@gmail.com>

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+BUILD_PATH=$(ls ../build/libs/*.jar)
+JAR_NAME=$(basename "$BUILD_PATH")
+echo "> build 파일명: $JAR_NAME"
+CURRENT_PID=$(pgrep -f "$JAR_NAME")
+if [ -z "$CURRENT_PID" ]
+then
+  sleep 1
+else
+  echo "> 이미 실행 중인 애플리케이션이 존재하여 종료합니다.."
+  kill -9 "$CURRENT_PID"
+  sleep 5
+fi
+echo "> 프로젝트 변경 사항 로드 중.."
+git pull origin gyuminbae
+echo "> 프로젝트 빌드 중.."
+./gradlew bootJar
+
+DEPLOY_JAR=$BUILD_PATH
+echo "> 애플리케이션을 시작합니다."
+nohup java -jar "$DEPLOY_JAR" &
+tail -f nohup.out
