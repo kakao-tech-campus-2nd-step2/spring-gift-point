a9a75a74c2fb67cbe64195d27f4fd09866530eb1
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-08-01 23:24:59 +0900

    fix: 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ git pull
 echo "> build..."
 ./gradlew bootJar
 
-CURRENT_PID=$(pgrep -f *.jar)
+CURRENT_PID=$(pgrep -f .jar)
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 동중인 애플리케이션이 없으므로 종료하지 않습니다."
+        echo "> 현재 작동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-        echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
+        echo "> kill -9 $CURRENT_PID"
+        kill -9 $CURRENT_PID
         sleep 5
 fi
 
