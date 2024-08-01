706de31d3a1ae2418d1934da5e2e14ce5614304c
test1 <test>
2024-08-02 04:36:05 +0900

    feat : 배포 변경

--- src/main/java/gift/deploy.sh
+++ src/main/java/gift/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/spring-gift-point
+DEPLOY_PATH=/home/ubuntu/
 
-ehco "> 해당 repository로 이동"
-cd $REPOSITORY
+echo "> 해당 repository로 이동"
+cd "$REPOSITORY" || exit
 
 echo "> git pull origin step2"
 git pull origin step2
@@ -11,25 +12,21 @@ echo "> 프로젝트 build 시작"
 ./gradlew build
 
 echo "build 파일 복사"
-cp ./build/libs/*.jar $REPOSITORY
+BUILD_PATH=$(ls ./build/libs/*.jar)
+JAR_NAME=$(basename "$BUILD_PATH")
+cp "$BUILD_PATH" "$DEPLOY_PATH"
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-BUILD_PATH=$(ls $REPOSITORY/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_PATH)
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+CURRENT_PID=$(pgrep -f "$JAR_NAME")
 
-if [ -z $CURRENT_PID ]
-then
-  sleep 1
-else
-  kill -15 $CURRENT_PID
+if [ -n "$CURRENT_PID" ]; then
+  echo "> 현재 애플리케이션 종료"
+  kill -15 "$CURRENT_PID"
   sleep 5
+else
+  echo "> 현재 구동중인 애플리케이션 없음"
 fi
 
-DEPLOY_PATH=/home/ubuntu/
-cp $BUILD_PATH $DEPLOY_PATH
-cd $DEPLOY_PATH
-
-echo "> api실행"
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
+echo "> api 실행"
+DEPLOY_JAR="$DEPLOY_PATH$JAR_NAME"
+nohup java -jar "$DEPLOY_JAR" > "$DEPLOY_PATH/app.log" 2>&1 &
