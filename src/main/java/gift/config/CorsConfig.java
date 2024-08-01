053614cd0fd6ba4e0be4f6ba7d1e29751ede8e80
dkswoals <a4445656@gmail.com>
2024-08-01 16:08:46 +0900

    chore: 배포 전 cors 설정 추가

--- src/main/java/gift/config/CorsConfig.java
+++ src/main/java/gift/config/CorsConfig.java
@@ -2,10 +2,12 @@ package gift.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedMethods("*")
