2c3564e122b77010185523fc5c61c009ad91e84d
2iedo <162278702+2iedo@users.noreply.github.com>
2024-08-01 14:23:32 +0900

    refactor : allowedOrigin 전체로 변경

--- src/main/java/gift/Config/CorsConfig.java
+++ src/main/java/gift/Config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 //.exposedHeaders("Custom-Header")
