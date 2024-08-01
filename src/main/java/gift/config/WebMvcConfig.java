4acc89a1e0d60f0ecc315da10db892658f8cc2c7
amm0124 <amm012450@gmail.com>
2024-08-01 16:22:49 +0900

    FEAT : step2 cors 추가

--- src/main/java/gift/config/WebMvcConfig.java
+++ src/main/java/gift/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import gift.service.MemberService;
 import gift.service.WishService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -29,4 +30,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(wishService, memberRepository ,jwtUtil));
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
+                .allowCredentials(true); // 쿠키 인증 요청 허용
+    }
+
 }
