fe03399e6e057c57e7914599e3ae9728a1344218
sanghee0820 <lsh277604@gmail.com>
2024-08-01 15:27:46 +0900

    fix: exception handler Exception.class 삭제

--- src/main/java/gift/common/exception/CommonExceptionHandler.java
+++ src/main/java/gift/common/exception/CommonExceptionHandler.java
@@ -2,21 +2,12 @@ package gift.common.exception;
 
 import gift.common.util.ApiResponse;
 import gift.product.exception.kakao.KakaoApiException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CommonExceptionHandler {
-    
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<String>> handleException(Exception e) {
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생하였습니다."));
-    }
-
     @ExceptionHandler(KakaoApiException.class)
     public ResponseEntity<ApiResponse<String>> handleKakaoApiException(KakaoApiException e) {
 
