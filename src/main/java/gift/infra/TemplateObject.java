87147ddcbe8d9c50c32b1722ebe9dade97326e35
bokyeong <wntjdals0412@gmail.com>
2024-08-01 23:27:18 +0900

    Refactor: JsonNaming으로 필드 이름 설정

--- src/main/java/gift/infra/TemplateObject.java
+++ src/main/java/gift/infra/TemplateObject.java
@@ -2,12 +2,13 @@ package gift.infra;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import org.hibernate.validator.constraints.Length;
 
 @JsonFormat(shape = Shape.OBJECT)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record TemplateObject(
-        @JsonProperty("object_type")
         String objectType,
         @Length(min = 1, max = 200)
         String text,
@@ -17,9 +18,7 @@ public record TemplateObject(
     mobile_web_url : 모바일 카카오톡에서 사용하는 웹 링크 URL
      */
     public record Link(
-            @JsonProperty("web_url")
             String webUrl,
-            @JsonProperty("mobile_web_url")
             String mobileWebUrl) {
     }
 }
