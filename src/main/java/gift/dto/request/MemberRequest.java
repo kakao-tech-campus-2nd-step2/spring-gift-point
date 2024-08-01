ddd4438c6b782c44edf97229fe432929111c8cb8
wvssm <neoramiregi@gmail.com>
2024-08-01 21:44:21 +0900

    feat(MemberRequest): MemberRequest에 points 필드 추가

--- src/main/java/gift/dto/request/MemberRequest.java
+++ src/main/java/gift/dto/request/MemberRequest.java
@@ -2,8 +2,8 @@ package gift.dto.request;
 
 import gift.domain.Member;
 
-public record MemberRequest(Long id, String email, String password) {
+public record MemberRequest(Long id, String email, String password, int points) {
     public Member toEntity(){
-        return new Member(this.id, this.email, this.password);
+        return new Member(this.id, this.email, this.password, this.points);
     }
 }
