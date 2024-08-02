13b998c9e3ad9558c6b24062a8b71f0b4a768263
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-08-03 02:42:08 +0900

    feat : CORS 테스트 코드 수정

--- src/test/java/gift/AcceptanceTest.java
+++ src/test/java/gift/AcceptanceTest.java
@@ -22,16 +22,104 @@ public class AcceptanceTest {
     private MockMvc mockMvc;
 
     @Test
-    void cors() throws Exception {
+    void corsProducts() throws Exception {
         mockMvc.perform(
                 options("/api/products")
                     .header(HttpHeaders.ORIGIN, "http://localhost:8080")
                     .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "Content-Type, Authorization")
             )
             .andExpect(status().isOk())
-            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
-            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHOD_NAMES))
-            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Location"))
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:8080"))
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHOD_NAMES))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS,
+                "Authorization, Location"))
+            .andDo(print());
+    }
+
+    @Test
+    void corsWishes() throws Exception {
+        mockMvc.perform(
+                options("/api/wishes")
+                    .header(HttpHeaders.ORIGIN, "http://localhost:8080")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "Content-Type, Authorization")
+            )
+            .andExpect(status().isOk())
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:8080"))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET,POST,DELETE"))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization"))
+            .andDo(print());
+    }
+
+    @Test
+    void corsCategories() throws Exception {
+        mockMvc.perform(
+                options("/api/categories")
+                    .header(HttpHeaders.ORIGIN, "http://localhost:8080")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "Content-Type, Authorization")
+            )
+            .andExpect(status().isOk())
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:8080"))
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHOD_NAMES))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS,
+                "Authorization, Location"))
+            .andDo(print());
+    }
+
+    @Test
+    void corsOrders() throws Exception {
+        mockMvc.perform(
+                options("/api/orders")
+                    .header(HttpHeaders.ORIGIN, "http://localhost:8080")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "POST")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "Content-Type, Authorization")
+            )
+            .andExpect(status().isOk())
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:8080"))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "POST"))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization"))
+            .andDo(print());
+    }
+
+    @Test
+    void corsMembers() throws Exception {
+        mockMvc.perform(
+                options("/api/members/register")
+                    .header(HttpHeaders.ORIGIN, "http://localhost:8080")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "POST")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "Content-Type")
+            )
+            .andExpect(status().isOk())
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:8080"))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "POST"))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization"))
+            .andDo(print());
+    }
+
+    @Test
+    void corsOptions() throws Exception {
+        mockMvc.perform(
+                options("/api/products/1/options")
+                    .header(HttpHeaders.ORIGIN, "http://localhost:8080")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "Content-Type, Authorization")
+            )
+            .andExpect(status().isOk())
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:8080"))
+            .andExpect(
+                header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHOD_NAMES))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS,
+                "Authorization, Location"))
             .andDo(print());
     }
 }
