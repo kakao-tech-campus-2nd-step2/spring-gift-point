95b526fd961149361e8e18b0b7e02657a83b2b3d
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-08-02 21:49:14 +0900

    Refactor: 스크립트 무한루프에서 1번으로 변경

--- deploy.sh
+++ deploy.sh
@@ -4,35 +4,26 @@
 REPO_PATH="/home/ubuntu/spring-gift-point"
 cd $REPO_PATH
 
-while true # 무한 루프
-do
-    # git pull로 최신 변경 사항 가져오기
-    git pull
+# /home/ubuntu/spring-gift-point/build/libs/*.jar 에있는 jar 파일 변수설정
+BUILD_PATH=$(ls /home/ubuntu/spring-gift-point/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_PATH) # jar 파일 명 추출 ex) spring-gift-0.0.1-SNAPSHOT.jar
 
-  # /home/ubuntu/spring-gift-point/build/libs/*.jar 에있는 jar 파일 변수설정
-  BUILD_PATH=$(ls /home/ubuntu/spring-gift-point/build/libs/*.jar)
-  JAR_NAME=$(basename $BUILD_PATH) # jar 파일 명 추출 ex) spring-gift-0.0.1-SNAPSHOT.jar
+CURRENT_PID=$(pgrep -f $JAR_NAME) # JAR_NAME과 일치하는 프로세스 ID를 찾아 저장
 
-  CURRENT_PID=$(pgrep -f $JAR_NAME) # JAR_NAME과 일치하는 프로세스 ID를 찾아 저장
+if [ -z $CURRENT_PID ] # 비어있으면 1초 대기 후
+then
+ echo "실행중인 프로세스가 없습니다." # 터미널에 빌드 시작 출력
+else
+  kill -15 $CURRENT_PID # 비어있지 않으면 kill -15 명령어로 프로세스 종료 후 5초 대기
+  sleep 5 # 프로그램이 종료 될때 까지 대기
+fi
 
-  if [ -z $CURRENT_PID ] # 비어있으면 1초 대기 후
-  then
-   echo "실행중인 프로세스가 없습니다." # 터미널에 빌드 시작 출력
-  else
-    kill -15 $CURRENT_PID # 비어있지 않으면 kill -15 명령어로 프로세스 종료 훟 5초 대기
-    sleep 5 # 프로그램이 종료 될때 까지 대기
-  fi
+DEPLOY_PATH=/home/ubuntu/spring-gift-point/ # 쉘 스크립트 경로
+cp $BUILD_PATH $DEPLOY_PATH # jar파일을 $DEPLOY_PATH로 복사
 
-  DEPLOY_PATH=/home/ubuntu/spring-gift-point/ # 쉘 스크립트 경로
-  cp $BUILD_PATH $DEPLOY_PATH # jar파일을 $DEPLOY_PATH로 복사
+cd $DEPLOY_PATH # DEPLOY_PATH로 이동
 
-  cd $DEPLOY_PATH # DEPLOY_PATH로 이동
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME # 이동된 jar파일
 
-  DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME # 이동된 jar파일
-
-  # nohup 명령어로 터미널 세션이 종료돼도 계속 실행, & 명령어로 백그라운드에서 실행 /dev/null 2> /dev/null < /dev/null로그를 기록하지 않음
-  nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
-
-  # 60초마다 업데이트 확인
-  sleep 60
-done
+# nohup 명령어로 터미널 세션이 종료돼도 계속 실행, & 명령어로 백그라운드에서 실행 /dev/null 2> /dev/null < /dev/null로그를 기록하지 않음
+nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
