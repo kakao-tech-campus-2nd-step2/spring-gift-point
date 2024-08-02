b5d89df8bc28a58ea2e7e67f01b5d43e948acae9
youcaslte03 <luckthb325@naver.com>
2024-08-02 21:41:45 +0900

    fix:배포 스크립트 재작성

--- deploy.sh
+++ deploy.sh
@@ -13,16 +13,13 @@ echo ">Build 시작"
 ./gradlew bootJar
 
 echo ">디렉토리 이동"
-cd PROJECT_PATH
+cd $PROJECT_PATH
 
 echo ">Build 파일 복사"
 cp $PROJECT_PATH/$PROJECT_NAME/$BUILD_PATH/*.jar $PROJECT_PATH
 
 echo ">실행중 PID 확인"
-CURRENT_PID=$(pgrep -f JAR_NAME)
-
-if [ -z $CURRENT_PID ]; then
-        echo " >실행중인 애플리케이션이 없어서 바로 실행됩니다)
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]; then
         echo " >실행중인 애플리케이션이 없어서 바로 실행됩니다..\n"
@@ -33,5 +30,5 @@ else
 fi
 
 echo ">새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $PROJECT_PATH/ | grep *.jar | tail -n 1)
-nohup java -jar $PROJECT_PATH/$JAR_NAME &
+JAR_NAME=$(ls -tr $PROJECT_PATH/$PROJECT_NAME/$BUILD_PATH | grep *.jar | tail -n 1)
+nohup java -jar $JAR_NAME &
