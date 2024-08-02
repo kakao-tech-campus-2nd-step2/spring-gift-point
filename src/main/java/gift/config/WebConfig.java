852369f09f315c36d660c3ed8261b2a1d50007c6
NextrPlue <p.plue1881@gmail.com>
2024-08-02 19:02:54 +0900

    fix(config): add exposed header 'Location'

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -26,11 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .exposedHeaders("Location");
     }
 }
