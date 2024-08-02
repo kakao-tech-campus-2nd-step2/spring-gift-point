7772f2a22bb1accc3083e9cd2e6d8b4df7ab6abd
EunKyung Lee <81898507+pkyung@users.noreply.github.com>
2024-08-02 20:09:23 +0900

    충남대 BE_이은경 6주차 과제 (2단계) (#194)
    * Chore: 5주차 코드 이동
    
    * Chore: docs 수정
    
    * Docs: 기능 목록 작성 - step1
    
    * Feat: api 통일
    
    1. authController에 /api 추가함
    2. OptionResponse productId 제거
    3. Option에 toDto productId 제거
    4. wishes 조회 시 product 반환이 아닌 wishResponse 반환하도록 수정
    5. wishResponse에는 wishId를 추가함
    6. wish 삭제 시 wishId에 대해서 삭제되도록 수정함 (기존에 productId에
       대해서 수정했음)
    
    * Feat: 수정된 api에 대한 관리자 페이지 수정
    
    1. auth.js에는 /api 추가함
    2. script.js에는 category로 반환하는 productResponse에 대한 수정 작업합
    3. wish.js에는 delete route 수정과 wish 반환값 수정함
    
    * Feat: 상품 카테고리 별 조회 추가
    
    기존의 상품을 조회하는 api는 /all 을 추가하고 카테고리 별로 조회하는
    api는 쿼리 파라미터로 categoryId를 받아서 처리했다.
    
    * Docs: 기능 목록 구현 - step1
    
    * Fix: order 과정에서 카카오 로그인이 아닌 경우 처리
    
    기존의 코드에는 카카오 로그인이 아닌 경우, 주문 메시지 처리 과정에서
    에러가 났다.
    member 객체에 accessToken이 null인지 확인 후, null 이면 메시지 전송
    과정은 하지 않도록 처리했다.
    
    * Fix: route 수정
    
    * Fix: page 직렬화 반환 수정
    
    * Test: 테스트 api 수정에 따라 수정
    
    * Fix: cors 수정 및 data 추가 sql
    
    * Fix: sql 테스트 통과가 안되어 파일 삭제
    
    * Fix: categories url 실수 수정
    
    * Refactor: 디미터 법칙 위반된 것을 수정함
    
    Wish 도메인에 isOwnedBy 메서드를 추가하여 getId와 같은 메서드를 직접
    호출하지 않고 비교했습니다.
    
    * Refactor: 오류 해결

--- src/main/java/gift/controller/CategoryController.java
+++ src/main/java/gift/controller/CategoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api/categories/")
+@RequestMapping("/api/categories")
 @RestController
 public class CategoryController {
     private final CategoryService categoryService;
