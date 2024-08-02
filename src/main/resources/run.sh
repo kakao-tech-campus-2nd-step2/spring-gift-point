0082a09c22210ffd83907282d69b80c53d129ec6
daolove0323 <daolove0323@gmail.com>
2024-08-02 15:41:27 +0900

    feat: 배포스크립트 작성

---
+++ src/main/resources/run.sh
@@ -0,0 +1,14 @@
+cd ~/desktop/github
+ssh -tt  -i gift-key.pem ubuntu@43.203.228.50 <<EOF
+PROJECT_PATH=/home/ubuntu/repo/spring-gift-point
+cd \$PROJECT_PATH
+git pull
+
+export PROJECT_PATH=$PATH:/usr/bin:/bin
+killall -9 java
+
+./gradlew clean bootJar
+cd build/libs
+java -jar spring-gift-0.0.1-SNAPSHOT.jar &
+exit
+EOF
