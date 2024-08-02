98d546d3b90a5db178a4219c720d5acb4086a0a9
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-08-02 13:20:13 +0900

    rewrite: cors 설정 재설정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -27,9 +27,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedOrigins("http://localhost:3000", "localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .maxAge(1800);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
