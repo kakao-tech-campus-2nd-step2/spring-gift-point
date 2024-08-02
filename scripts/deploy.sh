7e38c5a14f7f4311a20039a8c5722077e89bc61a
sseooh <sseooh@gmail.com>
2024-08-02 10:15:27 +0900

    feat: 배포 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUILD_PATH=$(ls build/libs/*.jar)
+BUILD_PATH=$(ls /home/ubuntu/spring-gift-point/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_PATH)
 
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -14,10 +14,11 @@ else
   sleep 5
 fi
 
-DEPLOY_PATH=/home/ubuntu/my_project/app/
+DEPLOY_PATH=/home/ubuntu/spring-gift-point/app/
+mkdir -p $DEPLOY_PATH
 cp $BUILD_PATH $DEPLOY_PATH
 cd $DEPLOY_PATH
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "Deploying $DEPLOY_JAR"
-nohup java -jar $DEPLOY_JAR > /home/ubuntu/my_project/logs/nohup.out 2>&1 &
+nohup java -jar $DEPLOY_JAR > /home/ubuntu/spring-gift-point/build/libs/nohup.out 2>&1 &
