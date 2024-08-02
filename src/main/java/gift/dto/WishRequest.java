ee8e06f3645df0e5cbc3e189ba7f376a10290442
noojmogstel <goaudgo@naver.com>
2024-08-02 16:01:51 +0900

    refactor: WishRequest 빈 생성자 추가

--- src/main/java/gift/dto/WishRequest.java
+++ src/main/java/gift/dto/WishRequest.java
@@ -3,6 +3,9 @@ package gift.dto;
 public class WishRequest {
     private Long productId;
 
+    public WishRequest() {
+    }
+
     public WishRequest(Long productId) {
         this.productId = productId;
     }
