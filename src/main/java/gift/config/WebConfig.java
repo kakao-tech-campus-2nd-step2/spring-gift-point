ffca722be72091aed155fcc05996fa864ffa8431
cussle <cussle@kakao.com>
2024-08-02 23:37:33 +0900

    feat: CORS 관련 설정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -2,6 +2,8 @@ package gift.config;
 
 import gift.interceptor.MemberInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +21,16 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(memberInterceptor)
             .addPathPatterns("/api/wishes/**", "/oauth/kakao/**", "/api/orders/**")
             .excludePathPatterns("/oauth/kakao", "/oauth/kakao/callback");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+            .allowedHeaders("*")
+            .exposedHeaders(HttpHeaders.LOCATION)
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
