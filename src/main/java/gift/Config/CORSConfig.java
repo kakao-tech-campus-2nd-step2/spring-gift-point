0f9aecc0cc6429342d20ec64ab060e49dc323493
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-08-02 14:48:32 +0900

    Feat: CORSConfig 생성

---
+++ src/main/java/gift/Config/CORSConfig.java
@@ -0,0 +1,18 @@
+package gift.Config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*") // 모든 origin 추가
+            .allowedMethods("GET", "POST", "DELETE") // 프론트에서 쓰이는 허용할 HTTP 메서드
+            .allowedHeaders("*") // 모든 헤더를 허용
+            .allowCredentials(true) // Bearer token으로 인증을 하기 때문에 true
+            .maxAge(1800L); // 과제 힌트에서 1800초로 설정되어 있어 1800초로 설정
+
+    }
+}
