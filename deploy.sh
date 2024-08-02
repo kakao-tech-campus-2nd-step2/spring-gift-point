1a456e704293f5f1c66e1c7d5cba7fa1c2b4c9ce
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-08-02 22:34:47 +0900

    Refactor: 재수정...

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # /home/ubuntu/spring-gift-point/build/libs/*.jar 에있는 jar 파일 변수설정
-BUILD_PATH=$(ls /home/ubuntu/spring-gift-point/build/*.jar)
+BUILD_PATH=$(ls /home/ubuntu/spring-gift-point/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_PATH) # jar 파일 명 추출 ex) spring-gift-0.0.1-SNAPSHOT.jar
 
 CURRENT_PID=$(pgrep -f $JAR_NAME) # JAR_NAME과 일치하는 프로세스 ID를 찾아 저장
 
 if [ -z $CURRENT_PID ] # 비어있으면 1초 대기 후
 then
-  echo "빌드 시작" # 터미널에 빌드 시작 출력
+  echo "빌드 시작" # 터미널에 빌드 시작 출력top
   sleep 1
 else
   kill -15 $CURRENT_PID # 비어있지 않으면 kill -15 명령어로 프로세스 종료 후 5초 대기
