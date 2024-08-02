44897ce2977902bc61c903105600e351c5519f4e
jjh4450 <jjh4450git@gmail.com>
2024-08-02 11:45:09 +0900

    chore(Category): 프론트 요구 사항에 알맞게 수정함

--- src/main/java/gift/controller/CategoryController.java
+++ src/main/java/gift/controller/CategoryController.java
@@ -13,20 +13,20 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/category")
+@RequestMapping("/api")
 @Tag(name = "카테고리 api", description = "카테고리와 관련된 api 입니다")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @PostMapping
+    @PostMapping("/category")
     @Operation(summary = "카테고리 생성", description = "새로운 카테고리를 생성합니다.")
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         return ResponseEntity.ok(categoryService.createCategory(categoryDTO));
     }
 
-    @GetMapping
+    @GetMapping("/categories")
     @Operation(summary = "모든 카테고리 조회", description = "모든 카테고리 정보를 가져옵니다.")
     public ResponseEntity<List<CategoryDTO>> getCategories() {
         return ResponseEntity.ok(categoryService.getAllCategories());
