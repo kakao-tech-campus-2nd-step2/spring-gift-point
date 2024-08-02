8fbe8d4e2ebac375e4285ff863cbf48608a2d2b6
test1 <test>
2024-08-02 15:17:11 +0900

    feat : cors설정

--- src/main/java/gift/WebConfig.java
+++ src/main/java/gift/WebConfig.java
@@ -4,11 +4,25 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
   private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins("http://localhost:3000")
+      .allowedMethods("*")
+      .allowedOriginPatterns("*") //추가한 부분
+      .exposedHeaders("*");
+  }
 
   public WebConfig(LoginUserArgumentResolver loginUserArgumentResolver) {
     this.loginUserArgumentResolver = loginUserArgumentResolver;
