2c7ba245252ecdac9c993ea25b9517065902093e
sanghee0820 <lsh277604@gmail.com>
2024-08-02 14:28:56 +0900

    fix: CorsConfig 빈 추가

--- src/main/java/gift/config/CORSConfig.java
+++ src/main/java/gift/config/CORSConfig.java
@@ -1,8 +1,10 @@
 package gift.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CORSConfig implements WebMvcConfigurer {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
