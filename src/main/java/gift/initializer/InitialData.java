9ef1308c3161cd4b61cfec642ef85ce8ff936fee
Seonghun Jeong <tjdgns5506@gmail.com>
2024-08-02 15:52:35 +0900

    refacotr: 카테고리 이미지 변경

--- src/main/java/gift/initializer/InitialData.java
+++ src/main/java/gift/initializer/InitialData.java
@@ -33,18 +33,18 @@ public class InitialData implements CommandLineRunner {
     }
 
     private void initialCategory() {
-        categoryRepository.save(new Category("교환권", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("상품권", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("뷰티", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("패션", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("리빙/도서", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("레저/스포츠", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("아티스트/캐릭터", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("유아동/반려", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("디지털/가전", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("카카오프렌즈", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("트랜드 선물", "color", "imageUrl", "description"));
-        categoryRepository.save(new Category("백화점", "color", "imageUrl", "description"));
+        categoryRepository.save(new Category("교환권", "#FEE500", "https://images.unsplash.com/photo-1535241556859-780cb9f395f2?q=80&w=2825&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "교환권임"));
+        categoryRepository.save(new Category("상품권", "#FEE500", "https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "상품권임"));
+        categoryRepository.save(new Category("뷰티", "#FEE500", "https://images.unsplash.com/photo-1477764250597-dffe9f601ae8?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "뷰티임"));
+        categoryRepository.save(new Category("패션", "#FEE500", "https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "패션임"));
+        categoryRepository.save(new Category("리빙/도서", "#FEE500", "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?q=80&w=2946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "리빙,도서임"));
+        categoryRepository.save(new Category("레저/스포츠", "#FEE500", "https://plus.unsplash.com/premium_photo-1684820878202-52781d8e0ea9?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "레저,스포츠임"));
+        categoryRepository.save(new Category("아티스트/캐릭터", "#FEE500", "https://plus.unsplash.com/premium_vector-1721995308967-c7802e7361c9?q=80&w=3176&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "아티스트,캐릭터임"));
+        categoryRepository.save(new Category("유아동/반려", "#FEE500", "https://plus.unsplash.com/premium_vector-1682270497445-1d30983b683d?q=80&w=2916&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "유아동,반려임"));
+        categoryRepository.save(new Category("디지털/가전", "#FEE500", "https://plus.unsplash.com/premium_vector-1716910449787-ce65ae43d54e?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "디지털,가전임"));
+        categoryRepository.save(new Category("카카오프렌즈", "#FEE500", "https://plus.unsplash.com/premium_vector-1720833276380-436f053a77bd?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "카카오프렌즈"));
+        categoryRepository.save(new Category("트랜드 선물", "#FEE500", "https://plus.unsplash.com/premium_vector-1701795462492-d97da6d52d73?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "트렌드 선물"));
+        categoryRepository.save(new Category("백화점", "#FEE500", "https://plus.unsplash.com/premium_vector-1683121609098-3a7345febc0b?q=80&w=2716&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "백화점"));
     }
 
     private void initialProduct() {
