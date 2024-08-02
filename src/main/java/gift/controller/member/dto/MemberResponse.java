b9ab9de91dea573ad469a54204e4f36b48d98724
momnpa333 <momnpa333@gmail.com>
2024-08-02 16:34:34 +0900

    refact: member response 수정

--- src/main/java/gift/controller/member/dto/MemberResponse.java
+++ src/main/java/gift/controller/member/dto/MemberResponse.java
@@ -1,16 +1,18 @@
 package gift.controller.member.dto;
 
 import gift.application.member.dto.MemberModel;
+import gift.model.member.Role;
 
 public class MemberResponse {
 
     public record Login(
-        String name
+        String name,
+        Role role
     ) {
 
         public static Login from(MemberModel.InfoAndJwt model) {
 
-            return new Login(model.info().name());
+            return new Login(model.info().name(), model.info().role());
         }
     }
 
