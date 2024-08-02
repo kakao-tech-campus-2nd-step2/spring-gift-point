6a9045f67f8395706d5bf634d34c44b445021b10
2iedo <162278702+2iedo@users.noreply.github.com>
2024-08-02 14:48:26 +0900

    feat : 포인트 조회 컨트롤러 추가

--- src/main/java/gift/Controller/PointController.java
+++ src/main/java/gift/Controller/PointController.java
@@ -1,2 +1,22 @@
-package gift.Controller;public class PointController {
+package gift.Controller;
+
+import gift.Model.response.PointResponse;
+import gift.Service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/points")
+public class PointController {
+    private final UserService userService;
+
+    public PointController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public PointResponse read(String email){
+        return userService.read(email);
+    }
 }
