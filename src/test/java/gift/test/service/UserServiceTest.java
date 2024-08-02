4fb236fdd109155baefb7fc327f6f662751b4916
GamJaDo <kim0615157@gmail.com>
2024-08-03 08:16:44 +0900

    test (토큰 구분 방식 변경에 따른 test 코드 변경)

--- src/test/java/gift/test/service/UserServiceTest.java
+++ src/test/java/gift/test/service/UserServiceTest.java
@@ -1,7 +1,6 @@
 package gift.test.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
