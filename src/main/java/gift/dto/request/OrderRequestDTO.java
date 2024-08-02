23e7ed9aa0a89f77f58ef97b98397afc3429837a
amm0124 <amm012450@gmail.com>
2024-08-02 20:29:46 +0900

    refactoring : order request dto 변경

--- src/main/java/gift/dto/request/OrderRequestDTO.java
+++ src/main/java/gift/dto/request/OrderRequestDTO.java
@@ -2,5 +2,11 @@ package gift.dto.request;
 
 import java.time.LocalDateTime;
 
-public record OrderRequestDTO(Long optionId, Integer quantity, String message) {
+public record OrderRequestDTO(Long optionId,
+                              Integer quantity,
+                              String message,
+                              Long productId,
+                              Long point,
+                              String phone,
+                              boolean receipt) {
 }
