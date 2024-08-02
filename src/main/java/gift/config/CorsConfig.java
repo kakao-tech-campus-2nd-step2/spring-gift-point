d9c7c124e04f1dbdef0accce8c7a7c760c341d77
dkswoals <a4445656@gmail.com>
2024-08-02 11:17:39 +0900

    fix: Cors 설정 변경
    허용 출처 제한

--- src/main/java/gift/config/CorsConfig.java
+++ src/main/java/gift/config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedMethods("*")
-            .allowedOrigins("*");
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedOrigins("http://localhost:3000")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
