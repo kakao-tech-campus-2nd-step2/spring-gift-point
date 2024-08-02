fca279d4004575e43a379e34e27fe23982dc1de9
Lee Hyungi <geist9110@naver.com>
2024-08-02 17:42:23 +0900

    feat: Add OpenAPIConfig about Authorization in swagger

---
+++ src/main/java/gift/config/OpenAPIConfig.java
@@ -0,0 +1,29 @@
+package gift.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String securitySchemeName = "token";
+        return new OpenAPI()
+            .components(
+                new Components()
+                    .addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            );
+    }
+}
--- src/main/java/gift/wishlist/WishlistController.java
+++ src/main/java/gift/wishlist/WishlistController.java
@@ -1,9 +1,13 @@
 package gift.wishlist;
 
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.HEADER;
+
 import gift.product.dto.ProductPaginationResponseDTO;
 import gift.wishlist.dto.ProductIdRequestDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -29,7 +33,12 @@ public class WishlistController {
     }
 
     @GetMapping
-    @Operation(summary = "위시리스트 조회", description = "사용자의 토큰을 통해 위시리스트를 조회합니다.")
+    @Operation(
+        summary = "위시리스트 조회",
+        description = "사용자의 토큰을 통해 위시리스트를 조회합니다.",
+        security = {@SecurityRequirement(name = "token")},
+        parameters = {@Parameter(name = "Authorization", in = HEADER, allowEmptyValue = true)}
+    )
     @ApiResponse(responseCode = "200", description = "조회 성공")
     @ApiResponse(responseCode = "400", description = "입력 양식이 잘못된 경우")
     @ApiResponse(responseCode = "403", description = "인증 실패")
@@ -42,7 +51,12 @@ public class WishlistController {
     }
 
     @PostMapping
-    @Operation(summary = "위시리스트 추가", description = "사용자의 위시리스트에 상품을 추가합니다.")
+    @Operation(
+        summary = "위시리스트 추가",
+        description = "사용자의 위시리스트에 상품을 추가합니다.",
+        security = {@SecurityRequirement(name = "token")},
+        parameters = {@Parameter(name = "Authorization", in = HEADER, allowEmptyValue = true)}
+    )
     @ApiResponse(responseCode = "200", description = "정상")
     @ApiResponse(responseCode = "400", description = "상품이 존재하지 않는 경우")
     @ApiResponse(responseCode = "400", description = "요청 양식이 잘못된 경우")
@@ -56,7 +70,12 @@ public class WishlistController {
     }
 
     @DeleteMapping("/{product_id}")
-    @Operation(summary = "위시리스트 삭제", description = "사용자의 위시리스트에서 해당 상품을 제거합니다.")
+    @Operation(
+        summary = "위시리스트 삭제",
+        description = "사용자의 위시리스트에서 해당 상품을 제거합니다.",
+        security = {@SecurityRequirement(name = "token")},
+        parameters = {@Parameter(name = "Authorization", in = HEADER, allowEmptyValue = true)}
+    )
     @ApiResponse(responseCode = "200", description = "성공")
     @ApiResponse(responseCode = "400", description = "상품이 존재하지 않는 경우")
     @ApiResponse(responseCode = "400", description = "해당 상품의 위시리스트가 존재하지 않는 경우")
