c609437783e51a64545583e1a7aa859f4ea79217
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-08-02 22:05:37 +0900

    Refactor: 스크립트 재변경...

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ CURRENT_PID=$(pgrep -f $JAR_NAME) # JAR_NAME과 일치하는 프로세스 ID를 
 
 if [ -z $CURRENT_PID ] # 비어있으면 1초 대기 후
 then
- echo "빌드 시작" # 터미널에 빌드 시작 출력
+  echo "빌드 시작" # 터미널에 빌드 시작 출력
+  sleep 1
 else
   kill -15 $CURRENT_PID # 비어있지 않으면 kill -15 명령어로 프로세스 종료 후 5초 대기
   sleep 5 # 프로그램이 종료 될때 까지 대기
@@ -20,10 +21,9 @@ fi
 
 DEPLOY_PATH=/home/ubuntu/spring-gift-point/ # 쉘 스크립트 경로
 cp $BUILD_PATH $DEPLOY_PATH # jar파일을 $DEPLOY_PATH로 복사
-
 cd $DEPLOY_PATH # DEPLOY_PATH로 이동
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME # 이동된 jar파일
 
 # nohup 명령어로 터미널 세션이 종료돼도 계속 실행
-nohup java -jar $DEPLOY_JAR > app.log 2> error.log < /dev/null &
+nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
