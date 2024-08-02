fefa93fc3e7c5a4155732331a279a9eceb9dc575
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-08-02 13:27:59 +0900

    feat: '/'에 매핑되는 homepage 추가

--- src/main/java/gift/controller/KakaoController.java
+++ src/main/java/gift/controller/KakaoController.java
@@ -9,10 +9,12 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
+@RequestMapping("/kakao")
 public class KakaoController {
 
     private final KakaoService kakaoService;
