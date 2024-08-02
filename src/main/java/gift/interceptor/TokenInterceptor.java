181f6557a5ff7ad30d4bcd081260e2e8e9f3e6a7
wvssm <neoramiregi@gmail.com>
2024-08-02 14:09:19 +0900

    feat(TokenInterceptor): OPTIONS 메서드일 때는 허용하도록 interceptor 수정

--- src/main/java/gift/interceptor/TokenInterceptor.java
+++ src/main/java/gift/interceptor/TokenInterceptor.java
@@ -20,6 +20,10 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
