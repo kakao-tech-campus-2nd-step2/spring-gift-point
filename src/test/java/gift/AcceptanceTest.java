d128aca9f81b6477c996497107ba49b9b808ff18
Hyun-Seo Jeong <90139789+hynseoj@users.noreply.github.com>
2024-08-02 22:01:15 +0900

    충남대 BE_정현서 6주차 과제 (2단계) (#284)
    * rename: user -> member로 변경
    
    * docs: 기능 요구사항 목록 작성
    
    * feat: 팀 내 API 명세서 작성
    
    * refactor: 회원 API 명세에 맞게 수정
    
    * refactor: 상품 API 명세에 맞게 수정
    
    * refactor: 주문 API 명세에 맞게 수정
    
    * refactor: 위시리스트 API 명세에 맞게 수정
    
    * refactor: 상품 카테고리별 조회 기능 구현
    
    * refactor: pageable에 `@ParameterObject` 어노테이션 추가
    
    * docs: 구현 완료 목록 체크
    
    * chore: build 세팅 변경
    
    * chore: 배포 관련 파일 작성
    
    * feat: cors 설정
    
    * feat: 배포 스크립트 작성
    
    * docs: 2단계 기능 요구사항 목록 작성

---
+++ src/test/java/gift/AcceptanceTest.java
@@ -0,0 +1,37 @@
+package gift;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AcceptanceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static final String ALLOWED_METHOD_NAMES = "GET,POST,PUT,DELETE,OPTIONS";
+
+    @Test
+    void cors() throws Exception {
+        mockMvc.perform(
+                options("/api/products")
+                    .header(HttpHeaders.ORIGIN, "http://localhost:3000")
+                    .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+            )
+            .andExpect(status().isOk())
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:3000"))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHOD_NAMES))
+            .andDo(print())
+        ;
+    }
+}
