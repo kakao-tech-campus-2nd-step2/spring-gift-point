9859c9821260952527b4b409d16b881af4b310c7
Lee Hyungi <geist9110@naver.com>
2024-08-03 04:27:21 +0900

    refactor: Remove Deprecate annotaion in AdminPageController

--- src/main/java/gift/admin/AdminPageController.java
+++ src/main/java/gift/admin/AdminPageController.java
@@ -3,14 +3,12 @@ package gift.admin;
 import gift.category.CategoryService;
 import gift.product.ProductService;
 import gift.product.dto.ProductPaginationResponseDTO;
-import gift.product.dto.ProductResponseDTO;
 import java.util.stream.IntStream;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AdminPageController {
@@ -26,21 +24,19 @@ public class AdminPageController {
         this.categoryService = categoryService;
     }
 
-    @Deprecated
-    @GetMapping(path = "/admin/{categoryId}")
+    @GetMapping(path = "/admin")
     public String adminPage(
         Model model,
-        Pageable pageable,
-        @PathVariable long categoryId
+        Pageable pageable
     ) {
-        Page<ProductPaginationResponseDTO> products = productService.getAllProducts(pageable, categoryId);
+        Page<ProductPaginationResponseDTO> products = productService.getAllProducts(pageable);
 
         model.addAttribute("products", products);
-        model.addAttribute("page", pageable.getPageNumber() + 1);
+        model.addAttribute("page", pageable.getPageNumber());
         model.addAttribute("totalProductsSize", products.getTotalElements());
         model.addAttribute("currentPageProductSize", products.get().toList().size());
         model.addAttribute("pageLists",
-            IntStream.range(1, products.getTotalPages() + 1).boxed().toList());
+            IntStream.range(0, products.getTotalPages()).boxed().toList());
         model.addAttribute("categories", categoryService.getAllCategories());
 
         return "/product/page";
