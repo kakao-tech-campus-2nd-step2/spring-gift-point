48858d864fa9123aba5e1423211c65a3c57ca706
Lee Hyungi <geist9110@naver.com>
2024-08-02 17:42:47 +0900

    feat: Add WebConfig for CORS Error

---
+++ src/main/java/gift/config/WebConfig.java
@@ -0,0 +1,38 @@
+package gift.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/orders")
+                    .allowedOriginPatterns("*")
+                    .allowedMethods("POST")
+                    .allowedHeaders("Authorization")
+                    .allowCredentials(true);
+
+                registry.addMapping("/members/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("POST");
+
+                registry.addMapping("/api/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET");
+
+                registry.addMapping("/wishes/**")
+                    .allowedOriginPatterns("*")
+                    .allowedMethods("GET", "POST", "DELETE")
+                    .allowedHeaders("Authorization")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
