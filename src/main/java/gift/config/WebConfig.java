8f44e118fe1779545d9507c2966a2c880a7a0663
GamJaDo <kim0615157@gmail.com>
2024-08-03 08:10:02 +0900

    feat (cors)

---
+++ src/main/java/gift/config/WebConfig.java
@@ -0,0 +1,18 @@
+package gift.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        		.allowedOrigins("http://http://13.124.128.255:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
