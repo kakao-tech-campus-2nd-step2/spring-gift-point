955a1e9259ad516fb759541fcb6a017d7785c1d6
BOMIN LYU <rbm0524@naver.com>
2024-08-02 22:28:28 +0900

    fix : CORS 오류 해결

--- src/main/java/gift/Config/WebConfig.java
+++ src/main/java/gift/Config/WebConfig.java
@@ -4,6 +4,7 @@ import gift.Annotation.LoginMemberArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,6 +18,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberResolverHandlerMethodArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {    // CORS 설정
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://geongyu09-step1--gorgeous-chebakia-afa13a.netlify.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
