d200ba0e6cca9ca3a15accd9b3d16634067fa2ce
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-08-02 17:44:15 +0900

    feat: Setting CORS

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -3,6 +3,7 @@ package gift.config;
 import gift.component.LoginMemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,6 +20,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(1800);
     }
 
 }
