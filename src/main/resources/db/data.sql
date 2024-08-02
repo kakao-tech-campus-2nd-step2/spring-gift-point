ede649a53fdbe60b34bbed8ca9f8d32455bbc506
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-08-02 10:27:49 +0900

    refactor : 명세서에 맞게 db 수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,17 +1,17 @@
-INSERT INTO category (name)
-VALUES ('교환권'),
-       ('백화점');
+INSERT INTO category (name, color, image_url, description)
+VALUES ('교환권', '#00FF00', '예시1 Image Url', ''),
+       ('백화점', '#00FF00', '예시1 Image Url', '');
 
 INSERT INTO product (name, price, category_id, image_url)
 VALUES ('예시1', '100', 1, '예시1 Image Url'),
        ('예시2', '200', 2, '예시2 Image Url');
 
-INSERT INTO member (name, email, password, role)
-VALUES ('관리자', 'admin@email.com', 'password', 'admin');
-INSERT INTO member (name, email, password, role)
-VALUES ('사용자', 'member@email.com', 'password', 'user');
-INSERT INTO member (name, email, password, role)
-VALUES ('카카오', 'kakaouser@email.com', 'password', 'kakaouser');
+INSERT INTO member (email, password, role)
+VALUES ('admin@email.com', 'password', 'admin');
+INSERT INTO member (email, password, role)
+VALUES ('member@email.com', 'password', 'user');
+INSERT INTO member (email, password, role)
+VALUES ('kakaouser@email.com', 'password', 'kakaouser');
 
 INSERT INTO options (name, quantity, product_id)
 VALUES ('임시 옵션1', 1, 1),
