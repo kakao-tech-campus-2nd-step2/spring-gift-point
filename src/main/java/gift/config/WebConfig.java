afd5972f945072c09a6b0e999b45fdb09be35bd7
Kiwoong Kim <gw150311@naver.com>
2024-08-02 17:38:09 +0900

    chore: config 수정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -16,9 +16,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                     .allowedOrigins("http://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true)
-                    .maxAge(3600);
+                    .allowedHeaders("*");
             }
         };
     }
