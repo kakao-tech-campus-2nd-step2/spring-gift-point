159be09bd05890b654cc43f24d38aed5a289b754
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-08-02 16:53:08 +0900

    feat : deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,17 @@ JAR_NAME=$(basename $BUILD_PATH)
 
 # GitHub에서 최신 코드 가져오기
 if [ ! -d "$PROJECT_PATH" ]; then
-  echo "Cloning repository..."
-  git clone $GITHUB_REPO $PROJECT_PATH
+  echo "깃허브 저장소 클론"
+  git clone --branch $GIT_BRANCH $GIT_REPOSITORY $PROJECT_PATH
+else
+  cd $PROJECT_PATH
+  git fetch origin
+  git checkout $BRANCH
+  git pull origin $BRANCH
 fi
 
-cd $PROJECT_PATH
-git fetch origin
-git checkout $BRANCH
-git pull origin $BRANCH
-
 # 빌드
+cd $PROJECT_PATH
 ./gradlew build
 
 # 현재 실행 중인 애플리케이션 PID 확인
@@ -29,10 +30,10 @@ CURRENT_PID=$(pgrep -f $JAR_NAME)
 # 애플리케이션이 실행 중인 경우 종료
 if [ -z $CURRENT_PID ]
 then
-  echo "No application is running"
+  echo "실행중인 애플리케이션 없음"
   sleep 1
 else
-  echo "Killing application with PID: $CURRENT_PID"
+  echo "PID로 애플리케이션 죽이기: $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
@@ -44,7 +45,7 @@ cd $DEPLOY_PATH
 
 # 새로운 애플리케이션 실행
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "Deploying $DEPLOY_JAR"
+echo "배포중 $DEPLOY_JAR"
 nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
 
-echo "Deployment finished"
+echo "배포 완료"
