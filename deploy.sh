0e33acbbeb49586ceb8a7572edeb70a6a45d4b06
youcaslte03 <luckthb325@naver.com>
2024-08-02 20:03:49 +0900

    fix:배포스크립트 재작성

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
-BUILD_PATH=$(ls /home/ubuntu/step2/spring-gift-point/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_PATH)
+PROJECT_PATH=home/ubuntu/step2
+PROJECT_NAME=spring-gift-point
+BUILD_PATH=build/libs
 
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+cd $PROJECT_PATH/$PROJECT_NAME
 
-if [ -z $CURRENT_PID ]
-then
-  sleep 1
+echo ">Git pull"
+git pull
+
+echo ">Build 시작"
+./gradlew bootJar
+
+echo ">디렉토리 이동"
+cd PROJECT_PATH
+
+echo ">Build 파일 복사"
+cp $PROJECT_PATH/$PROJECT_NAME/$BUILD_PATH/*.jar $PROJECT_PATH
+
+echo ">실행중 PID 확인"
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+
+if [ -z $CURRENT_PID ]; then
+        echo " >실행중인 애플리케이션이 없어서 바로 실행됩니다..\n"
 else
-  kill -15 $CURRENT_PID
-  sleep 5
+        echo "실행중인 애플리케이션이 있어서 이를 종료합니다. [PID = $CURRENT_PID]\n"
+        kill -15 $CURRENT_PID
+        sleep 5
 fi
 
-DEPLOY_PATH=/home/ubuntu/
-cp $BUILD_PATH $DEPLOY_PATH
-cd $DEPLOY_PATH
-
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
+echo ">새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $PROJECT_PATH/ | grep *.jar | tail -n 1)
+nohub java -jar $PROJECT_PATH/$JAR_NAME 2>&1 &
