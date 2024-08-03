8f8cf79f9a28a4eceba007d2935d7043124a4a0c
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-08-03 11:13:24 +0900

    Feat: 어노테이션 추가

--- src/main/java/gift/Config/CORSConfig.java
+++ src/main/java/gift/Config/CORSConfig.java
@@ -1,8 +1,10 @@
 package gift.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CORSConfig implements WebMvcConfigurer {
 
     @Override
