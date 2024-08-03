d1cf1e831d4c4863937733d1448e439e6a5e12aa
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-08-03 14:30:13 +0900

    전남대 BE_정호성 6주차 과제 (2단계) (#261)
    * docs: readme 작성
    
    * refactor: globalExceptionHandler 구조 변경, exception 패키지 세분화
    
    * refactor: 세부 예외들 package위치 변경, 각 서비스에서 예외 호출 일관성 적용
    
    * feat: API 명세에 맞춰서 Member Login 수정
    
    * feat: API 명세에 맞춰서 Category 수정
    *duplicated 예외 추가 (entity에서 unique로 예외를 잡는 것은 500 internal 에러가 나타남)
    *request DTO, response DTO 분리가 필요해 분리 진행
    *카테고리 삽입과 수정은 인증이 필요하므로 추가
    
    * refactor: 멤버 회원가입시, 중복 확인 예외 추가
    
    * feat: API 명세에 맞춰서 Product 수정
    *Request Response DTO 분리
    *제품 조회시 Category값을 선택적으로 받게 구현
    
    * feat: API 명세에 맞춰서 OPtion 수정
    * Request, Response DTO 분리
    
    * feat: API 명세에 맞춰서 Wish수정
    * Request, Response DTO 분리
    * Wish에 count 삭제, 이에 따라 update 삭제
    * Wish를 삭제할 때, product id로 삭제하는 endpoint를 wish id로 변경
    
    * feat: API 명세에 맞춰서 Order 수정
    * Request, Response DTO 분리
    * GET과 POST 요청에 동일하게 처리 (https://dejavuhyo.github.io/posts/spring-receive-get-post-same-time/)
    
    * feat: illegalexception들 전부 사용자정의 예외로 변경
    
    * feat: cors 문제 해결
    
    * fix: allowCredentials
    
    ---------
    
    Co-authored-by: yoong-saks <wjdghtjd06@gmail.com>

--- src/main/java/gift/web/WebConfig.java
+++ src/main/java/gift/web/WebConfig.java
@@ -6,6 +6,7 @@ import gift.web.jwt.JwtUtils;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -21,5 +22,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthUserArgumentResolver(memberService, jwtUtils));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .maxAge(3600)
+            .allowedHeaders("*")
+            .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE");
+
     }
 }
