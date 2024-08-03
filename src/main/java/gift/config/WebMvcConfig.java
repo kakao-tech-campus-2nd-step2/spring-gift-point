717c055c728c44ff5012828ebd9c7fbc51324cae
Lucerna00 <pjs3859811@naver.com>
2024-08-03 22:37:33 +0900

    feat(WebMvcConfig.java): set up CORS

--- src/main/java/gift/config/WebMvcConfig.java
+++ src/main/java/gift/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import gift.argumentresolver.LoginMemberArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +19,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Authorization", "Content-Type");
     }
 }
