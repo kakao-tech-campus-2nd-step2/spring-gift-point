42e72eaac8ee4f6fdab970422a43a2d414cfb472
KimSongMok <51540711+kimsongmok@users.noreply.github.com>
2024-08-03 21:08:54 +0900

    강원대 BE_김송목 6주차 과제(2단계) (#247)
    * feat: add gift.sh, deploy complete
    
    * fix: fix callback api
    
    * fix: fix callback

--- src/main/java/gift/controller/AuthController.java
+++ src/main/java/gift/controller/AuthController.java
@@ -1,5 +1,6 @@
 package gift.controller;
 
+import gift.dto.KakaoCallbackResponseDto;
 import gift.dto.OrderDto;
 import gift.service.AuthService;
 import gift.service.OrderService;
@@ -8,6 +9,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -36,23 +39,13 @@ public class AuthController {
     return new RedirectView(authorizationUrl);
   }
 
-  @Operation(summary = "토큰 콜백", description = "카카오 인증 후 콜백을 처리하여 사용자 정보를 홈페이지로 리다이렉트합니다.")
+  @Operation(summary = "카카오 로그인 콜백 처리", description = "카카오 로그인 콜백을 처리하고 JWT 토큰과 카카오 액세스 토큰을 반환합니다.")
   @GetMapping("/callback")
-  public RedirectView callback(@RequestParam String code) {
-    String token = authService.getToken(code);
-    String userInfo = authService.getKakaoUserInfo(token).toString();
-    return new RedirectView("/home?userInfo=" + userInfo + "&kakaoAccessToken=" + token);
+  public ResponseEntity<KakaoCallbackResponseDto> handleKakaoCallback(
+          @RequestParam("jwtToken") String jwtToken,
+          @RequestParam("kakaoAccessToken") String kakaoAccessToken) {
+    KakaoCallbackResponseDto responseDto = new KakaoCallbackResponseDto(jwtToken, kakaoAccessToken);
+    return ResponseEntity.status(HttpStatus.OK).body(responseDto);
   }
 
-  @Operation(summary = "주문 목록 조회 (페이지네이션 적용)", description = "카카오 로그인 콜백 처리하고 토큰 반환합니다.")
-  @GetMapping("/callback/orders")
-  public List<OrderDto> getOrderListWithPagination(@RequestHeader("Authorization") String authorization,
-                                                   @RequestHeader("Kakao-Access-Token") String kakaoAccessToken,
-                                                   @RequestParam int page) {
-    String token = authorization.replace("Bearer ", "");
-    Long memberId = jwtUtil.getMemberIdFromToken(token);
-
-    PageRequest pageRequest = PageRequest.of(page, 10);
-    return orderService.getOrders(pageRequest).getContent();
-  }
 }
