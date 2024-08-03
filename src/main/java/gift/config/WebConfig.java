1182d40bf9b1da8e9b486b792ee46af12995b323
박민규 <pmq0101@naver.com>
2024-08-03 22:48:39 +0900

    feat(WebConfig): 프론트 배포 주소 추가

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins(
-                "http://localhost:3000")
+                "http://localhost:3000", "http://react-deploy2.pages.dev")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
