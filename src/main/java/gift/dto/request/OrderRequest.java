d8d24b167fef189ebeac4a11f642272ffdd571be
wvssm <neoramiregi@gmail.com>
2024-08-04 03:15:52 +0900

    feat(OrderRequest): OrderRequest에 points 필드 추가

--- src/main/java/gift/dto/request/OrderRequest.java
+++ src/main/java/gift/dto/request/OrderRequest.java
@@ -1,14 +1,4 @@
 package gift.dto.request;
 
-import gift.domain.Member;
-import gift.domain.Option;
-import gift.domain.Order;
-import gift.domain.Product;
-
-import java.time.LocalDateTime;
-
-public record OrderRequest(Long optionId, int quantity, String message) {
-    public Order toEntity(LocalDateTime orderDateTime, Member member, Option option) {
-        return new Order(this.quantity,orderDateTime, this.message, member, option);
-    }
+public record OrderRequest(Long optionId, int quantity, String message, int points) {
 }
