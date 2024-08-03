3f4a0ddf87e27e458c34920e426ed94ca5b8e80d
kuma4188 <psb4188@gmail.com>
2024-08-03 22:40:34 +0900

    refactor : model에 포인트 변수추가

--- src/main/java/gift/model/SiteUser.java
+++ src/main/java/gift/model/SiteUser.java
@@ -1,37 +1,60 @@
 package gift.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
 
-@Getter
-@Setter
 @Entity
-@Table(name = "site_user")
 public class SiteUser {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(unique = true , nullable = false)
+
 	private String username;
-	@Column(nullable = false)
+
 	private String password;
-	@Column(unique = true , nullable = false)
+
 	private String email;
 
-	public SiteUser() {
+	private int points = 1000; // 초기 포인트 설정
+
+	// getters and setters
+	public Long getId() {
+		return id;
 	}
 
-	public SiteUser(String username, String password, String email) {
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
 }
-
-
