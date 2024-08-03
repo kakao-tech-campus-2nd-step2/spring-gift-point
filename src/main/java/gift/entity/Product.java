aaee35d2cf28e4e5fc43b1d3d09fc301cbc06ee3
youcaslte03 <luckthb325@naver.com>
2024-08-03 16:36:07 +0900

    fix: 상품명 최대 리미트 증가

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -22,7 +22,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 15)
+    @Column(nullable = false, length = 50)
     private String name;
 
     @Column(nullable = false)
