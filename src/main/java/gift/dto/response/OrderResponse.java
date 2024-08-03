e3aa8cbcfdf0cc0741751364a300ebd2c8876ec8
wvssm <neoramiregi@gmail.com>
2024-08-04 03:16:33 +0900

    feat(OrderResponse): OrderResponse에 pointsUsed, pointsReceived, payment 필드 추가

--- src/main/java/gift/dto/response/OrderResponse.java
+++ src/main/java/gift/dto/response/OrderResponse.java
@@ -4,9 +4,9 @@ import gift.domain.Order;
 
 import java.time.LocalDateTime;
 
-public record OrderResponse(Long id, Long optionId, int quantity, LocalDateTime orderDateTime, String message) {
+public record OrderResponse(Long id, Long optionId, int quantity, LocalDateTime orderDateTime, int pointsUsed, int pointsReceived, int payment, String message) {
     public static OrderResponse from(final Order order){
         Long optionId = order.getOption().getId();
-        return new OrderResponse(order.getId(), optionId, order.getQuantity(), order.getOrderDateTime(), order.getMessage());
+        return new OrderResponse(order.getId(), optionId, order.getQuantity(), order.getOrderDateTime(), order.getPointsUsed(), order.getPointsReceived(), order.getPayment(), order.getMessage());
     }
 }
