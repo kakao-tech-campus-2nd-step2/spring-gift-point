0420ae7d1b9f8f61c61644d1690d8360db01015b
Seonghun Jeong <tjdgns5506@gmail.com>
2024-08-03 20:25:07 +0900

    feat: PointService 에 subtractPoint(Long memberId, OrderRequest orderRequest) 추가

--- src/main/java/gift/service/PointService.java
+++ src/main/java/gift/service/PointService.java
@@ -1,5 +1,6 @@
 package gift.service;
 
+import gift.dto.request.OrderRequest;
 import gift.dto.response.PointResponse;
 import gift.entity.Member;
 import org.springframework.stereotype.Service;
@@ -7,13 +8,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class PointService {
     private final MemberService memberService;
+    private final OptionService optionService;
 
-    public PointService(MemberService memberService) {
+    public PointService(MemberService memberService, OptionService optionService) {
         this.memberService = memberService;
+        this.optionService = optionService;
     }
 
     public PointResponse getMemberPoint(Long memberId) {
         Member member = memberService.getMember(memberId);
         return new PointResponse(member.getPoint());
+    }
+
+    public void subtractPoint(Long memberId, OrderRequest orderRequest) {
+        int productPrice = optionService.getProductPrice(orderRequest.optionId());
+        int totalPrice = productPrice * orderRequest.quantity();
+
+        memberService.getMember(memberId)
+                .subtractPoint(totalPrice);
     }
 }
