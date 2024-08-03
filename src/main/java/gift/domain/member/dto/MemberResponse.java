31aa16fdbb058d332d553045f6915537dc8021d9
Kangboom <jkl6147@naver.com>
2024-08-03 14:46:59 +0900

    feat: member 응답 dto point 필드 추가

--- src/main/java/gift/domain/member/dto/MemberResponse.java
+++ src/main/java/gift/domain/member/dto/MemberResponse.java
@@ -4,9 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "회원 응답 Dto")
 public record MemberResponse(
+    @Schema(description = "Id 식별자")
+    Long id,
     @Schema(description = "회원 이메일")
     String email,
     @Schema(description = "회원 패스워드")
-    String password) {
-
+    String password,
+    @Schema(description = "멤버 포인트")
+    int point)
+{
 }
