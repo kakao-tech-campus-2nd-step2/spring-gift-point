f01e5c88e8c5e2d8553bb6708780895eecfee921
Woojae Jeong <woo6388@naver.com>
2024-08-03 23:44:53 +0900

    refactor : ResponseOrderDTO에 phoneNumber 필드 추가

--- src/main/java/gift/DTO/ResponseOrderDTO.java
+++ src/main/java/gift/DTO/ResponseOrderDTO.java
@@ -1,6 +1,7 @@
 package gift.DTO;
 
 import gift.Model.Entity.Order;
+import gift.Model.Value.CashReceipt;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "주문 응답 DTO")
@@ -20,12 +21,16 @@ public class ResponseOrderDTO {
     @Schema(description = "주문 메세지")
     private String message;
 
-    public ResponseOrderDTO(Long id, Long optionId, Integer quantity, String orderDateTime, String message) {
+    @Schema(description = "주문 메세지")
+    private String phoneNumber;
+
+    public ResponseOrderDTO(Long id, Long optionId, Integer quantity, String orderDateTime, String message, String phoneNumber) {
         this.id = id;
         this.optionId = optionId;
         this.quantity = quantity;
         this.orderDateTime = orderDateTime;
         this.message = message;
+        this.phoneNumber = phoneNumber;
     }
 
     public Long getId() {
@@ -48,8 +53,18 @@ public class ResponseOrderDTO {
         return message;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
     public static ResponseOrderDTO of(Order order){
-        return new ResponseOrderDTO(order.getId(), order.getOption().getId(), order.getQuantity().getValue(), order.getOrderDateTime().toString(), order.getMessage().orElseGet(()->""));
+        return new ResponseOrderDTO(
+                order.getId(),
+                order.getOption().getId(),
+                order.getQuantity().getValue(),
+                order.getOrderDateTime().toString(),
+                order.getMessage().orElseGet(()->""),
+                order.getCashReceipt().map(CashReceipt::getValue).orElse(""));
     }
 
 }
