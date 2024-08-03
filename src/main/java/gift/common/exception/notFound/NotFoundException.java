18bc4e4d6181f3d1ff2f8d37d70763790e43d1b3
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-08-04 02:44:38 +0900

    feat: error 추가

---
+++ src/main/java/gift/common/exception/notFound/NotFoundException.java
@@ -0,0 +1,9 @@
+package gift.common.exception.notFound;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
