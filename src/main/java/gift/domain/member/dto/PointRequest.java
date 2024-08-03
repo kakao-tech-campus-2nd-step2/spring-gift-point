4a3eb45801a27e7dacad0e9d1d6d06937165faa6
Kangboom <jkl6147@naver.com>
2024-08-03 14:44:55 +0900

    feat: 포인트 요청 dto 추가

---
+++ src/main/java/gift/domain/member/dto/PointRequest.java
@@ -0,0 +1,19 @@
+package gift.domain.member.dto;
+
+import jakarta.validation.constraints.Min;
+
+public class PointRequest {
+    @Min(value = 1, message = "1원 이상 입력해주세요")
+    private int point;
+
+    public PointRequest(){
+    }
+
+    public PointRequest(int point) {
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+}
