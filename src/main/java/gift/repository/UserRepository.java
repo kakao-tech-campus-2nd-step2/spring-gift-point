944076137e55cd92058381b918636128f3a53597
pinkbeenyeti <rbgusdn12@gmail.com>
2024-08-04 01:02:02 +0900

    feat: point 반환 메서드 추가

--- src/main/java/gift/repository/UserRepository.java
+++ src/main/java/gift/repository/UserRepository.java
@@ -2,10 +2,15 @@ package gift.repository;
 
 import gift.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    @Query("SELECT u.point FROM User u WHERE u.id = :userId")
+    Long getPointById(@Param("userId") Long userId);
 }
