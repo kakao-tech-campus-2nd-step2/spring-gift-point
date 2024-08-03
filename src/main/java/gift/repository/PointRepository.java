a13b8cb61877a5c4e2937bc4a71e37a6a0fa1a42
Seonghun Jeong <tjdgns5506@gmail.com>
2024-08-03 20:24:23 +0900

    feat: PointRepository 추가

---
+++ src/main/java/gift/repository/PointRepository.java
@@ -0,0 +1,7 @@
+package gift.repository;
+
+import gift.entity.Point;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointRepository extends JpaRepository<Point,Long> {
+}
