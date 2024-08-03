484ee8717542e649326a2d67c6ed471d6c57e532
mogld <143364802+mogld@users.noreply.github.com>
2024-08-03 20:12:23 +0900

    feat: Point기능 추가 후 관리자 화면 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,7 +22,8 @@ CREATE TABLE product (
 CREATE TABLE member (
                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
                         email VARCHAR(255) NOT NULL UNIQUE,
-                        password VARCHAR(255) NOT NULL
+                        password VARCHAR(255) NOT NULL,
+                        points INT DEFAULT 0
 );
 
 CREATE TABLE product_option (
