5d24f4f8d3ba7ce31ce86aca43fadedf7babdf82
ez23re <ez23re@gmail.com>
2024-08-03 17:49:25 +0900

    feat : CorsConfig 생성

---
+++ src/main/java/gift/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package gift.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 일단 모든 도메인 허용 -> 실제로는 보안상으로 모든 허용은 권장하지 않음
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .maxAge(3600);
+            }
+        };
+    }
+}
