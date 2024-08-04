5935e62577b46fd22f35a14a067b892bd60551e5
Shsin9797 <aaa8141@naver.com>
2024-08-04 23:27:22 +0900

    feat : 포인트 충전, 조회 기능 구현 - token으로 어떤 멤버의 포인트를 충전하고 조회할지 확인 하고 반영후 , 멤버가 가진 최종 포인트를 반환

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -1,7 +1,10 @@
 package gift.controller;
 
+import gift.dto.MemberPointChargingRequestDto;
+import gift.dto.MemberPointViewResponseDto;
 import gift.dto.MemberRequestDto;
 import gift.dto.MemberResponseDto;
+import gift.service.BasicTokenService;
 import gift.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,26 +12,31 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.naming.AuthenticationException;
 import java.util.List;
 
-@Tag(name = "auth", description = "인증 API")
+@Tag(name = "member", description = "회원 API")
 @RestController
 @RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
+    private final BasicTokenService basicTokenService;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, BasicTokenService basicTokenService) {
         this.memberService = memberService;
+        this.basicTokenService = basicTokenService;
     }
 
     @GetMapping("")
@@ -66,7 +74,7 @@ public class MemberController {
             @ApiResponse(responseCode = "400", description = "로그인 실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     public ResponseEntity<MemberResponseDto> login(@ModelAttribute MemberRequestDto memberRequestDto) throws AuthenticationException {
         if (memberService.login(memberRequestDto.getEmail(), memberRequestDto.getPassword())) {
-            return new ResponseEntity<>(new MemberResponseDto(memberRequestDto.getEmail(),memberService.generateTokenFrom(memberRequestDto.getEmail())), HttpStatus.OK);
+            return new ResponseEntity<>(new MemberResponseDto(memberRequestDto.getEmail(), memberService.generateTokenFrom(memberRequestDto.getEmail())), HttpStatus.OK);
         }
         throw new AuthenticationException("로그인 실패");
     }
@@ -79,5 +87,33 @@ public class MemberController {
     public ResponseEntity<List<MemberResponseDto>> MemberList() {
         List<MemberResponseDto> memberResponseDto = memberService.getAllMemberResponseDto();
         return new ResponseEntity<>(memberResponseDto, HttpStatus.OK);
+    }
+
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "멤버 포인트 충전 성공", content = @Content(schema = @Schema(implementation = MemberResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
+    @GetMapping("/point")
+    @Operation(summary = "멤버 포인트 충전", description = "멤버가 가진 포인트를 충전합니다.")
+    public ResponseEntity<MemberPointViewResponseDto> ChargeMemberPoint(
+            @RequestBody MemberPointChargingRequestDto memberPointChargingRequestDto,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader
+    ) {
+        Long memberId = basicTokenService.getUserIdByDecodeTokenValue(authorizationHeader);
+        MemberPointViewResponseDto remainPoint = memberService.updateMemberPoint(memberPointChargingRequestDto, memberId);
+        return new ResponseEntity<>(remainPoint, HttpStatus.OK);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "멤버 포인트 조회 성공", content = @Content(schema = @Schema(implementation = MemberResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
+    @GetMapping("/point")
+    @Operation(summary = "멤버 포인트 조회", description = "멤버 포인트를 조회합니다.")
+    public ResponseEntity<MemberPointViewResponseDto> ViewMemberPoint(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader
+    ) {
+        Long memberId = basicTokenService.getUserIdByDecodeTokenValue(authorizationHeader);
+        MemberPointViewResponseDto remainPoint = memberService.getMemberPoint(memberId);
+        return new ResponseEntity<>(remainPoint, HttpStatus.OK);
     }
 }
---
+++ src/main/java/gift/dto/MemberPointChargingRequestDto.java
@@ -0,0 +1,9 @@
+package gift.dto;
+
+public class MemberPointChargingRequestDto {
+    private Long amount;
+
+    public Long getAmount() {
+        return amount;
+    }
+}
