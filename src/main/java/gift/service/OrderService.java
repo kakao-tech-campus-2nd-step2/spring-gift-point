0332eb31dac15eda6f67bf6a6a3cc5a8c19b941e
seojiu <seojiu5598@gmail.com>
2024-08-05 07:36:43 +0900

    feat: 상품 구매시 적립 기능 추가

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -1,15 +1,20 @@
 package gift.service;
 
 import gift.dto.OrderRequestDto;
+import gift.model.Member;
 import gift.model.Option;
 import gift.model.Order;
 import gift.model.Product;
+import gift.repository.MemberRepository;
 import gift.repository.OrderRepository;
 import gift.repository.WishlistRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class OrderService {
@@ -17,13 +22,15 @@ public class OrderService {
     private final OptionService optionService;
     private final WishlistRepository wishlistRepository;
     private final KakaoMessageService kakaoMessageService;
+    private final MemberRepository memberRepository;
 
     public OrderService(OrderRepository orderRepository, OptionService optionService,
-                        WishlistRepository wishlistRepository, KakaoMessageService kakaoMessageService) {
+                        WishlistRepository wishlistRepository, KakaoMessageService kakaoMessageService, MemberRepository memberRepository) {
         this.orderRepository = orderRepository;
         this.optionService = optionService;
         this.wishlistRepository = wishlistRepository;
         this.kakaoMessageService = kakaoMessageService;
+        this.memberRepository = memberRepository;
     }
 
     @Transactional
@@ -40,6 +47,10 @@ public class OrderService {
         Product product = optionService.findProductByOptionId(option.getId());
 
         wishlistRepository.deleteByMemberIdAndProductId(memberId, product.getId());
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
+        Member updatedMember = new Member(member.getId(), member.getEmail(), member.getPassword(), member.getActiveToken(), (int) (orderRequestDto.getPoints()*0.5));
+        memberRepository.save(updatedMember);
 
         kakaoMessageService.sendMessageToKakao(order, memberId);
         return order;
