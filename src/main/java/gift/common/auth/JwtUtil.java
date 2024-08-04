185fc48268eb455c7d2cf6657bec7fde35abb726
pykido <animoto1@naver.com>
2024-08-04 14:50:03 +0900

    refactor : 토큰 길이 수정 - 2

--- src/main/java/gift/common/auth/JwtUtil.java
+++ src/main/java/gift/common/auth/JwtUtil.java
@@ -2,42 +2,37 @@ package gift.common.auth;
 
 import gift.member.model.Member;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final Key key;
-
-    public JwtUtil(@Value("${jwt.secret}") String secretKey) {
-        this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
-    }
+    private static final String SECRET_KEY = "N4cYFXz4pYr/sdf9p0sl/j4L8kdKd1gdPv+0Wn4fZ8M7aOg4xqKr4sljxfwHt4Kp7op4l+Jd3ZV4Rmd1Jd7AfQ==JASDJWQDWEWQEKSA"; // 비밀 키
+    private static final long EXPIRATION_TIME = 86400000; // 24시간
 
     // JWT 토큰을 파싱하여 그 안에 포함된 클레임을 반환
     public Claims extractClaims(String token) {
         return Jwts.parser()
-                .setSigningKey(key)
+                .setSigningKey(SECRET_KEY)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
 
-    // 토큰이 유효한지 확인
+    // 토큰 유효성 검사
     public boolean isTokenValid(String token) {
         try {
             Jwts.parser()
-                    .setSigningKey(key)
+                    .setSigningKey(SECRET_KEY)
                     .build()
                     .parseClaimsJws(token);
             return true;
-        } catch (Exception e) {
+        } catch (JwtException e) {
             e.printStackTrace();
             return false;
         }
@@ -54,14 +49,15 @@ public class JwtUtil {
         return authorizationHeader.startsWith("Bearer ") ? authorizationHeader.substring(7) : null;
     }
 
-    // accesToken 생성
+
+    // JWT 생성
     public String generateToken(Member member) {
         return Jwts.builder()
                 .setSubject(member.getId().toString())
-                .claim("email", member.getEmail())  // 이메일 클레임 추가
+                .claim("email", member.getEmail()) // 이메일 클레임 추가
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 26시간 만료
-                .signWith(key)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
                 .compact();
     }
 }
