075f26f241bd7577f0e5021924064198ed21e6a1
stopmin <geemin0@naver.com>
2024-08-04 16:12:04 +0900

    chore: products -> contents

--- src/main/java/gift/order/domain/OrderListResponse.java
+++ src/main/java/gift/order/domain/OrderListResponse.java
@@ -3,22 +3,22 @@ package gift.order.domain;
 import java.util.List;
 
 public class OrderListResponse {
-    private List<OrderResponse> products;
+    private List<OrderResponse> contents;
     private int number;
     private long totalElement;
     private int size;
     private boolean last;
 
     public OrderListResponse(List<OrderResponse> products, int number, long totalElement, int size, boolean last) {
-        this.products = products;
+        this.contents = products;
         this.number = number;
         this.totalElement = totalElement;
         this.size = size;
         this.last = last;
     }
 
-    public List<OrderResponse> getProducts() {
-        return products;
+    public List<OrderResponse> getContents() {
+        return contents;
     }
 
     public int getNumber() {
