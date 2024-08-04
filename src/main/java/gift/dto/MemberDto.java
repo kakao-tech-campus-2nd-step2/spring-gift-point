09f23b8a0627fb3645f5e11d6a102af877597f67
hyunseo <wndlthsk@naver.com>
2024-08-04 16:09:51 +0900

    fix : memberDto에서 id 삭제

--- src/main/java/gift/dto/MemberDto.java
+++ src/main/java/gift/dto/MemberDto.java
@@ -2,8 +2,7 @@ package gift.dto;
 
 import jakarta.validation.constraints.*;
 
-public record MemberDto(Long id,
-                        @NotEmpty(message = "이메일 입력은 필수 입니다.")
+public record MemberDto(@NotEmpty(message = "이메일 입력은 필수 입니다.")
                         @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$",
                             message = "이메일 형식에 맞지 않습니다.")
                         String email,
@@ -11,8 +10,7 @@ public record MemberDto(Long id,
                         @Size(min = 5, message = "비밀번호는 최소 5자 이상이어야 합니다.")
                         String password) {
 
-    public MemberDto(Long id, String email, String password) {
-        this.id = id;
+    public MemberDto(String email, String password) {
         this.email = email;
         this.password = password;
     }
