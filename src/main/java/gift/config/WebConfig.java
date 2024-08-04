c413e040910c483daee3bc9b04a089e238cdecfb
evol <61404196+Jinhwa-Park@users.noreply.github.com>
2024-08-04 11:33:25 +0900

    feat: add WebMvcConfigurer for CORS setting

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Autowired
@@ -21,5 +23,14 @@ public class WebConfig {
         registrationBean.setFilter(jwtAuthenticationFilter);
         registrationBean.addUrlPatterns("/api/*");
         return registrationBean;
+    }
+
+    // 전역 CORS 설정 추가
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://example.com") // 허용할 출처를 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*"); // 모든 헤더 허용
     }
 }
