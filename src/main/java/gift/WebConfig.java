58143abe553975d5d3bb9e3d619d0a53ff26c131
ariimo <159447466+ariimo@users.noreply.github.com>
2024-08-04 22:07:37 +0900

    전남대 BE_모아림 6주차 과제 (2단계) (#337)
    * refactor: Product Entity에서 Option 제외 및 로직 수정
    
    * refactor: API 명세 변경에 의한 RequestStateDTO 수정
    
    * refactor: API 명세 변경에 의한 Member 수정
    
    * refactor: API 명세 변경에 의한 Category 수정
    
    * refactor: API 명세 변경에 의한 Product 수정
    
    * refactor: API 명세 변경에 의한 Option 수정
    
    * feat: RequestMemberDto 추가
    
    * refactor: API 명세 변경에 의한 Wish 수정
    
    * refactor: API 명세 변경에 의한 Order 수정
    
    * 일부 Test 주석처리
    
    * refactor: API 명세 변경에 의한 KaKaoController 수정
    
    * Update README.md
    
    * Test 주석처리
    
    * 배포 스크립트 작성
    
    * Update README.md
    
    * feat: WebConfig에 Cors 구현

--- src/main/java/gift/WebConfig.java
+++ src/main/java/gift/WebConfig.java
@@ -3,6 +3,7 @@ package gift;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +19,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Authorization", "Content-Type")
+            .exposedHeaders("Custom-Header")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
