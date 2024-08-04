f04a6cc1cb9cf92f96a9cabc92e6c2f100606468
hyunseo <wndlthsk@naver.com>
2024-08-04 20:17:05 +0900

    feat : 주문 시 포인트 차감, 최종 결제 값 저장, 포인트 적립

--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -11,7 +11,6 @@ import gift.dto.OrderPagedResponseDto;
 import gift.dto.OrderRequestDto;
 import gift.dto.OrderResponseDto;
 import gift.dto.ProductResponseDto;
-import gift.repository.MemberRepository;
 import gift.repository.OrderRepository;
 import gift.repository.ProductRepository;
 import java.util.NoSuchElementException;
@@ -27,50 +26,61 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final KakaoService kakaoService;
     private final ProductService productService;
-    private final WishService wishService;
 
     private final ApplicationEventPublisher applicationEventPublisher;
 
     public OrderService(ProductRepository productRepository, OrderRepository orderRepository, KakaoService kakaoService,
-        ProductService productService, WishService wishService,
-        ApplicationEventPublisher applicationEventPublisher) {
+        ProductService productService, ApplicationEventPublisher applicationEventPublisher) {
         this.productRepository = productRepository;
         this.orderRepository = orderRepository;
         this.kakaoService = kakaoService;
         this.productService = productService;
-        this.wishService = wishService;
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
     @Transactional
     public OrderResponseDto processOrder(OrderRequestDto orderRequestDto, Member member) {
+        ProductResponseDto product = productService.getProductById(orderRequestDto.productId());
+
         // 재고 처리
         productService.decreaseOptionQuantity(orderRequestDto.productId(), orderRequestDto.optionId(),orderRequestDto.quantity());
+        // 포인트 차감
+        member.subtractPoints(orderRequestDto.point()); // 포인트 입력값 예외 처리까지 수행
+        Integer finalPrice = product.price() * orderRequestDto.quantity() - orderRequestDto.point();
         // 주문 생성 및 저장
-        OrderResponseDto orderResponseDto = createOrder(member.getId(), orderRequestDto);
+        OrderResponseDto orderResponseDto = createOrder(member.getId(), orderRequestDto, finalPrice);
+        // 포인트 적립
+        member.addPoints(product.price(), orderResponseDto.quantity());
 
         // 위시리스트 삭제 & 주문 메시지 발송
-        OrderEvent orderEvent = new OrderEvent(orderRequestDto, member);
+        OrderEvent orderEvent = new OrderEvent(orderResponseDto.id(), member, product.id());
         applicationEventPublisher.publishEvent(orderEvent);
 
         return orderResponseDto;
     }
 
     @Transactional
-    public OrderResponseDto createOrder(Long memberId, OrderRequestDto orderRequestDto) {
-        Order order = new Order(memberId, orderRequestDto.productId(), orderRequestDto.optionId(), orderRequestDto.quantity(), orderRequestDto.message());
+    public OrderResponseDto createOrder(Long memberId, OrderRequestDto orderRequestDto, Integer finalPrice) {
+        Order order = new Order(
+            memberId,
+            orderRequestDto.productId(),
+            orderRequestDto.optionId(),
+            orderRequestDto.quantity(),
+            orderRequestDto.message(),
+            finalPrice
+        );
         Order savedOrder = orderRepository.save(order);
         return OrderResponseDto.convertToDto(savedOrder);
     }
 
     @Transactional(readOnly = true)
-    public void sendOrderMessage(OrderRequestDto orderRequestDto, Member member) {
+    public void sendOrderMessage(Long orderId, Member member) {
         try {
             kakaoService.sendKakaoMessageToMe(
                 member.getAccessToken(),
                 new KakaoMessageDto(
                     "text",
-                    createTextDetail(orderRequestDto, member.getEmail()),
+                    createTextDetail(orderId, member),
                     "https://gift.kakao.com",
                     "https://gift.kakao.com"
                 )
@@ -82,16 +92,20 @@ public class OrderService {
     }
 
     @Transactional(readOnly = true)
-    public String createTextDetail(OrderRequestDto orderRequestDto, String email) {
-        Product product = productRepository.findById(orderRequestDto.productId())
-            .orElseThrow(() -> new NoSuchElementException("해당 id의 상품 없음: " + orderRequestDto.productId()));
-        Option option = product.getOptionById(orderRequestDto.optionId());
+    public String createTextDetail(Long orderId, Member member) {
+        Order order = orderRepository.findById(orderId)
+            .orElseThrow(() -> new NoSuchElementException("[Text] 해당 id의 주문 없음: " + orderId));
+        Product product = productRepository.findById(order.getProductId())
+            .orElseThrow(() -> new NoSuchElementException("[Text] 해당 id의 상품 없음: " + order.getProductId()));
+        Option option = product.getOptionById(order.getOptionId());
 
-        String text = "계정 이메일: " + email
+        String text = "계정 이메일: " + member.getEmail()
             + "\n주문한 상품명: " + product.getName()
             + "\n주문한 옵션명: " + option.getName()
-            + "\n주문 수량: " + orderRequestDto.quantity()
-            + "\n요청 사항: " + orderRequestDto.message();
+            + "\n주문 수량: " + order.getQuantity()
+            + "\n요청 사항: " + order.getMessage()
+            + "\n최종 결제 금액: " + order.getFinalPrice()
+            + "\n남은 포인트: " + member.getPoints();
 
         return text;
     }
