a71241c143137c181ff3eac4424f49c61aaa6ec7
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-08-04 14:46:21 +0900

    feat: OrderService에 amountDiscountPolicy 적용

--- src/main/java/gift/order/application/OrderService.java
+++ src/main/java/gift/order/application/OrderService.java
@@ -2,6 +2,7 @@ package gift.order.application;
 
 import gift.auth.KakaoMessageSend;
 import gift.auth.KakaoService;
+import gift.discount.domain.DiscountPolicy;
 import gift.exception.type.NotFoundException;
 import gift.member.domain.Member;
 import gift.member.domain.MemberRepository;
@@ -9,9 +10,11 @@ import gift.option.application.OptionService;
 import gift.option.domain.Option;
 import gift.option.domain.OptionRepository;
 import gift.order.application.command.OrderCreateCommand;
+import gift.order.application.response.OrderSaveServiceResponse;
 import gift.order.domain.Order;
 import gift.order.domain.OrderRepository;
 import gift.wishlist.domain.WishlistRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,7 @@ public class OrderService {
     private final OptionService optionService;
     private final MemberRepository memberRepository;
     private final KakaoService kakaoService;
+    private final DiscountPolicy discountPolicy;
 
     public OrderService(
             OrderRepository orderRepository,
@@ -31,7 +35,8 @@ public class OrderService {
             OptionService optionService,
             WishlistRepository wishlistRepository,
             MemberRepository memberRepository,
-            KakaoService kakaoService
+            KakaoService kakaoService,
+            @Qualifier("amountDiscountPolicy") DiscountPolicy discountPolicy
     ) {
         this.orderRepository = orderRepository;
         this.optionRepository = optionRepository;
@@ -39,10 +44,11 @@ public class OrderService {
         this.wishlistRepository = wishlistRepository;
         this.memberRepository = memberRepository;
         this.kakaoService = kakaoService;
+        this.discountPolicy = discountPolicy;
     }
 
     @Transactional
-    public void save(OrderCreateCommand command, Long memberId) {
+    public OrderSaveServiceResponse save(OrderCreateCommand command, Long memberId) {
         Option option = optionRepository.findById(command.optionId())
                 .orElseThrow(() -> new NotFoundException("해당 옵션이 존재하지 않습니다."));
 
@@ -59,9 +65,12 @@ public class OrderService {
         memberRepository.findById(memberId)
                 .ifPresent(member -> sendOrderMessage(command, member));
 
-        Order order = command.toOrder(option);
+        int finalPrice = discountPolicy.calculateFinalAmount(option.getProduct().getPrice() * command.quantity());
+        int originalPrice = option.getProduct().getPrice() * command.quantity();
 
-        orderRepository.save(order);
+        Order order = orderRepository.save(command.toOrder(option));
+
+        return OrderSaveServiceResponse.of(order, originalPrice, finalPrice);
     }
 
     private void sendOrderMessage(OrderCreateCommand command, Member member) {
