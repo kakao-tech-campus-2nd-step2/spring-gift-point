3e96a679996318fd6bf18a6e1ee56b6a49ae59f2
BOMIN LYU <rbm0524@naver.com>
2024-08-04 16:48:46 +0900

    fix : 이전 버전으로 복구

--- src/main/resources/scripts/deploy.sh
+++ src/main/resources/scripts/deploy.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-# 설정 부분
-REPO_URL="https://github.com/rbm0524/spring-gift-point.git"
-REPO_DIR="/home/ubuntu/spring-gift-point"
-BUILD_DIR="$REPO_DIR/build/libs"
-DEPLOY_PATH="/home/ubuntu/"
-LOG_FILE="/home/ubuntu/deploy.log"
-BRANCH="step3"
-
-# 레포지토리 클론 또는 업데이트
-if [ ! -d "$REPO_DIR" ]; then
-  printf "레포지토리 클론 중...\n"
-  git clone $REPO_URL $REPO_DIR
-fi
-
-printf "레포지토리 업데이트 및 브랜치 체크아웃 중...\n"
-cd $REPO_DIR
-git fetch origin
-git checkout $BRANCH
-git pull origin $BRANCH
-
-printf "프로젝트 빌드 중...\n"
-./gradlew build
-
 printf "빌드 경로에서 JAR 파일 찾는 중...\n"
-BUILD_PATH=$(ls $BUILD_DIR/*.jar)
+BUILD_PATH=$(ls /home/ubuntu/repository/spring-gift-point/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_PATH)
 printf "빌드된 JAR 파일: %s\n" $JAR_NAME
 
@@ -34,20 +11,23 @@ CURRENT_PID=$(pgrep -f $JAR_NAME)
 if [ -z $CURRENT_PID ]
 then
   printf "현재 실행 중인 애플리케이션이 없습니다.\n"
+  sleep 1
 else
   printf "실행 중인 애플리케이션(PID: %s) 종료 중...\n" $CURRENT_PID
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
+DEPLOY_PATH=/home/ubuntu/
 printf "JAR 파일을 배포 경로로 복사 중...\n"
 cp $BUILD_PATH $DEPLOY_PATH
 cd $DEPLOY_PATH
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 printf "애플리케이션을 백그라운드에서 시작 중...\n"
-nohup java -jar $DEPLOY_JAR > $LOG_FILE 2>&1 &
+nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
 
 printf "애플리케이션 배포 완료. 로그 파일: %s\n" $LOG_FILE
 
-# tail -f $LOG_FILE 명령어로 배포 로그를 확인할 수 있습니다.
+# tail -f /home/ubuntu/deploy.log
+# 위 명령어로 배포 로그를 확인할 수 있습니다.
