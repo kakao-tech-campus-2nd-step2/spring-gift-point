c8771e2cd1849366b2b494b2b1988245f267f48d
dong_yxxn <g0326kdy@gmail.com>
2024-08-04 10:01:58 +0900

    feat: PointDTO 생성

--- src/main/java/gift/user/domain/dto/PointDTO.java
+++ src/main/java/gift/user/domain/dto/PointDTO.java
@@ -1,2 +1,17 @@
-package gift.user.domain.dto;public class PointDTO {
+package gift.user.domain.dto;
+
+public class PointDTO {
+    Integer point;
+
+    public PointDTO(Integer point) {
+        this.point = point;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
 }
