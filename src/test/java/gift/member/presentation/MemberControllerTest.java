13dd84fea51f9b33ff0124679ba11e4c2c0740bf
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-08-04 14:51:27 +0900

    test: Member 포인트 추가 및 조회 관련 테스트코드 작성

--- src/test/java/gift/member/presentation/MemberControllerTest.java
+++ src/test/java/gift/member/presentation/MemberControllerTest.java
@@ -3,11 +3,16 @@ package gift.member.presentation;
 import gift.auth.KakaoService;
 import gift.auth.TokenService;
 import gift.member.application.MemberService;
-import gift.member.application.MemberServiceResponse;
 import gift.member.application.command.MemberEmailUpdateCommand;
 import gift.member.application.command.MemberPasswordUpdateCommand;
-import gift.member.presentation.request.MemberJoinRequest;
+import gift.member.application.response.MemberLoginServiceResponse;
+import gift.member.application.response.MemberRegisterServiceResponse;
+import gift.member.application.response.MemberServiceResponse;
+import gift.member.domain.Member;
+import gift.member.domain.OauthProvider;
 import gift.member.presentation.request.MemberLoginRequest;
+import gift.member.presentation.request.MemberRegisterRequest;
+import gift.member.presentation.request.PointUpdateRequest;
 import gift.member.presentation.request.ResolvedMember;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -61,31 +66,45 @@ public class MemberControllerTest {
     @Test
     void 회원가입_테스트() throws Exception {
         // Given
-        MemberJoinRequest request = new MemberJoinRequest(email, password);
-        when(memberService.join(request.toCommand())).thenReturn(memberId);
+        MemberRegisterRequest request = new MemberRegisterRequest(email, password);
+        MemberRegisterServiceResponse memberRegisterServiceResponse = MemberRegisterServiceResponse.from(new Member(
+                1L,
+                email,
+                password,
+                OauthProvider.COMMON));
+        when(memberService.register(request.toCommand())).thenReturn(memberRegisterServiceResponse);
         when(tokenService.createToken(memberId)).thenReturn(token);
 
         // When & Then
-        mockMvc.perform(post("/api/member/join")
+        mockMvc.perform(post("/api/members/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"email\":\"" + email + "\", \"password\":\"" + password + "\"}"))
-                .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.AUTHORIZATION, "Bearer " + token));
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.email").value(email))
+                .andExpect(jsonPath("$.token").value(token));
     }
 
     @Test
     void 로그인_테스트() throws Exception {
         // Given
         MemberLoginRequest request = new MemberLoginRequest(email, password);
-        when(memberService.login(request.toCommand())).thenReturn(memberId);
-        when(tokenService.createToken(memberId)).thenReturn(token);
+        MemberLoginServiceResponse memberLoginServiceResponse = MemberLoginServiceResponse.from(
+                new Member(
+                        1L,
+                        email,
+                        password,
+                        OauthProvider.COMMON
+                ));
+        when(memberService.login(request.toCommand())).thenReturn(memberLoginServiceResponse);
+        when(tokenService.createToken(memberLoginServiceResponse.id())).thenReturn(token);
 
         // When & Then
-        mockMvc.perform(post("/api/member/login")
+        mockMvc.perform(post("/api/members/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"email\":\"" + email + "\", \"password\":\"" + password + "\"}"))
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.AUTHORIZATION, "Bearer " + token));
+                .andExpect(jsonPath("$.email").value(email))
+                .andExpect(jsonPath("$.token").value(token));
     }
 
     @Test
@@ -95,7 +114,7 @@ public class MemberControllerTest {
         when(memberService.findById(eq(memberId))).thenReturn(response);
 
         // When & Then
-        mockMvc.perform(get("/api/member/{id}", memberId)
+        mockMvc.perform(get("/api/members/{id}", memberId)
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.email").value(email))
@@ -110,7 +129,7 @@ public class MemberControllerTest {
         when(memberService.findAll()).thenReturn(Arrays.asList(response1, response2));
 
         // When & Then
-        mockMvc.perform(get("/api/member")
+        mockMvc.perform(get("/api/members")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].email").value(email))
@@ -128,13 +147,13 @@ public class MemberControllerTest {
         when(memberService.findById(anyLong())).thenReturn(memberServiceResponse);
 
         // When & Then
-        mockMvc.perform(put("/api/member/email")
+        mockMvc.perform(put("/api/members/email")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"email\":\"" + newEmail + "\"}")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isOk());
 
-        verify(memberService).updateEmail(eq(expectedCommand), eq(resolvedMember.id()));
+        verify(memberService).updateEmail(eq(resolvedMember.id()), (eq(expectedCommand)));
     }
 
     @Test
@@ -148,13 +167,13 @@ public class MemberControllerTest {
         when(memberService.findById(anyLong())).thenReturn(memberServiceResponse);
 
         // When & Then
-        mockMvc.perform(put("/api/member/password")
+        mockMvc.perform(put("/api/members/password")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"password\":\"" + newPassword + "\"}")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isOk());
 
-        verify(memberService).updatePassword(eq(updateCommand), eq(member.id()));
+        verify(memberService).updatePassword(eq(member.id()), eq(updateCommand));
     }
 
     @Test
@@ -165,7 +184,7 @@ public class MemberControllerTest {
         when(memberService.findById(anyLong())).thenReturn(memberServiceResponse);
 
         // When & Then
-        mockMvc.perform(delete("/api/member")
+        mockMvc.perform(delete("/api/members")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
                 .andExpect(status().isNoContent());
 
@@ -179,7 +198,7 @@ public class MemberControllerTest {
         when(kakaoService.getKakaoRedirectUrl()).thenReturn(redirectUrl);
 
         // When & Then
-        mockMvc.perform(get("/api/member/login/kakao"))
+        mockMvc.perform(get("/api/members/login/kakao"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl(redirectUrl));
 
@@ -191,16 +210,59 @@ public class MemberControllerTest {
         // Given
         String code = "valid_authorization_code";
         Long memberId = 1L;
-        when(memberService.kakaoLogin(code)).thenReturn(memberId);
+        MemberLoginServiceResponse memberLoginServiceResponse = MemberLoginServiceResponse.from(new Member(
+                1L,
+                email,
+                password,
+                OauthProvider.KAKAO));
+        when(memberService.kakaoLogin(code)).thenReturn(memberLoginServiceResponse);
         when(tokenService.createToken(memberId)).thenReturn(token);
 
         // When & Then
-        mockMvc.perform(get("/api/member/login/kakao/callback")
+        mockMvc.perform(get("/api/members/login/kakao/callback")
                         .param("code", code))
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.AUTHORIZATION, "Bearer " + token));
+                .andExpect(jsonPath("$.email").value(email))
+                .andExpect(jsonPath("$.token").value(token));
 
         verify(memberService, times(1)).kakaoLogin(code);
         verify(tokenService, times(1)).createToken(memberId);
+    }
+
+    @Test
+    void 포인트_추가_테스트() throws Exception {
+        // Given
+        int amount = 100;
+        PointUpdateRequest request = new PointUpdateRequest(amount);
+        MemberServiceResponse memberServiceResponse = new MemberServiceResponse(memberId, email, password);
+        when(memberService.findById(anyLong())).thenReturn(memberServiceResponse);
+        when(memberService.getPoint(memberId)).thenReturn(amount);
+
+        // When & Then
+        mockMvc.perform(patch("/api/members/point")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"amount\":" + amount + "}")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.amount").value(amount));
+
+        verify(memberService, times(1)).addPoint(eq(memberId), eq(amount));
+    }
+
+    @Test
+    void 포인트_조회_테스트() throws Exception {
+        // Given
+        int point = 200;
+        MemberServiceResponse memberServiceResponse = new MemberServiceResponse(memberId, email, password);
+        when(memberService.findById(anyLong())).thenReturn(memberServiceResponse);
+        when(memberService.getPoint(memberId)).thenReturn(point);
+
+        // When & Then
+        mockMvc.perform(get("/api/members/point")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.amount").value(point));
+
+        verify(memberService, times(1)).getPoint(memberId);
     }
 }
