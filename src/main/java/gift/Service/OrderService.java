255cdfc417d15ace46d34b0ffcf9aa90ca94119d
BOMIN LYU <rbm0524@naver.com>
2024-08-04 17:41:06 +0900

    feat : 50%의 포인트 적립 추가

--- src/main/java/gift/Service/OrderService.java
+++ src/main/java/gift/Service/OrderService.java
@@ -1,6 +1,8 @@
 package gift.Service;
 
+import gift.Entity.Member;
 import gift.Entity.Product;
+import gift.Repository.MemberJpaRepository;
 import gift.Repository.ProductJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,12 @@ import java.util.List;
 public class OrderService {
 
     private final ProductJpaRepository productJpaRepository;
+    private final MemberJpaRepository memberJpaRepository;
 
     @Autowired
-    public OrderService(ProductJpaRepository productJpaRepository) {
+    public OrderService(ProductJpaRepository productJpaRepository, MemberJpaRepository memberJpaRepository) {
         this.productJpaRepository = productJpaRepository;
+        this.memberJpaRepository = memberJpaRepository;
     }
 
     public int calculateTotalPrice(List<OrderRequestDto> orderRequestDtoList) {
@@ -24,7 +28,9 @@ public class OrderService {
                     Product product = productJpaRepository.findById(orderRequestDto.getProductId()).orElseThrow();
                     int price = product.getPrice();
                     int quantity = orderRequestDto.getQuantity();
-
+                    Member member = memberJpaRepository.findById(orderRequestDto.getMemberId()).orElseThrow();
+                    member.setPoint(member.getPoint() + (price * quantity / 2)); //산 금액의 절반을 포인트로 설정!
+                    memberJpaRepository.save(member);
                     return price * quantity;
                 })
                 .sum();
