67541928023f8188d1996a935c474f306d53cb9d
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-08-04 18:30:40 +0900

    fix: 오더 레포지토리 수정

--- src/main/java/gift/repository/order/OrderSpringDataJpaRepository.java
+++ src/main/java/gift/repository/order/OrderSpringDataJpaRepository.java
@@ -4,7 +4,9 @@ import gift.domain.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderSpringDataJpaRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT o FROM Order o WHERE o.member.id = :memberId")
     Page<Order> findByMemberId(Long memberId, Pageable pageable);
 }
