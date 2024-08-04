8db0602be7cd5de69e50d519b89852be26a90638
Shsin9797 <aaa8141@naver.com>
2024-08-04 20:00:52 +0900

    feat : CORS 허용

---
+++ src/main/java/gift/config/WebConfig.java
@@ -0,0 +1,24 @@
+package gift.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.OPTIONS.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name())
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
