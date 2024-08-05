497afbc94e11b2e8af62cb1c654f0f48ca363279
Geon Kim <kky8586696@naver.com>
2024-08-05 11:18:28 +0900

    refactor : 인터셉터 OPTIONS 수정

--- src/main/java/gift/config/AuthInterceptor.java
+++ src/main/java/gift/config/AuthInterceptor.java
@@ -5,6 +5,7 @@ import gift.service.user.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -19,7 +20,7 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) { // preFlight 해결
+        if(HttpMethod.valueOf(request.getMethod()).equals(HttpMethod.OPTIONS)){
             return true;
         }
         String authHeader = request.getHeader("Authorization");
