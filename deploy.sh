0d249b581a3c3d175bbb2d8f9865ec91e31c0ab0
Awhn <ahnshwan@outlook.com>
2024-08-06 00:55:54 +0900

    fix : original script from aws

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-BUILD_PATH = $(ls /repository/spring-gift-point/build/*.jar)
+REPOSITORY=/home/ubuntu/repository/spring-gift-point/build/libs
 
-CURRENT_PID = $(pgep -f $JAR_NAME)
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-if [ -z $CURRENT_PID ]
-then
-  sleep 1
+CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-DEPLOY_PATH =/deploy/
-cp $BUILD_PATH $DEPLOY_PATH
-cd $DEPLOY_PATH
+echo "> 새 애플리케이션 배포"
 
-DEPLOY_JAR = $DEPLOY_PATH$JAR_NAME
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
