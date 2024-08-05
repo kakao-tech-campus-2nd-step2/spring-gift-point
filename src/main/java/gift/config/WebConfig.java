7ef5edb048afacd97378b63b2bce9e83e985d2d5
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-08-06 00:04:45 +0900

    feat : CORS 설정 추가

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -3,6 +3,7 @@ package gift.config;
 import gift.resolver.LoginMemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,5 +20,56 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/products/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Content-Type", "Authorization")
+            .exposedHeaders("Authorization", "Location")
+            .maxAge(1800)
+            .allowCredentials(true);
+
+        registry.addMapping("/api/categories/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Content-Type", "Authorization")
+            .exposedHeaders("Authorization", "Location")
+            .maxAge(1800)
+            .allowCredentials(true);
+
+        registry.addMapping("/api/orders/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("POST")
+            .allowedHeaders("Content-Type", "Authorization")
+            .exposedHeaders("Authorization")
+            .maxAge(1800)
+            .allowCredentials(true);
+
+        registry.addMapping("/api/wishes/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "DELETE")
+            .allowedHeaders("Content-Type", "Authorization")
+            .exposedHeaders("Authorization")
+            .maxAge(1800)
+            .allowCredentials(true);
+
+        registry.addMapping("/api/members/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("POST")
+            .allowedHeaders("Content-Type")
+            .exposedHeaders("Authorization")
+            .maxAge(1800)
+            .allowCredentials(true);
+
+        registry.addMapping("/api/products/{productId}/options/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Content-Type", "Authorization")
+            .exposedHeaders("Authorization", "Location")
+            .maxAge(1800)
+            .allowCredentials(true);
     }
 }
