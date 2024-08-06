832a471825da8654fca8096a3d8a94a13399ed75
bokyeong <wntjdals0412@gmail.com>
2024-08-06 10:20:45 +0900

    Feat: 포인트 업데이트시 유효성 체크

---
+++ src/main/java/gift/dto/PointUpdateDto.java
@@ -0,0 +1,39 @@
+package gift.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class PointUpdateDto {
+    @NotNull
+    Long id;
+    @Min(value = 0, message = "수정 가능한 포인트는 0부터 가능합니다.")
+    @Max(value = 30_000_000, message = "충전 가능한 포인트 한도는 최대 3000만원입니다.")
+    int point;
+
+    public PointUpdateDto() {
+    }
+
+    ;
+
+    public PointUpdateDto(Long id, int point) {
+        this.id = id;
+        this.point = point;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+}
