b15a852f34b224444abf4142262f6d803f203215
신예준 <dpwns1826@gmail.com>
2024-08-08 16:13:05 +0900

    feat: Swagger API 문서 추가

---
+++ src/main/java/gift/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package gift.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("카카오 선물하기")
+            .description("카카오테크캠퍼스 2기 BE : Step2 클론코딩")
+            .version("1.0.0");
+    }
+}
