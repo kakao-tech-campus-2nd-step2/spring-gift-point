807820a98d7002207a3fc565533919af81f50e0d
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-08-09 17:04:53 +0900

    feat : cors 설정 수정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -24,47 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/products/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("Content-Type", "Authorization")
-            .exposedHeaders("Authorization", "Location")
-            .maxAge(1800)
-            .allowCredentials(true);
-
-        registry.addMapping("/api/categories/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("Content-Type", "Authorization")
-            .exposedHeaders("Authorization", "Location")
-            .maxAge(1800)
-            .allowCredentials(true);
-
-        registry.addMapping("/api/orders/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("POST")
-            .allowedHeaders("Content-Type", "Authorization")
-            .exposedHeaders("Authorization")
-            .maxAge(1800)
-            .allowCredentials(true);
-
-        registry.addMapping("/api/wishes/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("GET", "POST", "DELETE")
-            .allowedHeaders("Content-Type", "Authorization")
-            .exposedHeaders("Authorization")
-            .maxAge(1800)
-            .allowCredentials(true);
-
-        registry.addMapping("/api/members/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("POST")
-            .allowedHeaders("Content-Type")
-            .exposedHeaders("Authorization")
-            .maxAge(1800)
-            .allowCredentials(true);
-
-        registry.addMapping("/api/products/{productId}/options/**")
+        registry.addMapping("/api/**")
             .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Content-Type", "Authorization")
